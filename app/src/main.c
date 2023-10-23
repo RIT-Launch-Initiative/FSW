6a01dca1d74d40da59b515c52989c19e1944f08a
Aaron Chan <aarchan108@gmail.com>
2023-10-23 12:07:42 -0400

    LSM6DSL starter code

--- app/src/main.c
+++ app/src/main.c
@@ -11,10 +11,51 @@
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-int main(void) {
-    int ret;
-    const struct device *sensor;
+static struct sensor_value accel_x_out;
+static struct sensor_value accel_y_out;
+static struct sensor_value accel_z_out;
+static struct sensor_value gyro_x_out;
+static struct sensor_value gyro_y_out;
+static struct sensor_value gyro_z_out;
 
+static int print_samples;
+static int lsm6dsl_trig_cnt;
+
+static inline float out_ev(struct sensor_value *val) {
+	return (val->val1 + (float) val->val2 / 1000000);
+}
+
+
+static void lsm6dsl_trigger_handler(const struct device *dev, const struct sensor_trigger *trig) {
+    static struct sensor_value accel_x, accel_y, accel_z;
+    static struct sensor_value gyro_x, gyro_y, gyro_z;
+
+    lsm6dsl_trig_cnt++;
+
+    sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
+    sensor_channel_get(dev, SENSOR_CHAN_ACCEL_X, &accel_x);
+    sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Y, &accel_y);
+    sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Z, &accel_z);
+
+    sensor_sample_fetch_chan(dev, SENSOR_CHAN_GYRO_XYZ);
+    sensor_channel_get(dev, SENSOR_CHAN_GYRO_X, &gyro_x);
+    sensor_channel_get(dev, SENSOR_CHAN_GYRO_Y, &gyro_y);
+    sensor_channel_get(dev, SENSOR_CHAN_GYRO_Z, &gyro_z);
+
+    if (print_samples) {
+        print_samples = 0;
+
+        accel_x_out = accel_x;
+        accel_y_out = accel_y;
+        accel_z_out = accel_z;
+
+        gyro_x_out = gyro_x;
+        gyro_y_out = gyro_y;
+        gyro_z_out = gyro_z;
+    }
+}
+
+int main(void) {
     printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
     while (1) {
