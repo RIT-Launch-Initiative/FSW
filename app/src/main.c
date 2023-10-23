2fc2e36066ec7821679c94052497dd124f9987db
Aaron Chan <aarchan108@gmail.com>
2023-10-23 14:34:24 -0400

    Stub out threads for each sensor

--- app/src/main.c
+++ app/src/main.c
@@ -11,49 +11,72 @@
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-static struct sensor_value accel_x_out;
-static struct sensor_value accel_y_out;
-static struct sensor_value accel_z_out;
-static struct sensor_value gyro_x_out;
-static struct sensor_value gyro_y_out;
-static struct sensor_value gyro_z_out;
+
 
 static int print_samples;
 static int lsm6dsl_trig_cnt;
 
-static inline float out_ev(struct sensor_value *val) {
-	return (val->val1 + (float) val->val2 / 1000000);
-}
+static void adxl375_processing_callback(int result, uint8_t *buf,uint32_t buf_len, void *userdata) {
+    static struct sensor_value accel_x;
+    static struct sensor_value accel_y;
+    static struct sensor_value accel_z;
 
+    while (true) {
 
-static void lsm6dsl_trigger_handler(const struct device *dev, const struct sensor_trigger *trig) {
-    static struct sensor_value accel_x, accel_y, accel_z;
-    static struct sensor_value gyro_x, gyro_y, gyro_z;
-
-    lsm6dsl_trig_cnt++;
-
-    sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
-    sensor_channel_get(dev, SENSOR_CHAN_ACCEL_X, &accel_x);
-    sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Y, &accel_y);
-    sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Z, &accel_z);
-
-    sensor_sample_fetch_chan(dev, SENSOR_CHAN_GYRO_XYZ);
-    sensor_channel_get(dev, SENSOR_CHAN_GYRO_X, &gyro_x);
-    sensor_channel_get(dev, SENSOR_CHAN_GYRO_Y, &gyro_y);
-    sensor_channel_get(dev, SENSOR_CHAN_GYRO_Z, &gyro_z);
-
-    if (print_samples) {
-        print_samples = 0;
-
-        accel_x_out = accel_x;
-        accel_y_out = accel_y;
-        accel_z_out = accel_z;
-
-        gyro_x_out = gyro_x;
-        gyro_y_out = gyro_y;
-        gyro_z_out = gyro_z;
     }
 }
+
+static void bmp388_processing_callback(int result, uint8_t *buf,uint32_t buf_len, void *userdata) {
+    static struct sensor_value pressure;
+    static struct sensor_value temperature;
+
+    while (true) {
+
+    }
+}
+
+static void lsm6dsl_processing_callback(int result, uint8_t *buf,uint32_t buf_len, void *userdata) {
+    static struct sensor_value accel_x;
+    static struct sensor_value accel_y;
+    static struct sensor_value accel_z;
+    static struct sensor_value gyro_x;
+    static struct sensor_value gyro_y;
+    static struct sensor_value gyro_z;
+
+    while (true) {
+
+    }
+}
+
+static void lis3mdl_processing_callback(int result, uint8_t *buf,uint32_t buf_len, void *userdata) {
+    static struct sensor_value mag_x;
+    static struct sensor_value mag_y;
+    static struct sensor_value mag_z;
+
+
+    while (true) {
+
+    }
+}
+
+static void ms5607_processing_callback(int result, uint8_t *buf,uint32_t buf_len, void *userdata) {
+    static struct sensor_value pressure;
+    static struct sensor_value temperature;
+
+    while (true) {
+
+    }
+}
+
+static void tmp117_processing_callback(int result, uint8_t *buf,uint32_t buf_len, void *userdata) {
+    static struct sensor_value temperature;
+
+    while (true) {
+
+    }
+}
+
+
 
 int main(void) {
     printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
