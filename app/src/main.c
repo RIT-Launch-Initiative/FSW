fd4dcb11a0ac0e76253878951630d92a842e7b92
Aaron Chan <aarchan108@gmail.com>
2023-11-10 12:23:30 -0500

    Wiznet configs added. Need to setup pins though

--- app/src/main.c
+++ app/src/main.c
@@ -8,58 +8,86 @@
 #include <app_version.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+#define SLEEP_TIME_MS   1000
+#define LED0_NODE DT_ALIAS(led0)
+
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL
+);
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 // Check if board is power module
-#ifdef CONFIG_BOARD_POWER_MODULE
+//#ifdef CONFIG_BOARD_POWER_MODULE
+//
+//int main(void) {
+//    const struct device *const ina = DEVICE_DT_GET_ONE(ti_ina219);
+//    struct sensor_value v_bus;
+//    struct sensor_value power;
+//    struct sensor_value current;
+//
+//    if (!device_is_ready(ina)) {
+//        printf("Device %s is not ready.\n", ina->name);
+//        return 0;
+//    }
+//
+//    while (true) {
+//        if (sensor_sample_fetch(ina)) {
+//            printf("Could not fetch sensor data.\n");
+//            return 0;
+//        }
+//
+//        sensor_channel_get(ina, SENSOR_CHAN_VOLTAGE, &v_bus);
+//        sensor_channel_get(ina, SENSOR_CHAN_POWER, &power);
+//        sensor_channel_get(ina, SENSOR_CHAN_CURRENT, &current);
+//
+//        printf("Bus: %f [V] -- "
+//               "Power: %f [W] -- "
+//               "Current: %f [A]\n",
+//               sensor_value_to_double(&v_bus),
+//               sensor_value_to_double(&power),
+//               sensor_value_to_double(&current));
+//        k_sleep(K_MSEC(2000));
+//    }
+//
+//    return 0;
+//}
+//
+//#elif CONFIG_BOARD_RADIO_MODULE
+int main() {
+    int ret;
 
-int main(void) {
-    const struct device *const ina = DEVICE_DT_GET_ONE(ti_ina219);
-    struct sensor_value v_bus;
-    struct sensor_value power;
-    struct sensor_value current;
-
-    if (!device_is_ready(ina)) {
-        printf("Device %s is not ready.\n", ina->name);
+    if (!gpio_is_ready_dt(&led)) {
         return 0;
     }
 
-    while (true) {
-        if (sensor_sample_fetch(ina)) {
-            printf("Could not fetch sensor data.\n");
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
+
+    while (1) {
+        printk("Hello World!\n");
+        ret = gpio_pin_toggle_dt(&led);
+        if (ret < 0) {
             return 0;
         }
+        k_msleep(SLEEP_TIME_MS);
+    }
 
-        sensor_channel_get(ina, SENSOR_CHAN_VOLTAGE, &v_bus);
-        sensor_channel_get(ina, SENSOR_CHAN_POWER, &power);
-        sensor_channel_get(ina, SENSOR_CHAN_CURRENT, &current);
-
-        printf("Bus: %f [V] -- "
-               "Power: %f [W] -- "
-               "Current: %f [A]\n",
-               sensor_value_to_double(&v_bus),
-               sensor_value_to_double(&power),
-               sensor_value_to_double(&current));
-        k_sleep(K_MSEC(2000));
+    while (1) {
     }
 
     return 0;
 }
 
-#elif CONFIG_BOARD_RADIO_MODULE
-int main() {
 
-    return 0;
-}
-
-
-#elif CONFIG_BOARD_SENSOR_MODULE
-int main() {
-
-    return 0;
-}
-
-
-#else
-
-#endif
+//#elif CONFIG_BOARD_SENSOR_MODULE
+//int main() {
+//
+//    return 0;
+//}
+//
+//
+//#else
+//
+//#endif
