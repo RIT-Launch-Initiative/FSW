aeef0f3da80b0435f154bc02123947ea94c856b5
Aaron Chan <aarchan108@gmail.com>
2023-11-11 13:41:30 -0500

    Configure sensor mod clock to 16 MHz HSI

--- sensor_module/src/main.c
+++ sensor_module/src/main.c
@@ -5,6 +5,8 @@
 
 #include <app_version.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
+
 #include <zephyr/fs/fs.h>
 #include <zephyr/fs/littlefs.h>
 #include <zephyr/kernel.h>
@@ -14,8 +16,8 @@
 
 #define SLEEP_TIME_MS   1000
 
-
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL
+);
 K_QUEUE_DEFINE(net_tx_queue);
 
 static void network_init() {
@@ -26,19 +28,55 @@ static void network_init() {
 static void init(void) {
     // Queues
     k_queue_init(&net_tx_queue);
-
-    // Devices
-
-
     network_init();
 }
+
+static void lsm6dsl_task() {
+//    const struct device *const lsm6dsl_dev = DEVICE_DT_GET_ONE(lsm6dsl);
+//    struct sensor_value accel[3];
+//
+//    if (!device_is_ready(lsm6dsl_dev)) {
+//        LOG_ERR("LSM6DSL device not ready");
+//        return;
+//    }
+
+}
+
+static void lis3mdl_task() {
+
+}
+
+static void ms5607_task() {
+
+}
+
+static void tmp117_task() {
+
+}
+
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+#define LED1_NODE DT_ALIAS(led1)
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+
 
 int main() {
     init();
 
 
+    gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+    gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+
+    while (1) {
+        gpio_pin_toggle_dt(&led0);
+        gpio_pin_toggle_dt(&led1);
+        k_msleep(100);
+    }
+
     return 0;
 }
+
 
 
 
