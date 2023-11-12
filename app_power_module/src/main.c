c09cc2bcec4a65c6ed4f3c511643c405c37619b2
Aaron Chan <aarchan108@gmail.com>
2023-11-12 02:44:54 -0500

    INA219 initializations, but stuck trying to not get it to crash when initializing tasks

--- app_power_module/src/main.c
+++ app_power_module/src/main.c
@@ -11,22 +11,18 @@
 //#include <zephyr/fs/littlefs.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-//#include <zephyr/net/socket.h>
 #include <zephyr/storage/flash_map.h>
 
-#include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/net_event.h>
-#include <zephyr/net/conn_mgr_monitor.h>
 
 #include <zephyr/net/socket.h>
 #include <zephyr/net/ethernet.h>
-#include <zephyr/net/ethernet_mgmt.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 K_QUEUE_DEFINE(net_tx_queue);
 
-#define STACK_SIZE (2048)
-static K_THREAD_STACK_ARRAY_DEFINE(stacks, 2, STACK_SIZE);
+#define STACK_SIZE (256)
+static K_THREAD_STACK_ARRAY_DEFINE(ina_stacks, 3, STACK_SIZE);
 
 static struct net_if *net_interface;
 
@@ -42,51 +38,7 @@ typedef struct {
     ina_data_t ina_5v0;
 } power_module_data_t;
 
-static void ina_task(void *device, void *data, void *unused2) {
-    const struct device *const dev = (const struct device *) device;
-    ina_data_t *ina_data = (ina_data_t *) data;
-    
-    if (!device_is_ready(dev)) {
-        printk("Device %s is not ready.\n", dev->name);
-        return;
-    }
-
-    while (true) {
-        sensor_sample_fetch(dev);
-        sensor_channel_get(dev, SENSOR_CHAN_VOLTAGE, &ina_data->voltage);
-        sensor_channel_get(dev, SENSOR_CHAN_POWER, &ina_data->power);
-        sensor_channel_get(dev, SENSOR_CHAN_CURRENT, &ina_data->current);
-        k_sleep(K_MSEC(100));
-    }
-}
-
-static int init_net_stack(void) {
-    static const char ip_addr[] = "10.10.10.69";
-    int ret;
-
-    net_interface = net_if_get_default();
-    if (!net_interface) {
-        printk("No network interface found\n");
-        return -ENODEV;
-    }
-
-    struct in_addr addr;
-    ret = net_addr_pton(AF_INET, ip_addr, &addr);
-    if (ret < 0) {
-        printk("Invalid IP address\n");
-        return ret;
-    }
-
-    struct net_if_addr *ifaddr = net_if_ipv4_addr_add(net_interface, &addr, NET_ADDR_MANUAL, 0);
-    if (!ifaddr) {
-        printk("Failed to add IP address\n");
-        return -ENODEV;
-    }
-
-    printk("IPv4 address configured: %s\n", ip_addr);
-
-    return 0;
-}
+static power_module_data_t power_module_data = {0};
 
 int send_udp_broadcast(const char *data, size_t data_len) {
     int sock;
@@ -121,33 +73,132 @@ int send_udp_broadcast(const char *data, size_t data_len) {
     return 0;
 }
 
-static void tx_data(void *power_module_data, void *unused1, void *unused2) {
+static void tx_data(void *unused0, void *unused1, void *unused2) {
     while (true) {
-        send_udp_broadcast((const char *) power_module_data, sizeof(power_module_data));
+        send_udp_broadcast("Launch!", 7);
+//        send_udp_broadcast((const char *) &power_module_data, sizeof(power_module_data));
         k_sleep(K_MSEC(100));
     }
 }
+
+
+static void ina_task(void *p_id, void *unused1, void *unused2) {
+    const struct device *dev;
+    ina_data_t *ina_data;
+    int id = (int) p_id;
+
+    switch (id) {
+        case 0:
+            dev = DEVICE_DT_GET(DT_INST(0, ti_ina219));
+            ina_data = &power_module_data.ina_battery;
+            break;
+        case 1:
+            dev = DEVICE_DT_GET(DT_INST(1, ti_ina219));
+            ina_data = &power_module_data.ina_3v3;
+            break;
+        case 2:
+            dev = DEVICE_DT_GET(DT_INST(2, ti_ina219));
+            ina_data = &power_module_data.ina_5v0;
+            break;
+        default:
+            return;
+    }
+
+
+    while (true) {
+        sensor_sample_fetch(dev);
+        sensor_channel_get(dev, SENSOR_CHAN_VOLTAGE, &ina_data->voltage);
+        sensor_channel_get(dev, SENSOR_CHAN_POWER, &ina_data->power);
+        sensor_channel_get(dev, SENSOR_CHAN_CURRENT, &ina_data->current);
+
+        k_sleep(K_MSEC(100));
+    }
+}
+
+static int init_ina219_devices() {
+    const struct device *const ina_battery =  DEVICE_DT_GET(DT_INST(0, ti_ina219));
+    const struct device *const ina_3v3 =  DEVICE_DT_GET(DT_INST(1, ti_ina219));
+    const struct device *const ina_5v0 = DEVICE_DT_GET(DT_INST(2, ti_ina219));
+
+    int ret = 0;
+    if (device_is_ready(ina_battery)) {
+        struct k_thread ina_battery_thread;
+//        k_thread_create(&ina_battery_thread, ina_stacks[0], STACK_SIZE,
+//                        ina_task, (void *) 0, NULL, NULL, 0, 0, K_NO_WAIT);
+//        k_thread_start(&ina_battery_thread);
+    } else {
+        printk("Device %s is not ready.\n", ina_battery->name);
+        ret |= 0b1;
+    }
+
+    if (device_is_ready(ina_3v3)) {
+        struct k_thread ina_3v3_thread;
+//        k_thread_create(&ina_3v3_thread, ina_stacks[1], 256,
+//                        ina_task, 1, NULL, NULL, 0, 0, K_NO_WAIT);
+//        k_thread_start(&ina_3v3_thread);
+    } else {
+        printk("Device %s is not ready.\n", ina_3v3->name);
+        ret |= 0b10;
+    }
+
+    if (device_is_ready(ina_5v0)) {
+        struct k_thread ina_5v0_thread;
+//        k_thread_create(&ina_5v0_thread, ina_stacks[2], 256,
+//                        ina_task, 2, NULL, NULL, 0, 0, K_NO_WAIT);
+//        k_thread_start(&ina_5v0_thread);
+    } else {
+        printk("Device %s is not ready.\n", ina_5v0->name);
+        ret |= 0b100;
+    }
+
+    return ret;
+}
+
+static int init_net_stack(void) {
+    static const char ip_addr[] = "10.10.10.69";
+    int ret;
+
+    net_interface = net_if_get_default();
+    if (!net_interface) {
+        printk("No network interface found\n");
+        return -ENODEV;
+    }
+
+    struct in_addr addr;
+    ret = net_addr_pton(AF_INET, ip_addr, &addr);
+    if (ret < 0) {
+        printk("Invalid IP address\n");
+        return ret;
+    }
+
+    struct net_if_addr *ifaddr = net_if_ipv4_addr_add(net_interface, &addr, NET_ADDR_MANUAL, 0);
+    if (!ifaddr) {
+        printk("Failed to add IP address\n");
+        return -ENODEV;
+    }
+
+    printk("IPv4 address configured: %s\n", ip_addr);
+
+    return 0;
+}
+
 
 static int init(void) {
     // Queues
 //    k_queue_init(&net_tx_queue);
 
-    // Threads
-//    struct k_thread led_toggle_thread;
-//    k_thread_create(&led_toggle_thread, stacks[0], STACK_SIZE,
-//                    led_toggle, NULL, NULL, NULL,
-//                    0, 0, K_NO_WAIT);
-//    k_thread_start(&led_toggle_thread);
 
     const struct device *const wiznet = DEVICE_DT_GET_ONE(wiznet_w5500);
     if (!device_is_ready(wiznet)) {
         printk("Device %s is not ready.\n", wiznet->name);
+        return -ENODEV;
     } else {
         printk("Device %s is ready.\n", wiznet->name);
-        return init_net_stack();
+        init_net_stack();
     }
 
-    return -1;
+
+    return 0;
 }
 
 
@@ -158,8 +209,12 @@ int main(void) {
         }
     }
 
-    while (1) {
-        send_udp_broadcast("Launch!", 7);
+    init_ina219_devices();
+
+
+    while (true) {
+        send_udp_broadcast((const char *) &power_module_data, sizeof(power_module_data));
+        k_sleep(K_MSEC(100));
     }
     return 0;
 }
--- app_sensor_module/src/main.c
+++ app_sensor_module/src/main.c
@@ -19,8 +19,7 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL
 K_QUEUE_DEFINE(net_tx_queue);
 
 #define STACK_SIZE (2048)
-static K_THREAD_STACK_ARRAY_DEFINE(stacks,
-2, STACK_SIZE);
+static K_THREAD_STACK_ARRAY_DEFINE(stacks, 2, STACK_SIZE);
 
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
