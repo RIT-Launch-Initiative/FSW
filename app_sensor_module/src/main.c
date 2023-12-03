0e86ced9ba89ebf883885481f9553fcd7dc2358f
Aaron Chan <aarchan108@gmail.com>
2023-12-02 19:03:25 -0500

    Update sensors generic function

--- app_sensor_module/src/main.c
+++ app_sensor_module/src/main.c
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <app_version.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/gpio.h>
@@ -21,8 +16,6 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 K_QUEUE_DEFINE(net_tx_queue);
 
 #define STACK_SIZE (512)
-static K_THREAD_STACK_ARRAY_DEFINE(stacks, 4, STACK_SIZE);
-static struct k_thread threads[4] = {0};
 
 // #define LED0_NODE DT_ALIAS(led0)
 // static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -64,30 +57,12 @@ static void init(void) {
     // Queues
     k_queue_init(&net_tx_queue);
 
-    // if (!init_eth_iface()) {
-    //     printk("Ethernet ready\n");
-    //     if (!init_net_stack()) {
-    //         printk("Network stack initialized\n");
-    //         // printk("Ethernet thread started\n");
-    //         
-    //     }
-    // }
-
-    // k_thread_create(&threads[1], &stacks[1][0], STACK_SIZE,
-    //                 randomize_data, NULL, NULL, NULL,
-    //                 K_PRIO_PREEMPT(10), 0, K_NO_WAIT);
-    // k_thread_start(&threads[1]);
-    
-    k_thread_create(&threads[2], &stacks[2][0], STACK_SIZE,
-                    update_lsm6dsl_data, NULL, NULL, NULL,
-                     K_PRIO_PREEMPT(10), 0, K_NO_WAIT);
-
-    k_thread_start(&threads[2]);
-
-    k_thread_create(&threads[3], &stacks[3][0], STACK_SIZE,
-                    update_tmp117_data, NULL, NULL, NULL,
-                    K_PRIO_PREEMPT(10), 0, K_NO_WAIT);
-    k_thread_start(&threads[3]);
+    if (!init_eth_iface()) {
+        printk("Ethernet ready\n");
+        if (!init_net_stack()) {
+            printk("Network stack initialized\n");
+        }
+    }
 
 }
 
