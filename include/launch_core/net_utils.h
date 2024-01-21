ccd0e670d47659f80ca2dae763f688569056cdfc
Aaron Chan <aarchan108@gmail.com>
2024-01-20 19:49:33 -0800

    Functions for dealing with hashmap for port to function mapping. Need to figure out way to force hashmap kconfig to be selected when using the networking lib

--- include/launch_core/net_utils.h
+++ include/launch_core/net_utils.h
@@ -9,7 +9,20 @@
 
 #include <stdint.h>
 #include <stddef.h>
+
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+
+#include <zephyr/net/socket.h>
+#include <zephyr/net/ethernet.h>
+#include <zephyr/net/net_event.h>
+
+
+/********** UDP **********/
+typedef struct {
+    uint16_t port;
+    void (*handler)(uint8_t *data, size_t data_len);
+} l_udp_port_handler_t;
 
 /**
  * Initialize a UDP networking stack
@@ -26,5 +39,30 @@ int l_init_udp_net_stack(const char *ip);
  * @return Zephyr status code
  */
 int l_send_udp_broadcast(const uint8_t *data, size_t data_len, uint16_t port);
+
+/**
+ * Receive UDP callback
+ * @param dev - Network device
+ * @param packet - Packet received
+ * @param status - Status of the packet
+ * @return Zephyr status code
+ */
+int l_receive_udp_callback(const struct device *dev, struct net_pkt *packet, int status);
+
+/**
+ * Add a function for handling data from a specific port
+ * @param port - Port to handle
+ * @param handler - Function to handle data
+ * @return Zephyr status code
+ */
+int l_add_port_handler(uint16_t port, void (*handler)(uint8_t *data, size_t data_len));
+
+/**
+ * Remove a port handler
+ * @param port - Port to remove
+ * @return Zephyr status code
+ */
+int l_remove_port_handler(uint16_t port);
+
 
 #endif
--- lib/launch_core/net_utils.c
+++ lib/launch_core/net_utils.c
@@ -5,15 +5,14 @@
  */
 
 #include <launch_core/net_utils.h>
-
-#include <zephyr/kernel.h>
-#include <zephyr/net/socket.h>
-#include <zephyr/net/ethernet.h>
-#include <zephyr/net/net_event.h>
+#include <zephyr/sys/hash_map.h>
+#include <zephyr/types.h>
 
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(net_utils, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(net_utils);
+
+SYS_HASHMAP_DEFINE_STATIC(UDP_PORT_HANDLERS);
 
 static struct net_if *net_interface;
 
@@ -29,13 +28,13 @@ int l_init_udp_net_stack(const char *ip_addr) {
     struct in_addr addr;
     ret = net_addr_pton(AF_INET, ip_addr, &addr);
     if (ret < 0) {
-        LOG_INF("Invalid IP address\n");
+        LOG_ERR("Invalid IP address\n");
         return ret;
     }
 
     struct net_if_addr *ifaddr = net_if_ipv4_addr_add(net_interface, &addr, NET_ADDR_MANUAL, 0);
     if (!ifaddr) {
-        LOG_INF("Failed to add IP address\n");
+        LOG_ERR("Failed to add IP address\n");
         return -ENODEV;
     }
 
@@ -50,7 +49,7 @@ int l_send_udp_broadcast(const uint8_t *data, size_t data_len, uint16_t port) {
 
     sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sock < 0) {
-        LOG_INF("Failed to create socket (%d)\n", sock);
+        LOG_ERR("Failed to create socket (%d)\n", sock);
         return sock;
     }
 
@@ -59,14 +58,14 @@ int l_send_udp_broadcast(const uint8_t *data, size_t data_len, uint16_t port) {
     dst_addr.sin_port = htons(port);
     ret = net_addr_pton(AF_INET, "255.255.255.255", &dst_addr.sin_addr);
     if (ret < 0) {
-        LOG_INF("Invalid IP address format\n");
+        LOG_ERR("Invalid IP address format\n");
         close(sock);
         return ret;
     }
 
     ret = sendto(sock, data, data_len, 0, (struct sockaddr *) &dst_addr, sizeof(dst_addr));
     if (ret < 0) {
-        LOG_INF("Failed to send UDP broadcast (%d)\n", ret);
+        LOG_ERR("Failed to send UDP broadcast (%d)\n", ret);
         close(sock);
         return ret;
     }
@@ -75,4 +74,17 @@ int l_send_udp_broadcast(const uint8_t *data, size_t data_len, uint16_t port) {
 
     close(sock);
     return 0;
+}
+
+int l_receive_udp_callback(const struct device *dev, struct net_pkt *packet, int status) {
+    // TODO: Currently being implemented and tested in another branch
+    return 0;
+}
+
+int l_add_port_handler(uint16_t port, void (*handler)(uint8_t *data, size_t data_len)) {
+    return sys_hashmap_insert(&UDP_PORT_HANDLERS, &port, handler, NULL);
+}
+
+int l_remove_port_handler(uint16_t port) {
+    return sys_hashmap_remove(&UDP_PORT_HANDLERS, &port, NULL);
 }
