11b648e8c31f5d5f77bee450210620cfb7c16e13
Aaron Chan <aarchan108@gmail.com>
2024-01-20 19:10:52 -0800

    Document all shared library functions

--- include/launch_core/net_utils.h
+++ include/launch_core/net_utils.h
@@ -11,8 +11,20 @@
 #include <stddef.h>
 #include <zephyr/kernel.h>
 
+/**
+ * Initialize a UDP networking stack
+ * @param ip - IP address to bind to
+ * @return Zephyr status code
+ */
 int l_init_udp_net_stack(const char *ip);
 
+/**
+ * Send a UDP broadcast message
+ * @param buff - Buffer of data to transmit
+ * @param len - Size of the buffer
+ * @param port - Port to transmit on
+ * @return Zephyr status code
+ */
 int l_send_udp_broadcast(const uint8_t *data, size_t data_len, uint16_t port);
 
 #endif
