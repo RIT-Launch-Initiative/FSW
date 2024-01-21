5534223c3e784e5f2aef1d3850a83a938f4023a7
Aaron Chan <aarchan108@gmail.com>
2024-01-20 22:41:56 -0800

    Read sensor data function

--- include/launch_core/device_utils.h
+++ include/launch_core/device_utils.h
@@ -7,6 +7,14 @@
 #define DEVICE_UTILS_H
 
 #include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+
+typedef struct {
+    int num_readings;
+    enum sensor_channel *channels;
+    struct sensor_value **values;
+    float **float_values;
+} SENSOR_READINGS_ARGS_T;
 
 /**
  * Confirm that the device is present and ready to be used.
@@ -14,5 +22,14 @@
  * @return Zephyr status code (0 if device is ready, -ENODEV otherwise)
  */
 int l_check_device(const struct device *const dev);
+
+/**
+ * Read sensor data from a device.
+ * @param dev - Device to read from
+ * @param args - Arguments for reading the sensor data
+ * @param convert_to_float - Whether to convert the sensor data to floats and store them
+ * @return Zephyr status code
+ */
+int l_read_sensor_data(const struct device *const dev, SENSOR_READINGS_ARGS_T *args, bool convert_to_float);
 
 #endif //DEVICE_UTILS_H
--- lib/launch_core/device_utils.c
+++ lib/launch_core/device_utils.c
@@ -4,7 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <launch_core/device_utils.h>
+
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(device_utils);
 
 int l_check_device(const struct device *const dev) {
     if (!device_is_ready(dev)) {
@@ -13,5 +17,32 @@ int l_check_device(const struct device *const dev) {
     }
 
     printk("Device %s is ready.\n", dev->name);
+    return 0;
+}
+
+int l_read_sensor_data(const struct device *const dev, SENSOR_READINGS_ARGS_T *args, bool convert_to_float) {
+    int num_readings = args->num_readings;
+    enum sensor_channel *channels = args->channels;
+
+    struct sensor_value **values = args->values;
+    float **float_values = args->float_values;
+
+    int ret = sensor_sample_fetch(dev);
+    if (ret < 0) {
+        LOG_ERR("Failed to update %s readings. Errno %d\n", dev->name, ret);
+        return ret;
+    }
+
+    for (int i = 0; i < num_readings; i++) {
+        ret = sensor_channel_get(dev, channels[i], values[i]);
+        if (ret < 0) {
+            LOG_ERR("Failed to get sensor channel stored at index %d. Errno %d\n", i, ret);
+        }
+
+        if (convert_to_float) {
+            *float_values[i] = sensor_value_to_float(values[i]);
+        }
+    }
+
     return 0;
 }
