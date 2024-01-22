03755d42fcb7b766aca06e92c5629d0b91cfb0ca
Aaron Chan <aarchan108@gmail.com>
2024-01-21 21:42:19 -0800

    Synchronous ADC read

--- lib/launch_core/device_utils.c
+++ lib/launch_core/device_utils.c
@@ -56,6 +56,35 @@ int l_init_adc_channels(const struct adc_dt_spec *const channels, struct adc_seq
     return 0;
 }
 
+int l_read_adc_mv(const struct adc_dt_spec *const channel, struct adc_sequence *const sequence, int32_t *val) {
+    int ret = adc_read_dt(channel, sequence);
+    int32_t val_mv = 0;
+
+    if (ret == 0) {
+        if (channel->channel_cfg.differential) { // Differential channels are 16 bits
+            val_mv = (int32_t) *((int16_t*) sequence->buffer);
+        } else {
+            val_mv = *((int32_t *) (sequence->buffer));
+        }
+
+        ret = adc_raw_to_millivolts_dt(channel, &val_mv);
+        if (ret < 0) {
+            LOG_ERR("Could not convert ADC value from %d to millivolts. Errno %d.", channel->channel_id, ret);
+        }
+
+        *val = val_mv;
+    } else {
+        LOG_ERR("Could not read ADC value from %d. Errno %d.", channel->channel_id, ret);
+    }
+
+    return ret;
+}
+
+int l_async_read_adc_mv(const struct adc_dt_spec *const channel, struct adc_sequence *const sequence, int32_t *val) {
+    return -1;
+}
+
+
 /********** SENSORS **********/
 int l_update_get_sensor_data(const struct device *const dev, l_sensor_readings_args_t *args, bool convert_to_float) {
     l_update_sensors(&dev, 1);
