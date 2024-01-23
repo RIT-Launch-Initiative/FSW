10654da9520f6c628d3b49bf74b1642390ecb431
cowsed <richie.sommers@outlook.com>
2024-01-22 22:12:32 -0500

    finally figured out what io-channel-cells address-cells and size-cells are

--- drivers/sensor/mcp356x/mcp356x.c
+++ drivers/sensor/mcp356x/mcp356x.c
@@ -6,11 +6,12 @@
 #define DT_DRV_COMPAT microchip_mcp356x
 
 #include <zephyr/device.h>
+#include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/spi.h>
-
 #include <zephyr/logging/log.h>
+
 LOG_MODULE_REGISTER(mcp356x, CONFIG_SENSOR_LOG_LEVEL);
 
 struct mcp356x_data {
@@ -19,33 +20,23 @@ struct mcp356x_data {
 
 struct mcp356x_config {
   struct spi_dt_spec bus;
+  uint8_t channels; // 1 2 4
 };
 
-static int mcp356x_sample_fetch(const struct device *dev,
-                                enum sensor_channel chan) {
-  const struct mcp356x_config *config = dev->config;
-  struct mcp356x_data *data = dev->data;
+static int mcp356x_read_channel(const struct device *dev,
+                                const struct adc_sequence *sequence) {
 
-  return 0;
+  return -1;
 }
 
-static int mcp356x_channel_get(const struct device *dev,
-                               enum sensor_channel chan,
-                               struct sensor_value *val) {
-  struct mcp356x_data *data = dev->data;
-
-  if (chan != SENSOR_CHAN_VOLTAGE) {
-    return -ENOTSUP;
-  }
-
-  val->val1 = data->state;
-
-  return 0;
+int mcp356x_channel_setup(const struct device *dev,
+                          const struct adc_channel_cfg *channel_cfg) {
+  return -1;
 }
 
-static const struct sensor_driver_api mcp356x_api = {
-    .sample_fetch = &mcp356x_sample_fetch,
-    .channel_get = &mcp356x_channel_get,
+static const struct adc_driver_api mcp356x_api = {
+    .channel_setup = &mcp356x_channel_setup,
+    .read = &mcp356x_read_channel,
 };
 
 static int mcp356x_init(const struct device *dev) {
@@ -69,7 +60,7 @@ static int mcp356x_init(const struct device *dev) {
       .bus = SPI_DT_SPEC_GET(                                                  \
           DT_INST(i, DT_DRV_COMPAT),                                           \
           SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8), 0),         \
-  };                                                                           \
+      .channels = DT_PROP(DT_INST(i, DT_DRV_COMPAT), num_channels)};           \
   DEVICE_DT_INST_DEFINE(i, mcp356x_init, NULL, &mcp356x_data_##i,              \
                         &mcp356x_config_##i, POST_KERNEL,                      \
                         CONFIG_SENSOR_INIT_PRIORITY, &mcp356x_api);
