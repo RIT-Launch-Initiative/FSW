a33bc49dbecdd09ea49204ede05e3986e5adfadb
Aaron Chan <aarchan108@gmail.com>
2024-01-26 20:42:17 -0800

    Nuke main

--- app_potato/src/main.c
+++ app_potato/src/main.c
@@ -1,8 +1,7 @@
 #include <app_version.h>
 
-#include <launch_core/backplane_defs.h>
 #include <launch_core/device_utils.h>
-#include <launch_core/net_utils.h>
+#include <launch_core/net_utils.h> // TODO: Might need for SLIP
 
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/gpio.h>
@@ -12,51 +11,18 @@
 #include <zephyr/net/socket.h>
 #include <zephyr/storage/flash_map.h>
 
-#include "sensors.h"
-
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(main, CONFIG_APP_POTATO_LOG_LEVEL);
 K_QUEUE_DEFINE(net_tx_queue);
 
 #define STACK_SIZE (512)
-#define LED0_NODE DT_ALIAS(led0)
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-#define LED1_NODE DT_ALIAS(led1)
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-static const struct device *const wiznet = DEVICE_DT_GET_ONE(wiznet_w5500);
 
 static int init(void) {
-    char ip[MAX_IP_ADDRESS_STR_LEN];
-    int ret = -1;
-
-    k_queue_init(&net_tx_queue);
-
-    if (0 > l_create_ip_str_default_net_id(ip, SENSOR_MODULE_ID, 1)) {
-        LOG_ERR("Failed to create IP address string: %d", ret);
-        return -1;
-    }
-
-    k_queue_init(&net_tx_queue);
-
-    if (!l_check_device(wiznet)) {
-        if (!l_init_udp_net_stack(ip)) {
-            LOG_ERR("Failed to initialize network stack");
-        }
-    } else {
-        LOG_ERR("Failed to get network device");
-    }
-
-    // Sensors
-
     return 0;
 }
 
 int main() {
     if (!init()) {
         return -1;
-    }
-
-    while (1) {
     }
 
     return 0;
