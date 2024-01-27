6cf803229d38690de83d6db332228645b0495534
cowsed <richie.sommers@outlook.com>
2024-01-27 14:01:48 -0500

    remove unnecessary hardware configs

--- app_grim_reefer/src/main.c
+++ app_grim_reefer/src/main.c
@@ -4,32 +4,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
-
 #include <zephyr/fs/fs.h>
 #include <zephyr/fs/littlefs.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/storage/flash_map.h>
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(main, CONFIG_APP_GRIM_REEFER_LOG_LEVEL_DBG);
 
-static int init(void) {
-    return 0;
-}
-
+static int init(void) { return 0; }
 
 int main(void) {
-    if (init()) {
-        return -1;
-    }
+  if (init()) {
+    return -1;
+  }
 
-    // Won't run if initializing the network stack failed
-    while (true) {
-        // convert_and_send();
+  // Won't run if initializing the network stack failed
+  while (true) {
+    // convert_and_send();
 
-        k_sleep(K_MSEC(100));
-    }
-    return 0;
+    k_sleep(K_MSEC(100));
+  }
+  return 0;
 }
-
