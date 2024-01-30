57200b403be9fd2c3104e21427f3b41a81094dec
cowsed <richie.sommers@outlook.com>
2024-01-30 18:44:30 -0500

    added ldo enable and cam enable gpio. migrated stmcube file to most recent version

--- samples/shield_blinky/src/main.c
+++ samples/shield_blinky/src/main.c
@@ -1,35 +1,39 @@
 #include "zephyr/logging/log.h"
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/kernel.h>
 
-#include <zephyr/drivers/gpio.h>
 #include <stdio.h>
-
+#include <zephyr/drivers/gpio.h>
 
 // devicetree gets
 #define LED_NODE DT_ALIAS(led)
 
 const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
+const struct gpio_dt_spec ldo_enable =
+    GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, gpio_enable_ldos);
+const struct gpio_dt_spec cam_enable =
+    GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, gpio_enable_cameras);
+
 int main(void) {
-	int counter = 0;
-	// Boilerplate: set up GPIO
-	if (!gpio_is_ready_dt(&led)) {
-		printf("GPIO is not ready\n");
-		return 0;
-	}
+  int counter = 0;
+  // Boilerplate: set up GPIO
+  if (!gpio_is_ready_dt(&led)) {
+    printf("GPIO is not ready\n");
+    return 0;
+  }
 
-	if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
-		printf("Unable to configure LED output pin\n");
-		return 0;
-	}
+  if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+    printf("Unable to configure LED output pin\n");
+    return 0;
+  }
 
-	// forever
-	while(1) {
-		gpio_pin_toggle_dt(&led);
-		printk("LED toggle %d\n", ++counter);
-		k_msleep(100);
-	}
-	return 0;
+  // forever
+  while (1) {
+    gpio_pin_toggle_dt(&led);
+    printk("LED toggle %d\n", ++counter);
+    k_msleep(100);
+  }
+  return 0;
 }
