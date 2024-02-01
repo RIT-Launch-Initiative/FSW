fe7cdcc19221ae928b9363d711fcfcd926523408
cowsed <richie.sommers@outlook.com>
2024-01-31 19:23:31 -0500

    led, pin definitions and initilization

--- app_grim_reefer/src/main.c
+++ app_grim_reefer/src/main.c
@@ -21,7 +21,49 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_GRIM_REEFER_LOG_LEVEL_DBG);
 const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 
-static int init(void) { return 0; }
+#define LDO_EN_NODE DT_NODELABEL(ldo_enable)
+#define CAM_EN_NODE DT_NODELABEL(cam_enable)
+
+const struct gpio_dt_spec ldo_enable = GPIO_DT_SPEC_GET(LDO_EN_NODE, gpios);
+const struct gpio_dt_spec cam_enable = GPIO_DT_SPEC_GET(CAM_EN_NODE, gpios);
+
+static int init(void) {
+  // Init LEDS
+  if (!gpio_is_ready_dt(&led1)) {
+    LOG_ERR("LED 1 is not ready\n");
+    return -1;
+  }
+  if (gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE) < 0) {
+    LOG_ERR("Unable to configure LED 1 output pin\n");
+    return -1;
+  }
+  if (!gpio_is_ready_dt(&led2)) {
+    LOG_ERR("LED 2 is not ready\n");
+    return -1;
+  }
+  if (gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE) < 0) {
+    LOG_ERR("Unable to configure LED 2 output pin\n");
+    return -1;
+  }
+  // Init Enable pins
+  if (!gpio_is_ready_dt(&ldo_enable)) {
+    LOG_ERR("ldo enable pin is not ready\n");
+    return -1;
+  }
+  if (gpio_pin_configure_dt(&ldo_enable, GPIO_OUTPUT_ACTIVE) < 0) {
+    LOG_ERR("Unable to configure ldo enable output pin\n");
+    return -1;
+  }
+  if (!gpio_is_ready_dt(&cam_enable)) {
+    LOG_ERR("camera enable pin is not ready\n");
+    return -1;
+  }
+  if (gpio_pin_configure_dt(&cam_enable, GPIO_OUTPUT_ACTIVE) < 0) {
+    LOG_ERR("Unable to configure camera enable output pin\n");
+    return -1;
+  }
+  return 0;
+}
 
 int main(void) {
   if (init()) {
