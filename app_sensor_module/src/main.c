ecba2756f3b5a24a0e72f707974dd4a6a8077e23
Aaron Chan <aarchan108@gmail.com>
2024-02-22 23:13:35 -0800

    Get rid of more garbage

--- app_sensor_module/src/main.c
+++ app_sensor_module/src/main.c
@@ -7,41 +7,37 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#define STACK_SIZE (512)
+
 LOG_MODULE_REGISTER(main, CONFIG_APP_SENSOR_MODULE_LOG_LEVEL);
 
+// Queues
 static struct k_msgq ten_hz_telemetry_queue;
 static uint8_t ten_hz_telemetry_queue_buffer[CONFIG_TEN_HZ_QUEUE_SIZE * sizeof(ten_hz_telemetry_t)];
 
 static struct k_msgq hundred_hz_telemetry_queue;
 static uint8_t hundred_hz_telemetry_queue_buffer[CONFIG_HUNDRED_HZ_QUEUE_SIZE * sizeof(hundred_hz_telemetry_t)];
 
-static struct k_msgq extension_board_telemetry_queue;
-static uint8_t extension_board_telemetry_queue_buffer[CONFIG_EXTENSION_BOARD_QUEUE_SIZE * 16]; // TODO: Come up with a good size
 
-#define STACK_SIZE (512)
+// Devices
 //#define LED0_NODE DT_ALIAS(led0)
 //static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 //
 //#define LED1_NODE DT_ALIAS(led1)
 //static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-//static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-//static const struct device *const ms5611 = DEVICE_DT_GET_ONE(meas_ms5611);
-
-//static const struct device *const adxl375 = DEVICE_DT_GET_ONE(adi_adxl375);
 
 static int init(void) {
     char ip[MAX_IP_ADDRESS_STR_LEN];
 
     k_queue_init(&ten_hz_telemetry_queue);
     k_queue_init(&hundred_hz_telemetry_queue);
-    k_queue_init(&extension_board_telemetry_queue);
+
     int ret = 0;
 
     if (0 > l_create_ip_str_default_net_id(ip, SENSOR_MODULE_ID, 1)) {
         LOG_ERR("Failed to create IP address string: %d", ret);
         return -1;
     }
-
 
     if (!l_check_device(DEVICE_DT_GET_ONE(wiznet_w5500))) {
         if (!l_init_udp_net_stack(ip)) {
