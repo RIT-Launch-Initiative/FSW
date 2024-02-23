2a47ec1e85b97a28a355dae1d7b985b8fbb48a0f
Aaron Chan <aarchan108@gmail.com>
2024-02-22 22:53:52 -0800

    Move new sensor data types to core lib

--- app_sensor_module/src/sensors.h
+++ app_sensor_module/src/sensors.h
@@ -1,78 +1,22 @@
 #ifndef SENSORS_H_
 #define SENSORS_H_
 
+#include <launch_core/dev/sensor_data_types.h>
 
 typedef struct {
-    float accel_x;
-    float accel_y;
-    float accel_z;
-} accelerometer_data_t;
+    accelerometer_data_t adxl375;
+    accelerometer_data_t lsm6dsl_accel;
+    
+    barometer_data_t ms5611;
+    barometer_data_t bmp388;
 
-typedef struct __attribute__((packed)) {
-    float accel_x;
-    float accel_y;
-    float accel_z;
-} accelerometer_data_packed_t;
+    gyroscope_data_t lsm6dsl_gyro;
 
-typedef struct {
-    float pressure;
-    float temperature;
-} barometer_data_t;
+    magnetometer_data_t lis3mdl;
 
-typedef struct __attribute__((packed)) {
-    float pressure;
-    float temperature;
-} barometer_data_packed_t;
+    temperature_data_t LL
 
-typedef struct {
-    float gyro_x;
-    float gyro_y;
-    float gyro_z;
-} gyroscope_data_t;
-
-typedef struct __attribute__((packed)) {
-    float gyro_x;
-    float gyro_y;
-    float gyro_z;
-} gyroscope_data_packed_t;
-
-typedef struct {
-    float mag_x;
-    float mag_y;
-    float mag_z;
-} magnetometer_data_t;
-
-typedef struct __attribute__((packed)) {
-    float mag_x;
-    float mag_y;
-    float mag_z;
-} magnetometer_data_packed_t;
-
-typedef float temperature_data_t;
-typedef float temperature_data_float_t; // Just to keep things consistent
-
-
-typedef struct {
-    float pressure_ms5;
-    float temperature_ms5;
-
-    float pressure_bmp3;
-    float temperature_bmp3;
-
-    float accel_x;
-    float accel_y;
-    float accel_z;
-   
-    float magn_x;
-    float magn_y;
-    float magn_z;
-
-    float gyro_x;
-    float gyro_y;
-    float gyro_z;
-
-    float temperature_tmp;
-} sensor_module_telemetry_t;
+} hundred_hz_telemetry_t;
 
 typedef struct {
     float pressure_ms5;
