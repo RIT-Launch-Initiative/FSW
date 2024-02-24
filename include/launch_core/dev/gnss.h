1dbd671ed7771a2f63bf998cb35d8d93cf9b66d4
Aaron Chan <aarchan108@gmail.com>
2024-02-24 15:12:03 -0800

    Documentation for new GNSS lib

--- include/launch_core/dev/gnss.h
+++ include/launch_core/dev/gnss.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #ifndef L_GNSS_H
 #define L_GNSS_H
 
@@ -7,8 +13,26 @@
 #define L_GNSS_LONGITUDE_DIVISION_FACTOR 1000000000.0F
 #define L_GNSS_ALTITUDE_DIVISION_FACTOR 1000.0F
 
+/**
+ * Debug callback for logging GNSS fix status
+ * @param dev - GNSS device that triggered the callback
+ * @param data - GNSS data from the device
+ */
 void l_gnss_fix_debug_cb(const struct device *dev, const struct gnss_data *data);
+
+/**
+ * Debug callback for logging GNSS data
+ * @param dev - GNSS device that triggered the callback
+ * @param data - GNSS data from the device
+ */
 void l_gnss_data_debug_cb(const struct device *dev, const struct gnss_data *data);
+
+/**
+ * Debug callback for logging GNSS satellite count
+ * @param dev - GNSS device that triggered the callback
+ * @param satellites - Array of satellite data
+ * @param size - Number of satellite data in the array
+ */
 void l_gnss_debug_sat_count_cb(const struct device *dev, const struct gnss_satellite *satellites, uint16_t size);
 
 #endif // L_GNSS_H
--- lib/launch_core/dev/gnss.c
+++ lib/launch_core/dev/gnss.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <launch_core/dev/gnss.h>
 #include <zephyr/logging/log.h>
 
