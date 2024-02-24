da509794fc21792943e265401a0a116c7fdba5e9
Aaron Chan <aarchan108@gmail.com>
2024-02-24 14:58:24 -0800

    Add constants in gnss lib for calculating the actual latitude and longitude

--- include/launch_core/dev/gnss.h
+++ include/launch_core/dev/gnss.h
@@ -3,6 +3,10 @@
 
 #include <zephyr/drivers/gnss.h>
 
+#define L_GNSS_LATITUDE_DIVISION_FACTOR 1000000000.0F
+#define L_GNSS_LONGITUDE_DIVISION_FACTOR 1000000000.0F
+#define L_GNSS_ALTITUDE_DIVISION_FACTOR 1000.0F
+
 void l_gnss_fix_debug_cb(const struct device *dev, const struct gnss_data *data);
 void l_gnss_data_debug_cb(const struct device *dev, const struct gnss_data *data);
 void l_gnss_debug_sat_count_cb(const struct device *dev, const struct gnss_satellite *satellites, uint16_t size);
--- lib/launch_core/dev/gnss.c
+++ lib/launch_core/dev/gnss.c
@@ -14,7 +14,7 @@ void l_gnss_fix_debug_cb(const struct device *dev, const struct gnss_data *data)
 void l_gnss_data_debug_cb(const struct device *dev, const struct gnss_data *data) {
     if (data->info.fix_status != GNSS_FIX_STATUS_NO_FIX) {
         LOG_INF("%s has fix!\r\n", dev->name);
-        LOG_INF("\tCoordinates: %lld, %lld, %d\r\n", data->nav_data.latitude, data->nav_data.longitude, data->nav_data.altitude);
+        LOG_INF("\tCoordinates: %f, %f, %f\r\n", data->nav_data.latitude / L_GNSS_LATITUDE_DIVISION_FACTOR, data->nav_data.longitude / L_GNSS_LONGITUDE_DIVISION_FACTOR, data->nav_data.altitude / L_GNSS_ALTITUDE_DIVISION_FACTOR);
         LOG_INF("\tTime: %u-%u-%u %u:%u:%u.%u\r\n",
                 data->utc.month, data->utc.month_day, data->utc.century_year,
                 data->utc.hour, data->utc.minute, data->utc.millisecond / 1000, data->utc.millisecond % 1000);
