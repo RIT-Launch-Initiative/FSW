8910e316cef21171f4f031b3d8026404f659ea5a
Aaron Chan <aarchan108@gmail.com>
2024-02-24 15:09:24 -0800

    Fix compile warnings and pad 0s for logging time

--- lib/launch_core/dev/gnss.c
+++ lib/launch_core/dev/gnss.c
@@ -13,12 +13,16 @@ void l_gnss_fix_debug_cb(const struct device *dev, const struct gnss_data *data)
 
 void l_gnss_data_debug_cb(const struct device *dev, const struct gnss_data *data) {
     if (data->info.fix_status != GNSS_FIX_STATUS_NO_FIX) {
+        double latitude = data->nav_data.latitude / L_GNSS_LATITUDE_DIVISION_FACTOR;
+        double longitude = data->nav_data.longitude / L_GNSS_LONGITUDE_DIVISION_FACTOR;
+        double altitude = data->nav_data.altitude / L_GNSS_ALTITUDE_DIVISION_FACTOR;
+
         LOG_INF("%s has fix!\r\n", dev->name);
-        LOG_INF("\tCoordinates: %f, %f, %f\r\n", data->nav_data.latitude / L_GNSS_LATITUDE_DIVISION_FACTOR, data->nav_data.longitude / L_GNSS_LONGITUDE_DIVISION_FACTOR, data->nav_data.altitude / L_GNSS_ALTITUDE_DIVISION_FACTOR);
-        LOG_INF("\tTime: %u-%u-%u %u:%u:%u.%u\r\n",
+        LOG_INF("Coordinates: %lf, %lf, %lf\r\n", latitude, longitude, altitude);
+        LOG_INF("UTC Time: %u-%u-%u %u:%u:%02u.%02u\r\n",
                 data->utc.month, data->utc.month_day, data->utc.century_year,
                 data->utc.hour, data->utc.minute, data->utc.millisecond / 1000, data->utc.millisecond % 1000);
-        LOG_INF("\tSatellites: %u, Quality: %u\r\n", data->info.satellites_cnt, data->info.fix_quality);
+        LOG_INF("Satellites: %u, Quality: %u\r\n", data->info.satellites_cnt, data->info.fix_quality);
 
     } else {
         LOG_INF("%s has no fix!\r\n", dev->name);
