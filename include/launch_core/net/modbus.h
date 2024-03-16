93f0dd9d0f2a39fc37d0344fd48ab225a93fea4c
Aaron Chan <aarchan108@gmail.com>
2024-03-16 00:20:01 -0700

    Document init function

--- include/launch_core/net/modbus.h
+++ include/launch_core/net/modbus.h
@@ -8,6 +8,11 @@
 #define L_DEFAULT_MODBUS_PARITY UART_CFG_PARITY_NONE
 #define L_DEFAULT_MODBUS_STOP_BITS_CLIENT UART_CFG_STOP_BITS_2
 
+/**
+ * Initialize a Modbus Client or Server
+ * @param unit_id - Unit ID of the Modbus device. Set to 0 for a client.
+ * @return Zephyr status code of the initialization
+ */
 int l_modbus_init(const uint8_t unit_id);
 
 #endif //L_MODBUS_H_
