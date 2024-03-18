66e2835e5d6b83137c2936b09e9d21db60d59005
Aaron Chan <aarchan108@gmail.com>
2024-03-17 21:38:36 -0700

    Add a TODO for FP16 support

--- include/launch_core/types.h
+++ include/launch_core/types.h
@@ -1,6 +1,13 @@
 #ifndef L_SENSOR_DATA_TYPES_H
 #define L_SENSOR_DATA_TYPES_H
 
+#include <stdint.h>
+
+/****** Primitive Data Types ******/
+// TODO: See if we can support __fp16 in GSW first
+// typedef __fp16 float16_t;
+
+/****** Telemetry Data Types ******/
 typedef struct __attribute__((packed)) {
     float accel_x;
     float accel_y;
@@ -28,8 +35,7 @@ typedef struct __attribute__((packed)) {
     float current;
     float voltage;
     float power;
-} l_shunt_data_t
-
+} l_shunt_data_t;
 
 typedef float l_temperature_data_t;
 
