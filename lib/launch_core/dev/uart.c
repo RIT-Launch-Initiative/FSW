e1fbb410916896b00c882972bc348840497d0674
Aaron Chan <aarchan108@gmail.com>
2024-03-17 20:12:59 -0700

    Init rs485 functionality

--- lib/launch_core/dev/uart.c
+++ lib/launch_core/dev/uart.c
@@ -7,9 +7,16 @@ int l_uart_init_rs485(const struct device *dev) {
 
     int ret = uart_config_get(dev, &config);
     if (unlikely(ret < 0)) {
-        LOG_INF()
+        LOG_ERR("Failed to get UART device configuration");
+        return ret;
     }
 
+    config.flow_ctrl = UART_CFG_FLOW_CTRL_RS485;
 
-    return uart_configure(dev, &config);
+    ret = uart_configure(dev, &config);
+    if (unlikely(ret < 0)) {
+        LOG_ERR("Failed to set UART device configuration with RS485 flow control");
+    }
+
+    return ret;
 }
