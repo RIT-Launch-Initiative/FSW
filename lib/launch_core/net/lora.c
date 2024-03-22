cc015aed7b1a383e1c25f739f1c38dd01fae6248
Aaron Chan <aarchan108@gmail.com>
2024-03-22 00:15:16 -0700

    Remove misleading const in lora params

--- lib/launch_core/net/lora.c
+++ lib/launch_core/net/lora.c
@@ -27,7 +27,7 @@ static struct lora_modem_config config = {
         .tx = true
 };
 
-void l_lora_debug_recv_cb(const struct device *const dev, uint8_t *data, uint16_t size, int16_t rssi, int8_t snr) {
+void l_lora_debug_recv_cb(const struct device *dev, uint8_t *data, uint16_t size, int16_t rssi, int8_t snr) {
     if (size != 0) {
         LOG_INF("Received %d bytes:\n\tMem View: ", size);
         for (uint16_t i = 0; i < size; i++) LOG_INF("0x%02x ", data[i]);
@@ -39,44 +39,44 @@ void l_lora_debug_recv_cb(const struct device *const dev, uint8_t *data, uint16_
     }
 }
 
-int l_lora_tx(const struct device *const dev, uint8_t *buff, size_t len) {
+int l_lora_tx(const struct device *dev, uint8_t *buff, size_t len) {
     l_lora_configure(dev, true);
     int ret = lora_send(dev, buff, len);
     l_lora_configure(dev, false);
     return ret;
 }
 
-int l_lora_set_tx_rx(const struct device *const dev, bool transmit) {
+int l_lora_set_tx_rx(const struct device *dev, bool transmit) {
     config.tx = transmit;
     return lora_config(dev, &config);
 }
 
-int l_lora_set_frequency(const struct device *const dev, uint32_t frequency) {
+int l_lora_set_frequency(const struct device *dev, uint32_t frequency) {
     config.frequency = frequency;
     return lora_config(dev, &config);
 }
 
-int l_lora_set_bandwidth(const struct device *const dev, enum lora_signal_bandwidth bandwidth) {
+int l_lora_set_bandwidth(const struct device *dev, enum lora_signal_bandwidth bandwidth) {
     config.bandwidth = bandwidth;
     return lora_config(dev, &config);
 }
 
-int l_lora_set_data_rate(const struct device *const dev, enum lora_datarate data_rate) {
+int l_lora_set_data_rate(const struct device *dev, enum lora_datarate data_rate) {
     config.datarate = data_rate;
     return lora_config(dev, &config);
 }
 
-int l_lora_set_spreading_factor(const struct device *const dev, enum lora_coding_rate coding_rate) {
+int l_lora_set_spreading_factor(const struct device *dev, enum lora_coding_rate coding_rate) {
     config.coding_rate = coding_rate;
     return lora_config(dev, &config);
 }
 
-int l_lora_set_preamble_len(const struct device *const dev, uint16_t preamble_len) {
+int l_lora_set_preamble_len(const struct device *dev, uint16_t preamble_len) {
     config.preamble_len = preamble_len;
     return lora_config(dev, &config);
 }
 
-int l_lora_set_tx_power(const struct device *const dev, int8_t tx_power) {
+int l_lora_set_tx_power(const struct device *dev, int8_t tx_power) {
     config.tx_power = tx_power;
     return lora_config(dev, &config);
 }
