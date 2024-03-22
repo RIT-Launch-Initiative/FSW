6974998da1d01590ed98441a1a3dbbaaa9e247a6
Aaron Chan <aarchan108@gmail.com>
2024-03-21 22:44:28 -0700

    Stub unique functions

--- app_radio_module/src/receiver.c
+++ app_radio_module/src/receiver.c
@@ -1,1 +1,17 @@
+#if defined(RADIO_MODULE_RECEIVER)
 #include "radio_module_functionality.h"
+
+int init_lora_unique() {
+    return 0;
+}
+
+int init_udp_unique() {
+    return 0;
+}
+
+int start_tasks() {
+    return 0;
+}
+
+
+#endif
