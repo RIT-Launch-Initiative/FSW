9bbc7fca26ab008a01e10b25e3c329cc65b19576
Aaron Chan <aarchan108@gmail.com>
2024-03-22 00:10:36 -0700

    Document new functions

--- include/launch_core/net/lora.h
+++ include/launch_core/net/lora.h
@@ -24,7 +24,7 @@
  * @param transmit - True if the device should be configured for transmission. False otherwise
  * @return Zephyr status code
  */
-int l_lora_configure(const struct device *const dev, bool transmit);
+int l_lora_configure(const struct device *dev, bool transmit);
 
 /**
  * Transmit a message over LoRa.
@@ -33,7 +33,7 @@ int l_lora_configure(const struct device *const dev, bool transmit);
  * @param len - Size of the buffer
  * @return Zephyr status code
  */
-int l_lora_tx(const struct device *const lora_dev, uint8_t *buff, size_t len);
+int l_lora_tx(const struct device *lora_dev, uint8_t *buff, size_t len);
 
 /**
  * Debug callback function for LoRa reception. Should never be called directly
@@ -43,7 +43,7 @@ int l_lora_tx(const struct device *const lora_dev, uint8_t *buff, size_t len);
  * @param rssi - Received signal strength indicator
  * @param snr - Signal to noise ratio
  */
-void l_lora_debug_recv_cb(const struct device *const dev, uint8_t *data, uint16_t size, int16_t rssi, int8_t snr);
+void l_lora_debug_recv_cb(const struct device *dev, uint8_t *data, uint16_t size, int16_t rssi, int8_t snr);
 
 /**
  * Configure LoRa radio devices for transmission or reception.
@@ -51,16 +51,54 @@ void l_lora_debug_recv_cb(const struct device *const dev, uint8_t *data, uint16_
  * @param transmit - True if the device should be configured for transmission. False otherwise
  * @return Zephyr status code
  */
-int l_lora_set_tx_rx(const struct device *const dev, bool transmit);
+int l_lora_set_tx_rx(const struct device *dev, bool transmit);
 
+/**
+ * Set the frequency of the LoRa device
+ * @param dev - Device to configure
+ * @param frequency - Frequency to set the device to
+ * @return Zephyr status code
+ */
+int l_lora_set_frequency(const struct device *dev, uint32_t frequency);
 
-int l_lora_set_frequency(const struct device *const dev, uint32_t frequency);
-int l_lora_set_bandwidth(const struct device *const dev, enum lora_signal_bandwidth bandwidth);
-int l_lora_set_data_rate(const struct device *const dev, enum lora_datarate data_rate);
-int l_lora_set_coding_rate(const struct device *const dev, enum lora_coding_rate coding_rate);
-int l_lora_set_preamble_len(const struct device *const dev, uint16_t preamble_len);
-int l_lora_set_tx_power(const struct device *const dev, int8_t tx_power);
+/**
+ * Set the bandwidth of the LoRa device
+ * @param dev - Device to configure
+ * @param bandwidth - Bandwidth to set the device to
+ * @return Zephyr status code
+ */
+int l_lora_set_bandwidth(const struct device *dev, enum lora_signal_bandwidth bandwidth);
 
+/**
+ * Set the data rate of the LoRa device
+ * @param dev - Device to configure
+ * @param data_rate - Data rate to set the device to
+ * @return Zephyr status code
+ */
+int l_lora_set_data_rate(const struct device *dev, enum lora_datarate data_rate);
 
+/**
+ * Set the coding rate of the LoRa device
+ * @param dev - Device to configure
+ * @param coding_rate - Coding rate to set the device to
+ * @return
+ */
+int l_lora_set_coding_rate(const struct device *dev, enum lora_coding_rate coding_rate);
+
+/**
+ * Set the preamble length of the LoRa device
+ * @param dev - Device to configure
+ * @param preamble_len - Preamble length to set the device to
+ * @return Zephyr status code
+ */
+int l_lora_set_preamble_len(const struct device *dev, uint16_t preamble_len);
+
+/**
+ * Set the transmission power of the LoRa device
+ * @param dev - Device to configure
+ * @param tx_power - Transmission power to set the device to
+ * @return Zephyr status code
+ */
+int l_lora_set_tx_power(const struct device *dev, int8_t tx_power);
 
 #endif // L_LORA_UTILS_H_
