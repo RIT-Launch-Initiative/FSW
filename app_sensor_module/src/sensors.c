7b8d5df385ec55b132266a0e3cacc6c19f7909cb
Aaron Chan <aarchan108@gmail.com>
2024-04-01 23:06:30 -0700

    More helper functions for populating sensor arrays

--- app_sensor_module/src/sensors.c
+++ app_sensor_module/src/sensors.c
@@ -7,6 +7,8 @@
 // Launch Includes
 #include <launch_core/types.h>
 #include <launch_core/dev/dev_common.h>
+#include <launch_core/dev/sensor.h>
+
 
 // Zephyr Includes
 #include <zephyr/kernel.h>
@@ -37,19 +39,32 @@ static bool check_and_add_sensor(const struct device *dev, const struct device *
     return false;
 }
 
+static uint8_t populate_sensor_arr(const struct device **dev_arr, enum sensor_channel **channels_arr) {
+    uint8_t device_count = 0;
+
+    if (check_and_add_sensor(DEVICE_DT_GET(DT_INST(0, adi_adxl375)), dev_arr, &device_count)) {
+        channels_arr[device_count] = (enum sensor_channel[]) {
+                SENSOR_CHAN_ACCEL_X,
+                SENSOR_CHAN_ACCEL_Y,
+                SENSOR_CHAN_ACCEL_Z
+        };
+    }
+
+    return device_count;
+}
+
 static void hundred_hz_sensor_reading_task(void *unused0, void *unused1, void *unused2) {
     ARG_UNUSED(unused0);
     ARG_UNUSED(unused1);
     ARG_UNUSED(unused2);
 
     const struct device *sensor_arr[NUM_SENSORS] = {};
+    enum sensor_channel *channels_arr[NUM_SENSORS] = {};
+    const uint8_t num_sensors = populate_sensor_arr(sensor_arr, channels_arr);
+
     sensor_module_hundred_hz_telemetry_t telemetry = {};
 
-    uint8_t device_count = 0;
 
 
-    if (check_and_add_sensor(DEVICE_DT_GET(DT_INST(0, adi_adxl375)), sensor_arr, &device_count)) {
-
-    }
 
 }
