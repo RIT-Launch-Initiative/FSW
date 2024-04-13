39a410de20f09ed098cd9c4179c38329323af33e
Nate Aquino <naquino14@outlook.com>
2024-04-13 14:05:01 -0400

    Conversions library and altitude from pressure and temperature conversion

---
+++ include/launch_core/conversions.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 Aaron Chan
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Authors: Nate Aquino
+ */
+
+#ifndef _MATH_H
+#include <math.h>
+#endif // _MATH_H
+
+/* Constants */
+
+/* Temperature */
+#define CELSIUS_TO_KELVIN 273.15f
+
+/* Pressure */
+#define SEA_LEVEL_PRESSURE_BAR 1.01325f
+
+/* Magic */
+#define HYPSOMETRIC_FORMULA_DENOMINATOR 0.0065f
+#define PRESSURE_MAGIC_EXPONENT 0.190226f
+
+#ifndef CONVERSIONS_H
+#define CONVERSIONS_H
+
+/* Altitude from Pressure and Temperature */
+/**
+ * @brief Calculate the altitude from the pressure and temperature
+ * @param pressure The pressure in bar
+ * @param temperature The temperature in degrees Celsius
+ * @return The altitude in meters
+*/
+inline float l_alt_fr_press_temp(float pressure, float temperature);
+
+#endif //CONVERSIONS_H
---
+++ lib/launch_core/conversions.c
@@ -0,0 +1,6 @@
+#include <launch_core/conversions.h>
+#include "conversions.h"
+
+inline float l_alt_fr_press_temp(float pressure, float temperature) {
+    return ((pow((SEA_LEVEL_PRESSURE_BAR / pressure), PRESSURE_MAGIC_EXPONENT) - 1.0f) * (temperature + CELSIUS_TO_KELVIN)) / HYPSOMETRIC_FORMULA_DENOMINATOR;
+}
