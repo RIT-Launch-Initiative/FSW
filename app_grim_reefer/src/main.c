fdc513f58e0b8eb9955528b50d6091065fe0010d
cowsed <richie.sommers@outlook.com>
2024-04-17 13:42:53 -0400

    add in ina stuff from ina260dev branch

--- app_grim_reefer/src/main.c
+++ app_grim_reefer/src/main.c
@@ -3,20 +3,29 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include "config.h"
 
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/gpio.h>
-// #include <zephyr/fs/fs.h>
-// #include <zephyr/fs/littlefs.h>
+
+#include <launch_core/dev/dev_common.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/storage/flash_map.h>
+
+#include "testing.h"
+
 LOG_MODULE_REGISTER(main, CONFIG_APP_GRIM_REEFER_LOG_LEVEL_DBG);
 
 // devicetree gets
-#define LED1_NODE DT_NODELABEL(redled)
-#define LED2_NODE DT_NODELABEL(anotherled)
+#define LED1_NODE DT_NODELABEL(led1)
+#define LED2_NODE DT_NODELABEL(led2)
 
 const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
@@ -27,7 +36,35 @@ const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 const struct gpio_dt_spec ldo_enable = GPIO_DT_SPEC_GET(LDO_EN_NODE, gpios);
 const struct gpio_dt_spec cam_enable = GPIO_DT_SPEC_GET(CAM_EN_NODE, gpios);
 
-static int init(void) {
+#define BUZZER_NODE DT_NODELABEL(buzzer)
+const struct gpio_dt_spec buzzer = GPIO_DT_SPEC_GET(BUZZER_NODE, gpios);
+
+#define DBG_SERIAL_NODE DT_ALIAS(debug_serial)
+const struct device *const debug_serial_dev = DEVICE_DT_GET(DBG_SERIAL_NODE);
+
+#define BME280_NODE DT_NODELABEL(bme280)
+const struct device *bme280_dev = DEVICE_DT_GET(BME280_NODE);
+
+#define LSM6DSL_NODE DT_NODELABEL(lsm6dsl)
+const struct device *lsm6dsl_dev = DEVICE_DT_GET(LSM6DSL_NODE);
+
+#define FLASH_NODE DT_NODELABEL(w25q512)
+const struct device *flash_dev = DEVICE_DT_GET(FLASH_NODE);
+
+// Inas
+#define INA_BAT_NODE DT_NODELABEL(ina260_battery)
+const struct device *ina_bat_dev = DEVICE_DT_GET(INA_BAT_NODE);
+
+#define INA_LDO_NODE DT_NODELABEL(ina260_ldo)
+const struct device *ina_ldo_dev = DEVICE_DT_GET(INA_LDO_NODE);
+
+#define INA_GRIM_NODE DT_NODELABEL(ina260_3v3)
+const struct device *ina_grim_dev = DEVICE_DT_GET(INA_GRIM_NODE);
+
+static const struct adc_dt_spec adc_chan0 =
+    ADC_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 0);
+
+static int gpio_init(void) {
   // Init LEDS
   if (!gpio_is_ready_dt(&led1)) {
     LOG_ERR("LED 1 is not ready\n");
@@ -37,6 +74,7 @@ static int init(void) {
     LOG_ERR("Unable to configure LED 1 output pin\n");
     return -1;
   }
+
   if (!gpio_is_ready_dt(&led2)) {
     LOG_ERR("LED 2 is not ready\n");
     return -1;
@@ -54,6 +92,7 @@ static int init(void) {
     LOG_ERR("Unable to configure ldo enable output pin\n");
     return -1;
   }
+
   if (!gpio_is_ready_dt(&cam_enable)) {
     LOG_ERR("camera enable pin is not ready\n");
     return -1;
@@ -62,19 +101,76 @@ static int init(void) {
     LOG_ERR("Unable to configure camera enable output pin\n");
     return -1;
   }
+
+  if (!gpio_is_ready_dt(&buzzer)) {
+    LOG_ERR("buzzer pin is not ready\n");
+    return -1;
+  }
+  if (gpio_pin_configure_dt(&buzzer, GPIO_OUTPUT_ACTIVE) < 0) {
+    LOG_ERR("Unable to configure buzzer output pin\n");
+    return -1;
+  }
+
+  if (!device_is_ready(debug_serial_dev)) {
+    LOG_ERR("Debug serial not ready\n");
+    return -1;
+  }
+
+  gpio_pin_set_dt(&led1, 0);
+  gpio_pin_set_dt(&led2, 0);
+  gpio_pin_set_dt(&ldo_enable, 0);
+  gpio_pin_set_dt(&cam_enable, 0);
+  gpio_pin_set_dt(&buzzer, 0);
+
+  return 0;
+}
+
+static int sensor_init(void) {
+  const bool lsm6dsl_found = device_is_ready(lsm6dsl_dev);
+  const bool bme280_found = device_is_ready(bme280_dev);
+  const bool flash_found = device_is_ready(flash_dev);
+  const bool all_good = lsm6dsl_found && flash_found && bme280_found;
+  if (!all_good) {
+    LOG_ERR("Error setting up sensor and flash devices");
+    return -1;
+  }
+
+  const bool ina_bat_found = device_is_ready(ina_bat_dev);
+  const bool ina_ldo_found = device_is_ready(ina_ldo_dev);
+  const bool ina_grim_found = device_is_ready(ina_grim_dev);
+  const bool all_good2 = ina_bat_found && ina_ldo_found && ina_grim_found;
+
+  if (!all_good2) {
+    LOG_ERR("Error setting up INA260 devices");
+    return -1;
+  }
+
+  // ADC
+  if (!adc_is_ready_dt(&adc_chan0)) {
+    LOG_ERR("ADC controller device %s not ready\n", adc_chan0.dev->name);
+    return -1;
+  }
+  //
+  if (adc_channel_setup_dt(&adc_chan0) < 0) {
+    LOG_ERR("Could not setup channel\n");
+    return -1;
+  }
+
   return 0;
 }
 
 int main(void) {
-  if (init()) {
+  if (gpio_init()) {
     return -1;
   }
-
-  // Won't run if initializing the network stack failed
+  if (sensor_init()) {
+    return -1;
+  }
+  gpio_pin_set_dt(&ldo_enable, 1);
+  gpio_pin_set_dt(&buzzer, 0);
   while (true) {
-    // convert_and_send();
-
-    k_sleep(K_MSEC(100));
+    gpio_pin_toggle_dt(&led1);
+    k_msleep(2000);
   }
   return 0;
 }
