6d935ab70ce747c1faf0362fcd716f77f1d4128b
Aaron Chan <aarchan108@gmail.com>
2024-04-19 23:53:30 -0700

    Get rid of sensor mod warnings by deleting code. Brought back in a future PR

--- app_sensor_module/src/main.c
+++ app_sensor_module/src/main.c
@@ -1,11 +1,11 @@
 #include "sensors.h"
 
 #include <launch_core/backplane_defs.h>
-#include <launch_core/dev/dev_common.h>
+#include <launch_core/types.h>
 #include <launch_core/dev/uart.h>
 #include <launch_core/net/net_common.h>
 #include <launch_core/net/udp.h>
-#include <launch_core/types.h>
+#include <launch_core/dev/dev_common.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -20,47 +20,6 @@ static struct k_msgq ten_hz_telemetry_queue;
 static uint8_t ten_hz_telemetry_queue_buffer[CONFIG_TEN_HZ_QUEUE_SIZE * sizeof(sensor_module_ten_hz_telemetry_t)];
 
 extern struct k_msgq hundred_hz_telem_queue;
-// Threads
-static K_THREAD_STACK_DEFINE(telemetry_processing_stack, STACK_SIZE);
-static struct k_thread telemetry_processing_thread;
-
-// Devices
-#define LED0_NODE DT_ALIAS(led0)
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-#define LED1_NODE DT_ALIAS(led1)
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-
-static void telemetry_processing_task(void *, void *, void *) {
-    sensor_module_ten_hz_telemetry_t ten_hz_telem;
-    sensor_module_hundred_hz_telemetry_t hundred_hz_telem;
-
-    int ten_hz_socket =
-        l_init_udp_socket(SENSOR_MODULE_IP_ADDR, SENSOR_MODULE_BASE_PORT + SENSOR_MODULE_TEN_HZ_DATA_PORT);
-    int hundred_hz_socket =
-        l_init_udp_socket(SENSOR_MODULE_IP_ADDR, SENSOR_MODULE_BASE_PORT + SENSOR_MODULE_HUNDRED_HZ_DATA_PORT);
-
-    while (true) {
-        if (0 == k_msgq_get(&ten_hz_telemetry_queue, &ten_hz_telem, K_NO_WAIT)) {
-            l_send_udp_broadcast(ten_hz_socket, (uint8_t *) &ten_hz_telem, sizeof(sensor_module_ten_hz_telemetry_t),
-                                 SENSOR_MODULE_BASE_PORT + SENSOR_MODULE_TEN_HZ_DATA_PORT);
-        } else {
-            LOG_WRN("Failed to get data from 10 Hz queue");
-        }
-
-        if (0 == k_msgq_get(&hundred_hz_telem_queue, &hundred_hz_telem, K_NO_WAIT)) {
-            l_send_udp_broadcast(hundred_hz_socket, (uint8_t *) &hundred_hz_telem,
-                                 sizeof(sensor_module_hundred_hz_telemetry_t),
-                                 SENSOR_MODULE_BASE_PORT + SENSOR_MODULE_HUNDRED_HZ_DATA_PORT);
-        } else {
-            LOG_WRN("Failed to get data from 100 Hz queue");
-        }
-
-        // TODO: Extension board support. Need to figure out a robust way of doing this
-
-        // TODO: write to flash when data logging library is ready
-    }
-}
 
 static void initialize_networks(void) {
     char eth_ip[MAX_IP_ADDRESS_STR_LEN];
@@ -87,8 +46,7 @@ static void initialize_networks(void) {
             LOG_ERR("Failed to create IP address string");
         }
     } else {
-        LOG_ERR("Failed to initialize UART to RS485");
-        ;
+        LOG_ERR("Failed to initialize UART to RS485");;
     }
 }
 
