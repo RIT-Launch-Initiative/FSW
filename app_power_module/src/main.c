949e1b6d70917a313f692b475626b82e0455db45
Aaron Chan <aarchan108@gmail.com>
2024-04-29 19:11:03 -0700

    Add a debug blinky in main

--- app_power_module/src/main.c
+++ app_power_module/src/main.c
@@ -5,11 +5,22 @@
  */
 
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_POWER_MODULE_LOG_LEVEL);
 
-
 int main(void) {
+#ifdef CONFIG_DEBUG
+    static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+    static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+
+    while (true) {
+        gpio_pin_toggle_dt(&led0);
+        gpio_pin_toggle_dt(&led1);
+        k_msleep(1000);
+    }
+#endif
+
     return 0;
 }
