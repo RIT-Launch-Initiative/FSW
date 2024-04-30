7c90338c9f6b68f57e8f0680bcca5f047a5ab1a2
Aaron Chan <aarchan108@gmail.com>
2024-04-29 18:06:24 -0700

    Use timer sync instead of trying to calculate a sleep time

--- app_power_module/src/telemetry.c
+++ app_power_module/src/telemetry.c
@@ -6,28 +6,42 @@
 #include <zephyr/logging/log.h>
 
 #define INA219_UPDATE_TIME_MS       (67)
+#define ADC_UPDATE_TIME_MS          (15)
 #define SENSOR_READ_STACK_SIZE      (640)
-
 
 LOG_MODULE_REGISTER(telemetry);
 
 static void ina_task(void *, void *, void *);
+
 K_THREAD_DEFINE(ina_thread, SENSOR_READ_STACK_SIZE, ina_task, NULL, NULL, NULL, K_PRIO_PREEMPT(10), 0, 1000);
+
+static void adc_task(void *, void *, void *);
+
+K_THREAD_DEFINE(adc_thread, SENSOR_READ_STACK_SIZE, adc_task, NULL, NULL, NULL, K_PRIO_PREEMPT(10), 0, 1000);
 
 extern struct k_msgq ina_processing_queue;
 
-static void adc_task(void *, void *, void *);
-K_THREAD_DEFINE(adc_thread, SENSOR_READ_STACK_SIZE, adc_task, NULL, NULL, NULL, K_PRIO_PREEMPT(10), 0, 1000);
+K_TIMER_DEFINE(ina_task_timer, NULL, NULL);
+K_TIMER_DEFINE(adc_task_timer, NULL, NULL);
 
-static void init_ina_task(const struct device *sensors[3], bool ina_device_found[3]) {
+static bool init_ina_task(const struct device *sensors[3], bool ina_device_found[3]) {
     const char *sensor_names[] = {"Battery", "3v3", "5v0"};
+    int error_count = 0;
 
     for (int i = 0; i < 3; i++) {
         ina_device_found[i] = l_check_device(sensors[i]) == 0;
         if (!ina_device_found[i]) {
             LOG_ERR("INA219 %s sensor not found", sensor_names[i]);
+            error_count++;
         }
     }
+
+    if (error_count == 3) {
+        return false;
+    }
+
+    k_timer_start(&ina_task_timer, K_MSEC(INA219_UPDATE_TIME_MS), K_MSEC(INA219_UPDATE_TIME_MS));
+    return true;
 }
 
 static bool init_adc_task(const struct adc_dt_spec *p_vin_sense_adc, struct adc_sequence *p_vin_sense_sequence) {
@@ -35,6 +49,8 @@ static bool init_adc_task(const struct adc_dt_spec *p_vin_sense_adc, struct adc_
 
     if (!adc_ready) {
         LOG_ERR("ADC channel %d is not ready", p_vin_sense_adc->channel_id);
+    } else {
+        k_timer_start(&ina_task_timer, K_MSEC(ADC_UPDATE_TIME_MS), K_MSEC(ADC_UPDATE_TIME_MS));
     }
 
     return adc_ready;
@@ -50,9 +66,13 @@ static void ina_task(void *, void *, void *) {
     };
 
     bool ina_device_found[3] = {false};
-    init_ina_task(sensors, ina_device_found);
+    if (!init_ina_task(sensors, ina_device_found)) {
+        return;
+    }
 
     while (true) {
+        k_timer_status_sync(&ina_task_timer);
+
         l_update_sensors_safe(sensors, 3, ina_device_found);
         sensor_telemetry.timestamp = k_uptime_get_32();
 
@@ -62,12 +82,6 @@ static void ina_task(void *, void *, void *) {
 
         if (k_msgq_put(&ina_processing_queue, &sensor_telemetry, K_NO_WAIT)) {
             LOG_ERR("Failed to put data into INA219 processing queue");
-        }
-
-        // Wait some time for sensor to get new values (15 Hz -> 66.67 ms)
-        uint32_t time_to_wait = INA219_UPDATE_TIME_MS - (k_uptime_get_32() - sensor_telemetry.timestamp);
-        if (time_to_wait > 0) {
-            k_sleep(K_MSEC(time_to_wait));
         }
     }
 }
@@ -89,6 +103,8 @@ static void adc_task(void *, void *, void *) {
         return;
     }
     while (true) {
+        k_timer_status_sync(&adc_task_timer);
+
         if (0 <= l_read_adc_mv(&vin_sense_adc, &vin_sense_sequence, (int32_t *) &vin_adc_data_mv)) {
             LOG_ERR("Failed to read ADC value from %d", vin_sense_adc.channel_id);
         }
