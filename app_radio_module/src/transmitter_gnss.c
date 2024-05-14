64e798dbbeea8cb420776bd91024e9685529f321
Aaron <aarchan108@gmail.com>
2024-05-14 09:09:19 -0700

    Move non-debug declarations out of #defined area

--- app_radio_module/src/transmitter_gnss.c
+++ app_radio_module/src/transmitter_gnss.c
@@ -11,15 +11,27 @@
 #include <zephyr/kernel/thread.h>
 #include <zephyr/logging/log.h>
 
+#define GNSS_TX_QUEUE_SIZE 8
+#define GNSS_TASK_STACK_SIZE 1024
+
 LOG_MODULE_REGISTER(transmitter_gnss);
+
+// Forward Declaration
+static void gnss_tx_on_expire(struct k_timer* timer_id);
+static void gnss_data_cb(const struct device* dev, const struct gnss_data* data);
+static void gnss_log_task(void);
+
+// Message Queues
+K_MSGQ_DEFINE(gnss_tx_queue, sizeof(l_gnss_data_t), GNSS_TX_QUEUE_SIZE, 1);
+K_MSGQ_DEFINE(gnss_log_queue, sizeof(l_gnss_data_t), CONFIG_LORA_TX_QUEUE_SIZE, 1);
+
+// Threads
+K_THREAD_DEFINE(gnss_log_thread, GNSS_TASK_STACK_SIZE, gnss_log_task, NULL, NULL, NULL, K_PRIO_PREEMPT(20), 0, 1000);
 
 // Flags
 static bool ready_to_tx = false;
 bool logging_enabled = false;
 
-// Forward Declaration
-static void gnss_data_cb(const struct device* dev, const struct gnss_data* data);
-static void gnss_log_task(void);
 
 // Callbacks
 GNSS_DATA_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(gnss)), gnss_data_cb);
@@ -27,44 +39,11 @@ GNSS_DATA_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(gnss)), gnss_data_cb);
 
 // Timers
 struct k_timer gnss_tx_timer;
+K_TIMER_DEFINE(gnss_tx_timer, gnss_tx_on_expire, NULL);
 
 // External Variables
 extern struct k_msgq lora_tx_queue;
 float gnss_altitude;
-
-static void gnss_tx_on_expire(struct k_timer* timer_id); // Forward Declaration
-K_TIMER_DEFINE(gnss_tx_timer, gnss_tx_on_expire, NULL);
-
-#ifdef CONFIG_DEBUG
-
-#define GNSS_TX_QUEUE_SIZE 8
-#define GNSS_TASK_STACK_SIZE 1024
-
-static void gnss_debug_task(void);
-
-K_MSGQ_DEFINE(gnss_tx_queue, sizeof(l_gnss_data_t), GNSS_TX_QUEUE_SIZE, 1);
-K_THREAD_DEFINE(gnss_udp_tx, GNSS_TASK_STACK_SIZE, gnss_debug_task, NULL, NULL, NULL, K_PRIO_PREEMPT(25), 0, 1000);
-
-K_MSGQ_DEFINE(gnss_log_queue, sizeof(l_gnss_data_t), CONFIG_LORA_TX_QUEUE_SIZE, 1);
-K_THREAD_DEFINE(gnss_log_thread, GNSS_TASK_STACK_SIZE, gnss_log_task, NULL, NULL, NULL, K_PRIO_PREEMPT(20), 0, 1000);
-
-
-static void gnss_debug_task(void) {
-    const uint16_t gnss_port = RADIO_MODULE_BASE_PORT + RADIO_MODULE_GNSS_DATA_PORT;
-    int sock = l_init_udp_socket(RADIO_MODULE_IP_ADDR, gnss_port);
-    if (sock < 0) {
-        LOG_ERR("Failed to initialize UDP socket for GNSS debug task");
-        return;
-    }
-
-    while (1) {
-        l_gnss_data_t gnss_data = {0};
-        k_msgq_get(&gnss_tx_queue, &gnss_data, K_FOREVER);
-        l_send_udp_broadcast(sock, (uint8_t*) &gnss_data, sizeof(l_gnss_data_t), gnss_port);
-    }
-}
-
-#endif
 
 void config_gnss_tx_time(k_timeout_t interval) { k_timer_start(&gnss_tx_timer, interval, interval); }
 
@@ -109,3 +88,24 @@ static void gnss_log_task(void) {
 
     }
 }
+
+#ifdef CONFIG_DEBUG
+static void gnss_debug_task(void);
+K_THREAD_DEFINE(gnss_udp_tx, GNSS_TASK_STACK_SIZE, gnss_debug_task, NULL, NULL, NULL, K_PRIO_PREEMPT(25), 0, 1000);
+
+static void gnss_debug_task(void) {
+    const uint16_t gnss_port = RADIO_MODULE_BASE_PORT + RADIO_MODULE_GNSS_DATA_PORT;
+    int sock = l_init_udp_socket(RADIO_MODULE_IP_ADDR, gnss_port);
+    if (sock < 0) {
+        LOG_ERR("Failed to initialize UDP socket for GNSS debug task");
+        return;
+    }
+
+    while (1) {
+        l_gnss_data_t gnss_data = {0};
+        k_msgq_get(&gnss_tx_queue, &gnss_data, K_FOREVER);
+        l_send_udp_broadcast(sock, (uint8_t*) &gnss_data, sizeof(l_gnss_data_t), gnss_port);
+    }
+}
+
+#endif
