91925e3262fe7a78bb729a79e89661d38285f894
Aaron <aarchan108@gmail.com>
2024-05-18 02:12:58 -0700

    Bunp up main stack size for POTATO and call boot count check function

--- app_potato/src/main.c
+++ app_potato/src/main.c
@@ -1,9 +1,9 @@
-#include <app_version.h>
 #include <launch_core/dev/dev_common.h>
 #include <launch_core/dev/uart.h>
 #include <launch_core/extension_boards.h>
 #include <launch_core/net/net_common.h>
 #include <launch_core/net/udp.h>
+#include <launch_core/os/fs.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
@@ -92,6 +92,7 @@ static int init(void) {
 
 int main() {
     init();
+    l_fs_boot_count_check();
 
     return 0;
 }
