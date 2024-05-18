756c57b22d705b9e01aa9185113b40005e6d7756
Aaron <aarchan108@gmail.com>
2024-05-18 02:47:26 -0700

    Stub state machine code

--- app_sensor_module/src/main.c
+++ app_sensor_module/src/main.c
@@ -13,9 +13,68 @@
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_SENSOR_MODULE_LOG_LEVEL);
 
+#define DEFINE_STATE_FUNCTIONS(state_name)                                                                             \
+static void state_name##_state_entry(void *);                                                                      \
+static void state_name##_state_run(void *);                                                                        \
+static void state_name##_state_exit(void *) {}
+
+DEFINE_STATE_FUNCTIONS(pad);
+DEFINE_STATE_FUNCTIONS(pre_main);
+DEFINE_STATE_FUNCTIONS(post_main);
+DEFINE_STATE_FUNCTIONS(landing);
+
+static const struct smf_state states[] = {
+    [PAD_STATE] = SMF_CREATE_STATE(pad_state_entry, pad_state_run, pad_state_exit),
+    [PRE_MAIN_STATE] = SMF_CREATE_STATE(pre_main_state_entry, pre_main_state_run, pre_main_state_exit),
+    [POST_MAIN_STATE] = SMF_CREATE_STATE(post_main_state_entry, post_main_state_run, post_main_state_exit),
+    [LANDING_STATE] = SMF_CREATE_STATE(landing_state_entry, landing_state_run, landing_state_exit),
+};
+
+
 struct s_object {
     struct smf_ctx ctx;
 } state_obj;
+
+static void pad_state_entry(void*) {
+    LOG_INF("Entering pad state");
+}
+
+static void pad_state_run(void*) {
+    while (true) {
+
+    }
+}
+
+static void pre_main_state_entry(void*) {
+    LOG_INF("Entering pre_main state");
+}
+
+static void pre_main_state_run(void*) {
+    while (true) {
+
+    }
+}
+
+static void post_main_state_entry(void*) {
+    LOG_INF("Entering post_main state");
+}
+
+static void post_main_state_run(void*) {
+    while (true) {
+
+    }
+}
+
+
+static void landing_state_entry(void*) {
+    LOG_INF("Entering landing state");
+}
+
+static void landing_state_run(void*) {
+    while (true) {
+
+    }
+}
 
 static void init() {
     init_networking();
@@ -28,7 +87,7 @@ int main() {
     static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
     static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 
-
+    init();
 
     while (true) {
         gpio_pin_toggle_dt(&led0);
--- app_sensor_module/src/sensor_module.h
+++ app_sensor_module/src/sensor_module.h
@@ -5,6 +5,13 @@
 
 #define SENSOR_MODULE_IP_ADDR BACKPLANE_IP(SENSOR_MODULE_ID, 2, 1)
 
+typedef enum {
+    PAD_STATE = 0,
+    PRE_MAIN_STATE,
+    POST_MAIN_STATE,
+    LANDING_STATE
+} FLIGHT_STATES;
+
 int init_networking(void);
 
 #endif //SENSOR_MODULE_H
