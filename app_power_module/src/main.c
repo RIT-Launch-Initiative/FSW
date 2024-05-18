1c704ca0e33339c852f40049c40a7a8c42c16894
Aaron <aarchan108@gmail.com>
2024-05-18 01:10:24 -0700

    Update boot count function to not have boot count always start at 0, since opening file will return 0, regardless of creating/opening

--- app_power_module/src/main.c
+++ app_power_module/src/main.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <launch_core/os/fs.h>
+
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
@@ -11,5 +13,8 @@
 LOG_MODULE_REGISTER(main, CONFIG_APP_POWER_MODULE_LOG_LEVEL);
 
 int main(void) {
+    LOG_INF("Boot count: %d", l_fs_boot_count_check());
+
+
     return 0;
 }
