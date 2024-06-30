94d4a979176e2c50edc3c815fa6b9e4f380a2690
cowsed <richie.sommers@outlook.com>
2024-06-30 13:50:23 -0400

    OpenRocket event thread

--- app/samples/openrocket_sensors/src/main.c
+++ app/samples/openrocket_sensors/src/main.c
@@ -4,7 +4,7 @@ int or_data_interpolator();
 
 int main() {
     while (1) {
-        printk("asdadsa %d\n", or_data_interpolator());
+        // printk("asdadsa %d\n", or_data_interpolator());
         k_msleep(1000);
     }
     return 1;
