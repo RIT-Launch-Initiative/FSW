ba2782c7838fac4325a89ebd506b0f591089fc30
Aaron <30483634+AarC10@users.noreply.github.com>
2024-07-01 19:19:36 -0700

    Standardized RTOS Task Creation (#128)

---
+++ include/f_core/os/c_tenant.h
@@ -0,0 +1,41 @@
+/*
+* Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef C_TENANT_H
+#define C_TENANT_H
+
+class CTenant {
+public:
+    /**
+     * Constructor
+     * @param name Name of the tenant
+     */
+    explicit CTenant(const char *name);
+
+    /**
+     * Destructor
+     */
+    virtual ~CTenant() = default;
+
+    /**
+     * Initialize the tenant
+     */
+    virtual void Startup() {};
+
+    /**
+     * Perform any other initializations after all tenants in the same task have been initialized
+     */
+    virtual void PostStartup() {};
+
+    /**
+     * Run the tenant
+     */
+    virtual void Run() = 0;
+protected:
+    const char *name;
+
+};
+
+#endif //C_TENANT_H
