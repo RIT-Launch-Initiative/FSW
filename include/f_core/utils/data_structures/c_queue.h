37f6d349aa243d6cd30b5f85fb78fe1b3d2f3478
Aaron Chan <aarchan108@gmail.com>
2024-07-01 20:04:57 -0700

    CQueue implementation

--- include/f_core/utils/data_structures/c_queue.h
+++ include/f_core/utils/data_structures/c_queue.h
@@ -1,19 +1,32 @@
 #ifndef C_QUEUE_H
 #define C_QUEUE_H
+
 #include <zephyr/kernel.h>
 
+template<typename T>
 class CQueue {
 public:
+    CQueue(const k_queue queue) : queue(queue) {}
 
-    void push(const T& data) {
+    void Push(const T& data) {
         k_queue_append(&queue, &data);
     }
 
-    void pop(T& data) {
-        k_queue_get(&queue, &data, K_FOREVER);
+    void Pop(T& data, k_timeout_t timeout = K_NO_WAIT) {
+        k_queue_get(&queue, &data, timeout);
     }
 
+    void PeekHead(T& data, k_timeout_t timeout = K_NO_WAIT) {
+        k_queue_peek(&queue, &data, timeout);
+    }
 
+    void PeekTail(T& data, k_timeout_t timeout = K_NO_WAIT) {
+        k_queue_peek_tail(&queue, &data, timeout);
+    }
+
+    bool IsEmpty() {
+        return k_queue_is_empty(const_cast<k_queue*>(&queue));
+    }
 
 private:
     const k_queue queue;
