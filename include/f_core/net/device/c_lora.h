0f7b980161b654ab988635d297feb065e2a9432c
Aaron Chan <aarchan108@gmail.com>
2024-07-13 19:11:34 -0700

    Set config from constructor as an option for lora

--- include/f_core/net/device/c_lora.h
+++ include/f_core/net/device/c_lora.h
@@ -2,13 +2,12 @@
 #define C_LORA_H
 
 #include <f_core/net/c_transciever.h>
+#include <zephyr/drivers/lora.h>
 
-struct device;
-
-class CLora : public CTransceiver
-{
+class CLora : public CTransceiver {
 public:
     CLora(const device* lora_dev);
+    CLora(const device* lora_dev, const lora_modem_config& config);
 
     int TransmitSynchronous(const void* data, size_t len);
 
@@ -25,6 +24,17 @@ protected:
 
 private:
     const device* lora_dev;
+    lora_modem_config config = {
+        .frequency = 915000000,
+        .bandwidth = BW_125_KHZ,
+        .datarate = SF_12,
+        .coding_rate = CR_4_5,
+        .preamble_len = 8,
+        .tx_power = 13,
+        .tx = false,
+        .iq_inverted = false,
+        .public_network = false,
+    };
 };
 
 
--- lib/f_core/net/device/c_lora.cpp
+++ lib/f_core/net/device/c_lora.cpp
@@ -1,10 +1,17 @@
+#include <string.h>
 #include <f_core/net/device/c_lora.h>
 
 
 CLora::CLora(const device* lora_dev) : lora_dev(lora_dev) {}
 
+CLora::CLora(const device* lora_dev, const lora_modem_config& config) : lora_dev(lora_dev) {
+    memcpy(&this->config, &config, sizeof(lora_modem_config));
+}
+
+
 
 int CLora::TransmitSynchronous(const void* data, size_t len) {
+
     return 0;
 }
 
