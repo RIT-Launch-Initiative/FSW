feb06d7dec690b774f8fbd8045d379282b40260a
Aaron Chan <aarchan108@gmail.com>
2024-07-28 14:05:02 -0700

    Update prj.conf for lora to link properly. Require a receive and transmit to re-run corresponding receive and transmit

--- app/samples/lora_back_and_forth/src/main.cpp
+++ app/samples/lora_back_and_forth/src/main.cpp
@@ -12,24 +12,44 @@
 
 // Zephyr Includes
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/lora.h>
 
 LOG_MODULE_REGISTER(main);
+
+
+bool sentFinished = true;
+bool receiveFinished = true;
 
 int main() {
     static constexpr std::string payload = "Launch!";
 
-    // Weird linker error with device macros + LoRa when calling this ctor
-    CLora lora(*device_get_binding("lora0"));
+    gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+    const device* dev = DEVICE_DT_GET(DT_ALIAS(lora));
+    CLora lora(*dev);
 
     while (true) {
+        gpio_pin_toggle_dt(&led0);
+
+        if (receiveFinished) {
         lora.ReceiveAsynchronous(
             [](const device*, uint8_t* data, uint16_t size, int16_t rssi, int8_t snr) {
                 LOG_INF("Async Received: %s\tRSSI: %d\t SNR:%d\n", data, rssi, snr);
+                receiveFinished = true;
             }
-        );
+            );
 
-        lora.TransmitAsynchronous(payload.c_str(), payload.size(), nullptr);
+            receiveFinished = false;
+        }
+
+        if (sentFinished) {
+            lora.TransmitAsynchronous(payload.c_str(), payload.size(), nullptr);
+            LOG_INF("Async Sent: %s\n", payload.c_str());
+            sentFinished = false;
+        }
+
+        k_sleep(K_SECONDS(1));
     }
 
     return 0;
