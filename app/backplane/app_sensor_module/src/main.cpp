1b0c5d5845498ce942d1c95c19f5857f17ff1bc7
Aaron Chan <aarchan108@gmail.com>
2024-07-29 23:19:38 -0700

    native_sim setup

--- app/backplane/app_sensor_module/src/main.cpp
+++ app/backplane/app_sensor_module/src/main.cpp
@@ -7,13 +7,28 @@
 #include "c_sensing_tenant.h"
 #include "c_broadcast_tenant.h"
 
-int main() {
-    static CBroadcastTenant broadcastTenant = CBroadcastTenant("Broadcast Tenant");
+#include <f_core/os/c_task.h>
+#include <f_core/os/n_rtos.h>
 
-    for (int i = 0; i < 100; i++) {
-        broadcastTenant.Run();
-        k_msleep(1000);
-    }
+int main() {
+    // Networking Subsystem
+    static CBroadcastTenant broadcastTenant = CBroadcastTenant("Broadcast Tenant");
+    static CTask networkTask("Networking Task", 15, 512, 0);
+    networkTask.AddTenant(broadcastTenant);
+
+    // Sensing Subsystem
+    static CSensingTenant sensingTenant = CSensingTenant("Sensing Tenant");
+    static CTask sensingTask("Sensing Task", 15, 512, 0);
+    sensingTask.AddTenant(sensingTenant);
+
+    // Add tasks and start RTOS
+    NRtos::AddTask(networkTask);
+    NRtos::StartRtos();
+
+#ifdef CONFIG_ARCH_POSIX
+    k_sleep(K_SECONDS(10));
+    NRtos::StopRtos();
+#endif
 
     return 0;
 }
