6c0f63a9cfab6c75cc665a49867663ddf63be244
Aaron <30483634+AarC10@users.noreply.github.com>
2024-07-29 21:54:15 -0700

    F-Core Starter Networking Library  (#158)

---
+++ lib/f_core/net/transport/c_udp_socket.cpp
@@ -0,0 +1,90 @@
+#include <f_core/net/transport/c_udp_socket.h>
+#include <f_core/net/network/c_ipv4.h>
+
+#include <zephyr/net/socket.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(CUdpSocket);
+
+CUdpSocket::CUdpSocket(CIPv4& ip, uint16_t srcPort, uint16_t dstPort) : dstPort(dstPort) {
+    if (!ip.IsInitialized()) {
+        // Guarantee IPv4 is initialized
+        LOG_ERR("Failed to initialize IPv4 address %s", ip.GetIp());
+        return;
+    }
+
+    sock = zsock_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) {
+        return;
+    }
+
+    sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(srcPort),
+        .sin_addr = ip.GetAddr()
+    };
+
+    if (zsock_bind(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
+        LOG_ERR("Failed to bind socket.");
+        zsock_close(sock);
+    }
+}
+
+CUdpSocket::~CUdpSocket() {
+    zsock_close(sock);
+}
+
+int CUdpSocket::TransmitSynchronous(const void* data, size_t len) {
+    static const sockaddr_in addr{
+        .sin_family = AF_INET,
+        .sin_port = htons(dstPort),
+    };
+
+    z_impl_net_addr_pton(AF_INET, "255.255.255.255", const_cast<in_addr*>(&addr.sin_addr));
+
+    int ret = zsock_sendto(sock, data, len, 0, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr));
+    if (ret < 0) {
+        LOG_ERR("Failed to send broadcast message (%d)", ret);
+    }
+
+    return ret;
+}
+
+int CUdpSocket::ReceiveSynchronous(void* data, size_t len) {
+    return zsock_recvfrom(sock, data, len, 0, nullptr, nullptr);
+}
+
+int CUdpSocket::TransmitAsynchronous(const void* data, size_t len) {
+    static const sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(dstPort),
+    };
+    z_impl_net_addr_pton(AF_INET, "255.255.255.255", const_cast<in_addr*>(&addr.sin_addr));
+
+    int ret = zsock_sendto(sock, data, len, 0, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr));
+    if (ret < 0 && errno != EWOULDBLOCK && errno != EAGAIN) {
+        LOG_ERR("Failed to send async message (%d)", ret);
+    }
+
+    return ret;
+}
+
+int CUdpSocket::ReceiveAsynchronous(void* data, size_t len) {
+    static zsock_pollfd fds{
+        .fd = sock,
+        .events = ZSOCK_POLLIN
+    };
+
+    int ret = zsock_poll(&fds, 1, 0);
+    if (ret < 0) {
+        LOG_ERR("Polling error (%d)", ret);
+        return ret;
+    }
+
+    return zsock_recvfrom(sock, data, len, 0, nullptr, nullptr);
+}
+
+int CUdpSocket::SetRxTimeout(int timeout) {
+    return zsock_setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+}
