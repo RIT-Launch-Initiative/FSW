e12badd33530a4a8ae08a09083316f6fe62b80d5
Aaron <30483634+AarC10@users.noreply.github.com>
2024-08-03 18:27:11 -0700

    Messaging Library (k_msgq Implementation) (#161)

---
+++ include/f_core/messaging/c_message_port.h
@@ -0,0 +1,40 @@
+#ifndef CMESSAGEPORT_H
+#define CMESSAGEPORT_H
+
+#include <zephyr/kernel.h>
+
+template <typename T>
+class CMessagePort {
+public:
+    using MessageType = T;
+
+    /**
+     * Constructor
+     */
+    CMessagePort() = default;
+
+    /**
+     * Destructor
+     */
+    virtual ~CMessagePort() = default;
+
+    /**
+     * Send a message
+     * @param message Message to send
+     * @param timeout Time before sending times out
+     * @return Zephyr status code
+     */
+    virtual int Send(const T &message, const k_timeout_t timeout = K_FOREVER) = 0;
+
+    /**
+     * Receive a message
+     * @param message Message to receive
+     * @param timeout Time before receiving times out
+     * @return Zephyr status code
+     */
+    virtual int Receive(T& message, const k_timeout_t timeout = K_FOREVER) = 0;
+};
+
+
+
+#endif //CMESSAGEPORT_H
