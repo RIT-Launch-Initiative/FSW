b37989d0ab8db4177e4cd3f7f965f0f741ce5a55
Aaron Chan <aarchan108@gmail.com>
2024-10-16 22:27:24 -0400

    More documentation and move ctor to src

--- include/f_core/net/device/c_rs485.h
+++ include/f_core/net/device/c_rs485.h
@@ -1,28 +1,20 @@
 #ifndef C_RS485_H
 #define C_RS485_H
 
+// F-Core Includes
 #include <f_core/net/c_transciever.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/uart.h>
 
+// Zephyr Includes
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/device.h>
 
-class CRs485 : public CTransceiver
-{
+class CRs485 : public CTransceiver {
 public:
-    CRs485(const device &uart, const gpio_dt_spec &rs485_enable, void (*uartIrqUserDataCallback)(const device *dev, void *user_data) = nullptr)
-        : uart(uart), rs485_enable(rs485_enable), irqEnabled(uartIrqUserDataCallback != nullptr)
-    {
-      if (uartIrqUserDataCallback != nullptr) {
-        uart_irq_callback_set(&uart, uartIrqUserDataCallback);
-          uart_irq_rx_enable(&uart);
-          uart_irq_tx_enable(&uart);
-      } else {
-          uart_irq_rx_disable(&uart);
-          uart_irq_tx_disable(&uart);
 
-      }
-    };
+    /**
+    * Constructor
+    */
+    CRs485(const device &uart, const gpio_dt_spec &rs485_enable, void (*uartIrqUserDataCallback)(const device *dev, void *user_data) = nullptr);
 
     int TransmitSynchronous(const void* data, size_t len);
 
@@ -54,8 +46,10 @@ private:
   const gpio_dt_spec &rs485_enable;
   const bool irqEnabled;
 
-  bool setTxRx(const bool isTransmit) const
-  {
+  /**
+   * Set the RS485 transceiver to transmit or receive mode
+   */
+  bool setTxRx(const bool isTransmit) const {
     return gpio_pin_set_dt(&rs485_enable, isTransmit) == 0;
   }
 };
--- lib/f_core/net/device/c_rs485.cpp
+++ lib/f_core/net/device/c_rs485.cpp
@@ -1,8 +1,27 @@
+// Self Include
 #include <f_core/net/device/c_rs485.h>
+
+// Zephyr Includes
+#include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+
 LOG_MODULE_REGISTER(CRs485);
+
+CRs485::CRs485(const device& uart, const gpio_dt_spec& rs485_enable,
+               void (*uartIrqUserDataCallback)(const device* dev, void* user_data)) : uart(uart),
+    rs485_enable(rs485_enable), irqEnabled(uartIrqUserDataCallback != nullptr) {
+    if (uartIrqUserDataCallback != nullptr) {
+        uart_irq_callback_set(&uart, uartIrqUserDataCallback);
+        uart_irq_rx_enable(&uart);
+        uart_irq_tx_enable(&uart);
+    } else {
+        uart_irq_rx_disable(&uart);
+        uart_irq_tx_disable(&uart);
+    }
+}
+
 
 int CRs485::TransmitSynchronous(const void* data, size_t len) {
     if (setTxRx(enableTx) == false) {
@@ -16,7 +35,6 @@ int CRs485::TransmitSynchronous(const void* data, size_t len) {
     }
     return 0;
 }
-
 
 
 int CRs485::ReceiveSynchronous(void* data, size_t len) {
@@ -65,5 +83,7 @@ int CRs485::ReceiveAsynchronous(void* data, size_t len) {
 
 
 int CRs485::SetRxTimeout(int timeout) {
-    return -1;
+    // TODO: Should use a k_timer to implement this in a future PR
+    // Didn't see any documentation on timeouts with uart functions
+    return 0;
 }
