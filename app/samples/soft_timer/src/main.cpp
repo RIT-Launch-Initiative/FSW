8822c8a091ab06e8b2c44694ef5e62e38a6c4b0f
Aaron Chan <aarchan108@gmail.com>
2024-10-26 11:45:33 -0400

    Soft Timer project stubbed

---
+++ app/samples/soft_timer/src/main.cpp
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/kernel.h>
+#include <f_core/os/n_rtos.h>
+#include <f_core/os/c_task.h>
+#include "c_publisher.h"
+#include "c_receiver.h"
+#include "message.h"
+
+#include <f_core/messaging/c_msgq_message_port.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+int main() {
+
+
+    return 0;
+}
