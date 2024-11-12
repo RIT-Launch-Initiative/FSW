a2fef7eff31fdfedafe651d915529cd959f55051
Aaron Chan <aarchan108@gmail.com>
2024-11-11 19:10:57 -0500

    Stub filesystem sample code

---
+++ app/samples/filesystem/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/kernel.h>
+#include <f_core/os/n_rtos.h>
+#include <f_core/utils/c_soft_timer.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+
+int main() {
+
+    return 0;
+}
