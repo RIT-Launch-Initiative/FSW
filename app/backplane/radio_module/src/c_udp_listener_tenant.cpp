5c4641f20b14bafc9bf4049d03767b4e86ea3bfb
Aaron Chan <aarchan108@gmail.com>
2024-11-17 01:48:25 -0500

    Add error handling for receive and fix starving receive task

--- app/backplane/radio_module/src/c_udp_listener_tenant.cpp
+++ app/backplane/radio_module/src/c_udp_listener_tenant.cpp
@@ -13,12 +13,15 @@ void CUdpListenerTenant::PostStartup() {
 }
 
 void CUdpListenerTenant::Run() {
-    while (true) {
-        NRadioModuleTypes::RadioBroadcastData radioBroadcastData{0};
-
-        radioBroadcastData.port = listenPort;
-        radioBroadcastData.size = udp.ReceiveAsynchronous(&radioBroadcastData.data, sizeof(radioBroadcastData.data));
-
-        loraTransmitPort.Send(radioBroadcastData);
+    NRadioModuleTypes::RadioBroadcastData radioBroadcastData{0};
+    // Note len argument is the size of the data buffer, not how much data to receive! rcvResult will contain the actual amount of data received or -1 on error
+    const int rcvResult = udp.ReceiveAsynchronous(&radioBroadcastData.data, sizeof(radioBroadcastData.data));
+    if (rcvResult < 0) {
+        LOG_ERR("Failed to receive broadcast message (%d)", result);
+        return;
     }
+
+    radioBroadcastData.port = listenPort;
+    radioBroadcastData.size = rcvResult;
+    loraTransmitPort.Send(radioBroadcastData);
 }
