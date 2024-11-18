f02d2ad15f3d362bbdd8726717812c81a498fb8e
Aaron Chan <aarchan108@gmail.com>
2024-11-18 04:05:22 -0500

    Working UDP send to toggle LEDs now need to test over LoRa too

--- app/backplane/sensor_module/include/c_remote_activation.h
+++ app/backplane/sensor_module/include/c_remote_activation.h
@@ -1,13 +1,15 @@
 #ifndef C_REMOTE_ACTIVATION_TENANT_H
 #define C_REMOTE_ACTIVATION_TENANT_H
 
+#include <f_core/net/network/c_ipv4.h>
+#include <f_core/net/transport/c_udp_socket.h>
 #include <f_core/os/c_tenant.h>
 #include <zephyr/drivers/gpio.h>
 
 class CRemoteActivationTenant : public CTenant {
 public:
     explicit CRemoteActivationTenant(const char* name, const gpio_dt_spec &pin)
-        : CTenant(name), pin(pin)
+        : CTenant(name), ip(CIPv4{"10.3.2.1"}), udp(CUdpSocket{ip, commandPort, commandPort}), pin(pin)
     {
     }
 
@@ -20,6 +22,10 @@ public:
     void Run() override;
 
 private:
+    static constexpr uint16_t commandPort = 13001;
+
+    CIPv4 ip;
+    CUdpSocket udp;
     const gpio_dt_spec &pin;
 };
 
--- app/backplane/sensor_module/src/c_remote_activation.cpp
+++ app/backplane/sensor_module/src/c_remote_activation.cpp
@@ -1,5 +1,6 @@
 #include "c_remote_activation.h"
 
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(CRemoteActivationTenant);
@@ -9,5 +10,16 @@ void CRemoteActivationTenant::Startup() {};
 void CRemoteActivationTenant::PostStartup() {};
 
 void CRemoteActivationTenant::Run() {
-    gpio_pin_toggle_dt(&pin);
+    uint8_t command = 0;
+    if (udp.ReceiveAsynchronous(&command, sizeof(command)) < 0) {
+        LOG_ERR("Failed to receive command");
+        return;
+    }
+
+    // Check bit 0 set
+    if (command & 0b1) {
+        gpio_pin_toggle_dt(&pin);
+    } else if (!(command & 0b1)) {
+        gpio_pin_set_dt(&pin, 0);
+    }
 }
