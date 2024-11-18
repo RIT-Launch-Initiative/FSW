ba717162eb6473d22dcc309b258a29b0c13ff469
Aaron Chan <aarchan108@gmail.com>
2024-11-18 05:27:05 -0500

    Modified lora_bcast to do remote activation

--- app/backplane/radio_module/src/main.cpp
+++ app/backplane/radio_module/src/main.cpp
@@ -16,12 +16,36 @@
 
 LOG_MODULE_REGISTER(main);
 
+static CUdpSocket udp(CIPv4{"10.2.1.1"}, 12000, 13001);
+
+static void receiver_cb(const struct device* lora_dev, uint8_t* payload, uint16_t len, int16_t rssi, int8_t snr) {
+    LOG_INF("Received %d bytes. RSSI: %d SNR: %d", len, rssi, snr);
+
+    if (len == 3) {
+        udp.SetDstPort(payload[1] << 8 | payload[0]);
+        udp.TransmitAsynchronous(&payload[2], 1);
+    }
+}
+
+
+
 int main() {
+    CLora cLora = CLora(*DEVICE_DT_GET(DT_ALIAS(lora)));
 #ifndef CONFIG_RADIO_MODULE_RECEIVER
     LOG_INF("Transmitter started");
+    lora_recv_async(DEVICE_DT_GET(DT_ALIAS(lora)), &receiver_cb);
+    while (true) {
+        k_msleep(100);
+    }
     static CRadioModule radioModule{};
 #else
     LOG_INF("Receiver started");
+    while (true) {
+        LOG_INF("Sending");
+        lora_send(DEVICE_DT_GET(DT_ALIAS(lora)), (uint8_t*)"Hello", 5);
+    }
+
+
     static CReceiverModule radioModule{};
 #endif
     k_msleep(2000);
--- app/samples/lora_bcast/src/main.cpp
+++ app/samples/lora_bcast/src/main.cpp
@@ -11,6 +11,8 @@
 #include <f_core/net/device/c_lora.h>
 
 // Zephyr Includes
+#include <f_core/net/transport/c_udp_socket.h>
+#include <f_core/net/network/c_ipv4.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
@@ -22,8 +24,10 @@ LOG_MODULE_REGISTER(main);
 static bool receiveFinished = true;
 #endif
 
+static CUdpSocket udp(CIPv4{"10.2.1.1"}, 12000, 12000);
 int main() {
     CLora lora(*DEVICE_DT_GET(DT_ALIAS(lora)));
+    std::string payload = {13001 >> 8, 13001 & 0xFF, 0b1};
 
     while (true) {
 #ifdef CONFIG_RECEIVER
@@ -33,15 +37,23 @@ int main() {
                 [](const device*, uint8_t* data, uint16_t size, int16_t rssi, int8_t snr) {
                     LOG_INF("Async Received: %s\tRSSI: %d\t SNR:%d", data, rssi, snr);
                     receiveFinished = true;
+
+                    if (size == 3) {
+                        udp.SetDstPort(data[0] << 8 | data[1]);
+                        LOG_INF("Sending %d to port: %d", data[2], data[0] << 8 | data[1]);
+                        udp.TransmitSynchronous(&data[2], 1);
+                    }
                 }
                 );
 
             receiveFinished = false;
         }
 #else
-        static constexpr std::string payload = "Launch!";
+        // std::string payload where first byte it the first half of 13001 and the second byte is the second half of 13001. The third byte is 0b1
         lora.TransmitSynchronous(payload.c_str(), payload.size());
         LOG_INF("Sent: %s", payload.c_str());
+
+        payload[2] ^= 0b1;
 #endif
         k_msleep(100);
     }
