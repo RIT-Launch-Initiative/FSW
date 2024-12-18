df3f93ab017d66dead0dfa8fe2914c90aeb5dc74
Aaron Chan <aarchan108@gmail.com>
2024-12-18 02:44:18 -0500

    Validate arguments function

--- tools/test_flight/main.py
+++ tools/test_flight/main.py
@@ -5,27 +5,7 @@ import subprocess
 from threading import Thread, Barrier
 
 
-def run_simulation(start_barrier, stop_barrier, binary, binary_args, output_folder):
-    """Run the simulation for the given binary"""
-    # Create the output folder if it doesn't exist
-    # Wait for all threads to be ready
-    start_barrier.wait()
-    print(f"Running simulation for {binary}")
-    stop_barrier.wait()
-
-def main():
-    """Parse CLI args and run appropriate functions for simulation"""
-    parser = argparse.ArgumentParser(description="RIT Launch Initiative's SIL tool for FSW")
-
-    # Either executable or build folder must be provided
-    parser.add_argument("-e", "--executable", help="Executable to run in the simulation")
-    parser.add_argument("-b", "--build_folder", help="Folder of executables to execute in the simulation")
-    parser.add_argument("-rt", "--real-time", help="Run the simulation in real-time", action="store_true", default=False)
-    parser.add_argument("-o", "--output", help="Outputs folder", default="tf_out")
-
-    args = parser.parse_args()
-
-    # TODO: Handle compiling (and dealing with sanitizers)
+def validate_arguments(args):
     if not args.executable and not args.build_folder:
         print("No executable or build folder provided")
         return
@@ -41,6 +21,35 @@ def main():
     if not os.path.exists(args.output):
         os.mkdir(args.output)
 
+
+def run_simulation(start_barrier, stop_barrier, binary, binary_args, output_folder):
+    """Run the simulation for the given binary"""
+    # Create the output folder if it doesn't exist
+    # Wait for all threads to be ready
+    start_barrier.wait()
+    print(f"Running simulation for {binary}")
+    stop_barrier.wait()
+
+
+def main():
+    """Parse CLI args and run appropriate functions for simulation"""
+    parser = argparse.ArgumentParser(description="RIT Launch Initiative's SIL tool for FSW")
+
+    # NOTE: Either executable or build folder must be provided
+    parser.add_argument("-e", "--executable", help="Executable to run in the simulation")
+    parser.add_argument("-b", "--build_folder", help="Folder of executables to execute in the simulation")
+    parser.add_argument("-o", "--output", help="Outputs folder", default="tf_out")
+
+    # Extra Configuration Fun
+    parser.add_argument("-rt", "--real-time", help="Run the simulation in real-time", action="store_true",
+                        default=False)
+
+    args = parser.parse_args()
+
+    # TODO: Handle compiling (and dealing with sanitizers)
+    if not validate_arguments(args):
+        return
+
     binaries = os.listdir(args.build_folder)
 
     start_barrier = Barrier(len(binaries) + 1)
@@ -52,6 +61,7 @@ def main():
     print("Simulation started")
     stop_barrier.wait()
     print("Simulation complete")
+
 
 if __name__ == "__main__":
     main()
