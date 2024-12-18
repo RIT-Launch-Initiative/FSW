9802a04691657d4a7a915ecdab12788a48a40ad5
Aaron Chan <aarchan108@gmail.com>
2024-12-18 00:59:34 -0500

    Stub a python file for test_flight

---
+++ tools/test_flight/main.py
@@ -0,0 +1,18 @@
+import argparse
+
+def compile_binaries():
+    pass
+
+def run_simulation():
+    pass
+
+def execute():
+    pass
+
+def main():
+    """Parse CLI args and run appropriate functions for simulation"""
+    parser = argparse.ArgumentParser(description="RIT Launch Initiative's SIL tool for FSW")
+    pass
+
+if __name__ == "__main__":
+    main()
