94ddbd68e2a877ba7eac21c52ae2414133d27a07
Aaron Chan <aarchan108@gmail.com>
2024-12-26 18:46:38 -0500

    Get template path and basic yaml parsing

--- tools/autocoders/ac_types.py
+++ tools/autocoders/ac_types.py
@@ -1,7 +1,25 @@
 import argparse
+import yaml
+import os
 
 def main():
-    pass
+    template_path = (os.getcwd() + __file__).split("ac_types.py")[0] + "templates/ac_types.h"
+    print(template_path)
+
+    parser = argparse.ArgumentParser(description='Generate C++ types from a list of types.')
+    parser.add_argument("-f", "--files", nargs='+', help="Files to generate types from")
+
+    args = parser.parse_args()
+
+    for file in args.files:
+        with open(file, 'r') as stream:
+            try:
+                data = yaml.safe_load(stream)
+                for key, value in data.items():
+                    print(f"{key}: {value}")
+            except yaml.YAMLError as exc:
+                print(exc)
+
 
 if __name__ == '__main__':
     main()
