a11b3bc1bc5535787fc51e62d97f3c5f091a9ea2
Aaron Chan <aarchan108@gmail.com>
2024-12-27 00:38:35 -0500

    CMake managed to generate the types file but not the network definitions file

--- cmake/Autocoders.cmake
+++ cmake/Autocoders.cmake
@@ -4,32 +4,36 @@ function(AutocodeNetworkDefinitions)
     set(OUTPUT_DIR ${FSW_ROOT}/zephyr/include/generated/FSW)
     set(OUTPUT ${FSW_ROOT}/network_defs.h)
 
+    file(MAKE_DIRECTORY ${OUTPUT_DIR})
+
     add_custom_command(
         OUTPUT ${OUTPUT}
-        COMMAND python3 ${AC_SCRIPT} -f ${INPUTS} -o ${OUTPUT_DIR}
+        COMMAND python3 ${AC_SCRIPT} -f ${INPUTS} -o ${OUTPUT}
         DEPENDS ${AC_SCRIPT} ${INPUTS}
         COMMENT "Generating network definitions from ${INPUTS}"
     )
 
     target_include_directories(app PRIVATE ${OUTPUT_DIR})
 
-    add_custom_target(autocode_network_defs DEPENDS ${OUTPUT})
+    add_custom_target(ac_net_defs DEPENDS ${OUTPUT})
 endfunction()
 
 function(AutocodeTypes)
-    set(AC_SCRIPT ${CMAKE_SOURCE_DIR}/tools/autocoders/ac_types.py)
-    set(INPUTS ${CMAKE_SOURCE_DIR}/data/autocoder_inputs/types.yaml)
+    set(AC_SCRIPT ${FSW_ROOT}/tools/autocoders/ac_types.py)
+    set(INPUTS ${FSW_ROOT}/data/autocoder_inputs/types.yaml)
     set(OUTPUT_DIR ${CMAKE_BINARY_DIR}/zephyr/include/generated/FSW)
     set(OUTPUT ${OUTPUT_DIR}/types.h)
 
+    file(MAKE_DIRECTORY ${OUTPUT_DIR})
+
     add_custom_command(
         OUTPUT ${OUTPUT}
-        COMMAND python3 ${AC_SCRIPT} -f ${INPUTS} -o ${OUTPUT_DIR}
+        COMMAND python3 ${AC_SCRIPT} -f ${INPUTS} -o ${OUTPUT}
         DEPENDS ${AC_SCRIPT} ${INPUTS}
         COMMENT "Generating types from ${INPUTS}"
     )
 
     target_include_directories(app PRIVATE ${OUTPUT_DIR})
 
-    add_custom_target(autocode_types DEPENDS ${OUTPUT})
+    add_custom_target(ac_types DEPENDS ${OUTPUT})
 endfunction()
