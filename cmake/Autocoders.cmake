f0741942dff4b092642c7cb7bfed7aef2f735711
Aaron Chan <aarchan108@gmail.com>
2024-12-27 00:55:08 -0500

    Fix n_ac_network_defs.h not being generated to the right place

--- cmake/Autocoders.cmake
+++ cmake/Autocoders.cmake
@@ -1,8 +1,8 @@
 function(AutocodeNetworkDefinitions)
     set(AC_SCRIPT ${FSW_ROOT}/tools/autocoders/ac_network_defs.py)
     set(INPUTS ${FSW_ROOT}/data/autocoder_inputs/network_defs.yaml)
-    set(OUTPUT_DIR ${FSW_ROOT}/zephyr/include/generated/FSW)
-    set(OUTPUT ${FSW_ROOT}/network_defs.h)
+    set(OUTPUT_DIR ${CMAKE_BINARY_DIR}/zephyr/include/generated/FSW)
+    set(OUTPUT ${OUTPUT_DIR}/n_ac_network_defs.h)
 
     file(MAKE_DIRECTORY ${OUTPUT_DIR})
 
@@ -22,7 +22,7 @@ function(AutocodeTypes)
     set(AC_SCRIPT ${FSW_ROOT}/tools/autocoders/ac_types.py)
     set(INPUTS ${FSW_ROOT}/data/autocoder_inputs/types.yaml)
     set(OUTPUT_DIR ${CMAKE_BINARY_DIR}/zephyr/include/generated/FSW)
-    set(OUTPUT ${OUTPUT_DIR}/types.h)
+    set(OUTPUT ${OUTPUT_DIR}/n_ac_types.h)
 
     file(MAKE_DIRECTORY ${OUTPUT_DIR})
 
