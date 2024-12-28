d75929289907f95e46e890ea37a238297597075d
Aaron Chan <aarchan108@gmail.com>
2024-12-28 16:47:25 -0500

    Stubbed cmessage_benchmarks project. Update gitignore to ignore all the build folders, simulation outputs. Add radio module .idea

---
+++ app/samples/cmessage_benchmarks/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+* Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <f_core/os/n_rtos.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+volatile int interruptCount = 0;
+
+
+int main() {
+
+    return 0;
+}
