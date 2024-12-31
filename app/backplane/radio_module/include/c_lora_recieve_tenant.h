08ceb57bab14056cb00010e8658a51f0b7246411
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2024-12-30 23:03:14 -0500

    Moved CLoraRevieveTenant gpios initialization to member declaration

--- app/backplane/radio_module/include/c_lora_recieve_tenant.h
+++ app/backplane/radio_module/include/c_lora_recieve_tenant.h
@@ -11,9 +11,7 @@
 class CLoraRecieveTenant : public CTenant {
 public:
     explicit CLoraRecieveTenant(const char* name, CLora& lora, const char* ip, const uint16_t srcPort)
-        : CTenant(name), lora(lora), udp(CUdpSocket(CIPv4(ip), srcPort, srcPort),
-        gpios{CGpio(*DEVICE_DT_GET(DT_ALIAS(gpios0))), CGpio(*DEVICE_DT_GET(DT_ALIAS(gpios1))),
-                CGpio(*DEVICE_DT_GET(DT_ALIAS(gpios2))), CGpio(*DEVICE_DT_GET(DT_ALIAS(gpios3)))}) {}
+        : CTenant(name), lora(lora), udp(CUdpSocket(CIPv4(ip), srcPort, srcPort)) {}
 
     ~CLoraRecieveTenant() override = default;
 
@@ -26,7 +24,8 @@ public:
 private:
     CLora& lora;
     CUdpSocket udp;
-    const CGpio gpios[4];
+    const CGpio gpios[4] = {CGpio(*DEVICE_DT_GET(DT_ALIAS(gpios0))), CGpio(*DEVICE_DT_GET(DT_ALIAS(gpios1))),
+                CGpio(*DEVICE_DT_GET(DT_ALIAS(gpios2))), CGpio(*DEVICE_DT_GET(DT_ALIAS(gpios3)))};
 };
 
 
