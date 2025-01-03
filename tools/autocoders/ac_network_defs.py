ff8124d405ff6c407e30f8e998c29c9496590d70
Aaron Chan <aarchan108@gmail.com>
2025-01-03 18:41:18 -0500

    Blast any changes to tools

--- tools/autocoders/ac_network_defs.py
+++ tools/autocoders/ac_network_defs.py
@@ -1,25 +1,11 @@
 import argparse
 from datetime import datetime
+
 import yaml
 import jinja2
 import os
 
-def parse_yaml_files(file_paths):
-    general = []
-    modules = dict()
-
-    for file_path in file_paths:
-        with open(file_path, 'r') as stream:
-            try:
-                data = yaml.safe_load(stream)
-                general = data.get("general", [])
-                modules = data.get("modules", {})
-            except yaml.YAMLError as exc:
-                print(f"Error parsing {file_path}: {exc}")
-
-    return general, modules
-
-if __name__ == '__main__':
+def main():
     template_path = __file__.split(os.path.basename(__file__))[0] + "templates/ac_network_defs.h"
     template = jinja2.Template(open(template_path).read())
 
@@ -29,9 +15,22 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    file_paths = [os.path.join(os.getcwd(), fname) for fname in args.files]
+    files = [os.getcwd() + "/" + fname for fname in args.files]
+    general = []
+    modules = dict()
+    for file in args.files:
+        with open(file, 'r') as stream:
+            try:
+                data = yaml.safe_load(stream)
+                general = data["general"]
+                modules = data["modules"]
 
-    general, modules = parse_yaml_files(file_paths)
+            except yaml.YAMLError as exc:
+                print(exc)
+
 
     with open(args.output, 'w') as f:
-        f.write(template.render(files=file_paths, general=general, modules=modules, date_time=datetime.now()))
+        f.write(template.render(files=files, general=general, modules=modules, date_time=datetime.now()))
+
+if __name__ == '__main__':
+    main()
--- tools/autocoders/ac_types.py
+++ tools/autocoders/ac_types.py
@@ -1,27 +1,11 @@
 import argparse
 from datetime import datetime
+
 import yaml
 import jinja2
 import os
 
-def parse_yaml_types(file_paths):
-    types_list = []
-
-    for file_path in file_paths:
-        with open(file_path, 'r') as stream:
-            try:
-                data = yaml.safe_load(stream)
-
-                # 0th index of tuple is the name of the type
-                # 1st index of tuple is key-value with the description and information for each field
-                types_list += [(key, data[key]) for key in data]
-            except yaml.YAMLError as exc:
-                print(f"Error parsing YAML file: {file_path}")
-                print(exc)
-
-    return types_list
-
-if __name__ == '__main__':
+def main():
     template_path = __file__.split(os.path.basename(__file__))[0] + "templates/ac_types.h"
     template = jinja2.Template(open(template_path).read())
 
@@ -31,9 +15,23 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    file_paths = [os.path.join(os.getcwd(), fname) for fname in args.files]
+    files = [os.getcwd() + "/" + fname for fname in args.files]
+    types = []
 
-    types = parse_yaml_types(file_paths)
+    for file in args.files:
+        with open(file, 'r') as stream:
+            try:
+                data = yaml.safe_load(stream)
+
+                # 0th index of tuple is the name of the type
+                # 1st index of tuple is key-value with the description and information for each field
+                types += [(key, data[key]) for key in data]
+            except yaml.YAMLError as exc:
+                print("Error parsing YAML file: " + file)
+                print(exc)
 
     with open(args.output, 'w') as f:
-        f.write(template.render(files=file_paths, types=types, date_time=datetime.now()))
+        f.write(template.render(files=files, types=types, date_time=datetime.now()))
+
+if __name__ == '__main__':
+    main()
