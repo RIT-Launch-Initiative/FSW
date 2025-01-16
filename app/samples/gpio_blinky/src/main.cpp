10f4fbd00b4e2bfeb211ab24700797902d573c7a
Aaron Chan <aarchan108@gmail.com>
2025-01-15 19:29:52 -0500

    Have ctor initialize as gpio input or output and add as sample to CI

--- app/samples/gpio_blinky/src/main.cpp
+++ app/samples/gpio_blinky/src/main.cpp
@@ -15,24 +15,11 @@ int main() {
     gpio_dt_spec ledPin = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
     CGpio led(ledPin);
 
-    int ret = gpio_pin_configure_dt(&ledPin, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        return 0;
-    }
-
     while (true) {
-        ret = gpio_pin_toggle_dt(&ledPin);
-        if (ret) {
-            LOG_ERR("Failed to toggle LED: %d", ret);
-        }
-
+        led.pin_set(1);
         k_msleep(1000);
-
-//        led.pin_set(1);
-//        k_msleep(1000);
-//        led.pin_set(0);
-//        k_msleep(1000);
-
+        led.pin_set(0);
+        k_msleep(1000);
     }
 
     return 0;
