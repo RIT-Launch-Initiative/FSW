147551ad68dcd38e301415b5b458a310080e2fe0
cowsed <richie.sommers@outlook.com>
2025-01-27 21:31:56 -0500

    removed files that will come later

--- drivers/modem/rfm9Xw/hex2symbols.py
+++
@@ -1,8 +0,0 @@
-hexStr = '7a2ab'
-if len(hexStr) % 2 == 1:
-    print('invalid length input')
-    raise Exception("bad input")
-
-hs = [hexStr[i:i+2] for i in range(0, len(hexStr), 2)]
-eyes = [int(h, 16) for h in hs]
-print(eyes)
