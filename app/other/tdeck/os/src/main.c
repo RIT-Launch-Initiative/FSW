9d26c7a025b7a02ea53de01e5e1adc8aca7ddee1
cowsed <richie.sommers@outlook.com>
2025-01-29 19:33:59 -0500

    stuff n things

--- app/other/tdeck/os/src/main.c
+++ app/other/tdeck/os/src/main.c
@@ -26,7 +26,7 @@ LOG_MODULE_REGISTER(app);
 
 const struct device *bl_dev = DEVICE_DT_GET(DT_NODELABEL(backlight));
 
-static uint32_t count;
+static uint32_t count = 0;
 
 static void callback_delta(lv_event_t *e) {
     ARG_UNUSED(e);
@@ -105,8 +105,6 @@ int main(void) {
     lv_obj_t *ta = lv_textarea_create(lv_screen_active());
     lv_textarea_set_one_line(ta, true);
     lv_obj_align(ta, LV_ALIGN_CENTER, 0, 20);
-    // lv_obj_add_event_cb(ta, textarea_event_handler, LV_EVENT_READY, ta);
-    // lv_obj_add_state(ta, LV_STATE_FOCUSED); /*To be sure the cursor is visible*/
 
     display_blanking_off(display_dev);
 
