31a09ecd863cad473984c7035992b0f0b24d7ed3
Aaron Chan <aarchan108@gmail.com>
2025-02-06 15:43:50 -0500

    Add electric toothbrush sample starter

---
+++ app/samples/electric_toothbrush/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+* Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+
+int main() {
+   return 0;
+}
