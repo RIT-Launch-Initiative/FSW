74419f49c262bb9d2bb38e6866f298e4472f0c04
Aaron Chan <aarchan108@gmail.com>
2025-03-01 18:52:16 -0500

    Remaining logic

--- tools/flight_data_downloader/fdd.py
+++ tools/flight_data_downloader/fdd.py
@@ -4,29 +4,49 @@ from flight_data_downloader.fdd_transport import FDDTransport
 from flight_data_downloader.serial_transport import SerialTransport
 from flight_data_downloader.tftp_transport import TFTPTransport
 
-transport: FDDTransport = None
-
-
-def handle_set_command_initial(args):
-    pass
-
+transport = None
 
 def handle_set_command_serial(args):
-    pass
-
+    if args[0] == "serial_port":
+        transport.set_serial_port(args[1])
+    elif args[0] == "baud_rate":
+        transport.set_baud_rate(int(args[1]))
 
 def handle_set_command_tftp(args):
-    pass
+    if args[0] == "ip":
+        transport.set_ip(args[1])
 
 
 def handle_set_command(args):
-    if transport is None:
-        handle_set_command_initial(args)
-    elif isinstance(transport, TFTPTransport):
-        handle_set_command_initial(args)
-    elif isinstance(transport, SerialTransport):
-        handle_set_command_serial(args)
+    global transport
 
+    # Set the transport
+    if args[0] == "transport":
+        if args[1] == "tftp":
+            transport = TFTPTransport()
+        elif args[1] == "serial":
+            transport = SerialTransport()
+        else:
+            print("Invalid transport")
+        return
+
+    # Set the output folder
+    if args[0] == "output":
+        if len(args) == 1:
+            transport.set_output_folder(args[1])
+        elif len(args) == 0: # No folder specified
+            print("Output folder not specified")
+        else: # Spaces in folder name
+            print("Invalid output folder")
+
+    # Applies to specific transports
+    if transport is not None:
+        if isinstance(transport, TFTPTransport):
+            handle_set_command_tftp(args)
+        elif isinstance(transport, SerialTransport):
+            handle_set_command_serial(args)
+    else:
+        print("Transport not set")
 
 def handle_tree_command():
     if transport is not None:
