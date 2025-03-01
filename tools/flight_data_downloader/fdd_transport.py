0cfaf9317b726fcd4ab66225d3b8f4ad9aa36036
Aaron Chan <aarchan108@gmail.com>
2025-03-01 12:25:58 -0500

    FDDTransport class written

--- tools/flight_data_downloader/fdd_transport.py
+++ tools/flight_data_downloader/fdd_transport.py
@@ -1,13 +1,23 @@
+import os
+
 class FDDTransport:
+    __slots__ = ["output_folder"]
+
     def __init__(self):
-        pass
+        self.output_folder = os.getcwd()
 
-    def _get_file(file: str)
-        pass
+    def set_output_folder(self, folder: str):
+        self.output_folder = folder
 
-    def tree():
-        text = _get_file(file)
-        print("\n" + text)
+    def _get_file(self, file: str) -> str:
+        raise NotImplementedError("FDDTransport is an abstract class")
 
-    def download(file: str):
-        pass
+    def tree(self):
+        print("\n" + self._get_file("tree"))
+
+    def download(self, file: str):
+        file_name = file.split("/")[-1]
+        out_path = os.path.join(self.output_folder, file_name)
+
+        with open(out_path, "w") as fout:
+            fout.write(self._get_file(file))
--- tools/flight_data_downloader/serial_transport.py
+++ tools/flight_data_downloader/serial_transport.py
@@ -4,8 +4,5 @@ class SerialTransport(FDDTransport):
     def __init__(self):
         pass
 
-    def tree():
-        pass
-
     def download(file: str):
         pass
