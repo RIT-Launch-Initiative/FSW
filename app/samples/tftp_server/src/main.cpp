c2f4e2989f3dee3c06637bff33e46aa8baa7945f
Aaron Chan <aarchan108@gmail.com>
2025-03-08 16:19:50 -0500

    TFTP Server sample starter

---
+++ app/samples/tftp_server/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+* Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/kernel.h>
+#include <f_core/os/n_rtos.h>
+
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+int main() {
+
+    return 0;
+}
