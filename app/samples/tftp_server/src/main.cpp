400768c7e609f38c714f00598d9deef354a35ef2
Aaron Chan <aarchan108@gmail.com>
2025-03-08 19:53:37 -0500

    Sample

--- app/samples/tftp_server/src/main.cpp
+++ app/samples/tftp_server/src/main.cpp
@@ -6,6 +6,7 @@
 #include <zephyr/kernel.h>
 #include <f_core/os/n_rtos.h>
 #include <f_core/net/application/c_tftp_server_tenant.h>
+#include <f_core/net/application/c_udp_broadcast_tenant.h>
 
 #include <zephyr/logging/log.h>
 
@@ -13,7 +14,16 @@
 LOG_MODULE_REGISTER(main);
 
 int main() {
-    CTftpServerTenant *tftpServer = CTftpServerTenant::getInstance(CIPv4("10.0.0.1"));
+    CIPv4 ip("10.0.0.1");
+    CTftpServerTenant *tftpServer = CTftpServerTenant::getInstance(ip);
+    CUdpSocket udp(ip, 10000, 10000); // Just broadcast timestamps to ensure we can run both on the same thread
+
+    uint32_t uptime = k_uptime_get_32();
+    while (true) {
+        uptime = k_uptime_get_32();
+        tftpServer->Run();
+        udp.TransmitAsynchronous(&uptime, sizeof(uint32_t));
+    }
 
     return 0;
 }
