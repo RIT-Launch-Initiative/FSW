c476ecbfbd54d7f39a1d04be4d250476cb3ffbf5
Aaron Chan <aarchan108@gmail.com>
2025-03-19 19:10:48 -0400

    :)

--- lib/f_core/net/application/c_sntp_server_tenant.cpp
+++ lib/f_core/net/application/c_sntp_server_tenant.cpp
@@ -4,4 +4,18 @@ void CSntpServerTenant::Startup() {}
 
 void CSntpServerTenant::Cleanup() {}
 
-void CSntpServerTenant::Run() {}
+void CSntpServerTenant::Run() {
+    sockaddr srcAddr = {0};
+    socklen_t srcAddrLen = sizeof(srcAddr);
+    uint8_t packet[rwRequestPacketSize] = {0};
+    int rxLen = sock.ReceiveAsynchronous(packet, rwRequestPacketSize, &srcAddr, &srcAddrLen);
+
+    if (rxLen == 0) {
+        return;
+    } else if (rxLen < 0) {
+        LOG_ERR("Failed to receive packet (%d)", rxLen);
+        return;
+    }
+
+    
+}
