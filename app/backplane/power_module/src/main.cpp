f69bcd88741c1821d395398b08e7f4cc042116d4
Aaron Chan <aarchan108@gmail.com>
2025-03-22 11:03:02 -0400

    Time power mod logging

--- app/backplane/power_module/src/main.cpp
+++ app/backplane/power_module/src/main.cpp
@@ -20,7 +20,7 @@ int main() {
     NRtos::StartRtos();
 
 #ifdef CONFIG_ARCH_POSIX
-    k_sleep(K_SECONDS(10));
+    k_sleep(K_SECONDS(300));
     NRtos::StopRtos();
     powerModule.Cleanup();
     k_sleep(K_FOREVER);
