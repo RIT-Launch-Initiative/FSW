3dcb5782a676cc9702d5faf95086cf34a924341b
Aaron Chan <aarchan108@gmail.com>
2025-03-28 01:34:36 -0400

    Timeslicing sample starter

---
+++ app/samples/timeslicing/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2025 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/kernel.h>
+#include <f_core/os/n_rtos.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+
+
+
+int main() {
+
+    return 0;
+}
