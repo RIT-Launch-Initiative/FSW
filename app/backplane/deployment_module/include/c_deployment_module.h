fdf3a0673a89f9818183ff5ea7fd0d39428d0785
Aaron Chan <aarchan108@gmail.com>
2025-03-29 05:25:09 -0400

    Deployment mod integration

--- app/backplane/deployment_module/include/c_deployment_module.h
+++ app/backplane/deployment_module/include/c_deployment_module.h
@@ -7,6 +7,7 @@
 #include <f_core/os/flight_log.hpp>
 #include <f_core/os/tenants/c_datalogger_tenant.h>
 #include <n_autocoder_network_defs.h>
+#include <f_core/device/c_rtc.h>
 
 class CDeploymentModule : public CProjectConfiguration {
   public:
@@ -38,7 +39,11 @@ class CDeploymentModule : public CProjectConfiguration {
   private:
     static std::string generateFlightLogPath();
 
+    // Devices
+    CRtc rtc{*DEVICE_DT_GET(DT_ALIAS(rtc))};
+
     std::string ipAddrStr = CREATE_IP_ADDR(NNetworkDefs::DEPLOYMENT_MODULE_IP_ADDR_BASE, 1, CONFIG_MODULE_ID);
+    const char* sntpServerAddr = "10.2.1.1"; // TODO: Maybe we should look into hostnames? Also, still need to fix the create ip addr bug...
 
     CFlightLog flight_log;
 
--- app/backplane/deployment_module/src/c_deployment_module.cpp
+++ app/backplane/deployment_module/src/c_deployment_module.cpp
@@ -2,7 +2,9 @@
 
 // F-Core
 #include <f_core/messaging/c_msgq_message_port.h>
+#include <f_core/net/application/c_sntp_server_tenant.h>
 #include <f_core/os/n_rtos.h>
+#include <f_core/utils/n_time_utils.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(CDeploymentModule);
@@ -41,5 +43,5 @@ void CDeploymentModule::AddTasksToRtos() {
 }
 
 void CDeploymentModule::SetupCallbacks() {
-
+    NTimeUtils::SntpSynchronize(rtc, sntpServerAddr, 5, K_MSEC(100));
 }
