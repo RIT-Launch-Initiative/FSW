8b523931b2eda9835df9053994e4ee302c686756
Aaron <30483634+AarC10@users.noreply.github.com>
2025-03-31 20:27:26 -0400

    Deployment Module Noseover Triggers Pyro Charges (#260)

--- app/backplane/deployment_module/include/c_deployment_module.h
+++ app/backplane/deployment_module/include/c_deployment_module.h
@@ -2,10 +2,12 @@
 #define C_DEPLOYMENT_MODULE_H
 
 // F-Core Includes
+#include <c_pyro_control_observer.h>
 #include <f_core/c_project_configuration.h>
 #include <f_core/os/c_task.h>
 #include <f_core/os/flight_log.hpp>
 #include <f_core/os/tenants/c_datalogger_tenant.h>
+#include <f_core/net/application/c_udp_alert_tenant.h>
 #include <n_autocoder_network_defs.h>
 
 class CDeploymentModule : public CProjectConfiguration {
@@ -40,13 +42,14 @@ class CDeploymentModule : public CProjectConfiguration {
 
     std::string ipAddrStr = CREATE_IP_ADDR(NNetworkDefs::DEPLOYMENT_MODULE_IP_ADDR_BASE, 1, CONFIG_MODULE_ID);
 
-    CFlightLog flight_log;
-
     // Tenants
-    // TODO: Tenant for listening to events
+    CUdpAlertTenant alertTenant{"Alert Tenant", ipAddrStr.c_str(), NNetworkDefs::ALERT_PORT};
 
     // Tasks
     CTask networkTask{"Networking Task", 15, 1024, 0};
+
+    // Observers
+    CPyroControlObserver pyroControlObserver;
 };
 
 #endif //C_DEPLOYMENT_MODULE_H
---
+++ app/backplane/deployment_module/src/c_pyro_control_observer.cpp
@@ -0,0 +1,59 @@
+#include "c_pyro_control_observer.h"
+
+#include <cstdio>
+#include <f_core/n_alerts.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/rtc.h>
+
+LOG_MODULE_REGISTER(CPyroControlObserver);
+
+static void chargeDisableTimerCallback(k_timer* timer) {
+    auto* observer = static_cast<CPyroControlObserver*>(timer->user_data);
+    observer->DisableCallback();
+}
+
+CPyroControlObserver::CPyroControlObserver() {
+    flightLog.Write("Pyro Controller Observer initialized");
+    chargeDisableTimer = CSoftTimer(chargeDisableTimerCallback);
+    chargeDisableTimer.SetUserData(this);
+}
+
+
+void CPyroControlObserver::Notify(void* ctx) {
+    uint8_t pyroCount = 0;
+
+    switch (*static_cast<NAlerts::AlertType*>(ctx)) {
+        case NAlerts::NOSEOVER:
+            LOG_INF("Noseover detected. Deploying charges in one second.");
+            flightLog.Write("Noseover detected. Deploying charges in one second.");
+            // TODO: Settings library for handling deployment timing
+            k_sleep(K_SECONDS(1));
+
+            for (auto& [sense, ctrl, led] : pyroTrios) {
+                if (sense.GetPin() == 1) {
+                    ctrl.SetPin(1);
+                    led.SetPin(1);
+                    LOG_INF("Deployed charge %d", pyroCount);
+                }
+                pyroCount++;
+            }
+            flightLog.Write("Finished deploying charges");
+            chargeDisableTimer.StartTimer(3000);
+            break;
+        default:
+            break;
+    }
+
+    flightLog.Sync();
+}
+
+void CPyroControlObserver::DisableCallback() {
+    int pyroCount = 0;
+    for (auto& [sense, ctrl, led] : pyroTrios) {
+        ctrl.SetPin(0);
+        led.SetPin(0);
+        LOG_INF("Disabled charge %d", pyroCount);
+        pyroCount++;
+    }
+    flightLog.Write("Finished disabling charges");
+}
