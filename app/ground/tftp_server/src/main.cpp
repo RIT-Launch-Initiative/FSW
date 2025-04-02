af4082688c191f01b1bf2b2f26d4a52bdf66768f
Aaron Chan <aarchan108@gmail.com>
2025-04-02 18:37:07 -0400

    Tftp copy pasta

---
+++ app/ground/tftp_server/src/main.cpp
@@ -0,0 +1,37 @@
+/*
+* Copyright (c) 2024 RIT Launch Initiative
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <f_core/net/application/c_tftp_server_tenant.h>
+#include <f_core/net/application/c_udp_broadcast_tenant.h>
+#include <f_core/os/c_file.h>
+#include <f_core/os/n_rtos.h>
+#include <zephyr/fs/fs.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+int main() {
+    CIPv4 ip("10.0.0.1");
+    CTftpServerTenant *tftpServer = CTftpServerTenant::GetInstance(ip);
+
+    LOG_INF("Creating file");
+
+    CFile file("/lfs/test.txt", FS_O_READ | FS_O_WRITE | FS_O_CREATE);
+    if (file.GetInitStatus() < 0) {
+        LOG_ERR("Error opening file");
+        return -1;
+    }
+
+    int ret = file.Write("Hello, world!", 13);
+    LOG_INF("Wrote to file %d", ret);
+
+    while (true) {
+        tftpServer->Run();
+        k_msleep(100);
+    }
+
+    return 0;
+}
