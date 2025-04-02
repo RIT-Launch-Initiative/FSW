89ecd61bfe67bf1f49331f8d51730ef41609a431
Aaron <30483634+AarC10@users.noreply.github.com>
2025-04-01 21:27:24 -0400

    Network Time Protocol Server + Synchronization (#268)

--- app/backplane/deployment_module/include/c_deployment_module.h
+++ app/backplane/deployment_module/include/c_deployment_module.h
@@ -9,6 +9,7 @@
 #include <f_core/os/tenants/c_datalogger_tenant.h>
 #include <f_core/net/application/c_udp_alert_tenant.h>
 #include <n_autocoder_network_defs.h>
+#include <f_core/device/c_rtc.h>
 
 class CDeploymentModule : public CProjectConfiguration {
   public:
@@ -32,15 +33,14 @@ class CDeploymentModule : public CProjectConfiguration {
      */
     void SetupCallbacks() override;
 
-    /**
-    * Cleanup
-    */
-    void Cleanup() = delete;
-
   private:
     static std::string generateFlightLogPath();
 
+    // Devices
+    CRtc rtc{*DEVICE_DT_GET(DT_ALIAS(rtc))};
+
     std::string ipAddrStr = CREATE_IP_ADDR(NNetworkDefs::DEPLOYMENT_MODULE_IP_ADDR_BASE, 1, CONFIG_MODULE_ID);
+    const char* sntpServerAddr = "10.2.1.1"; // TODO: Maybe we should look into hostnames? Also, still need to fix the create ip addr bug...
 
     // Tenants
     CUdpAlertTenant alertTenant{"Alert Tenant", ipAddrStr.c_str(), NNetworkDefs::ALERT_PORT};
--- app/backplane/radio_module/src/main.cpp
+++ app/backplane/radio_module/src/main.cpp
@@ -29,7 +29,6 @@ int main() {
     radioModule.SetupCallbacks();
 
     NRtos::StartRtos();
-    
     k_sched_time_slice_set(1000, 14); // 1ms slice for priority 14
     k_sched_time_slice_set(5000, 15); // 5ms slice for priority 15
 
