7106949217a1a3c3a99752f7b5bd0f9d11bb7369
cowsed <richie.sommers@outlook.com>
2025-04-04 13:08:55 -0400

    cleanup bringup

--- app/payload/control_freak/board-setup/src/main.cpp
+++ app/payload/control_freak/board-setup/src/main.cpp
@@ -2,10 +2,6 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
 #define GPSRST_NODE DT_ALIAS(gpsreset)
 static const struct gpio_dt_spec gpsreset = GPIO_DT_SPEC_GET(GPSRST_NODE, gpios);
 
@@ -14,13 +10,6 @@ static const struct gpio_dt_spec gpssafeboot = GPIO_DT_SPEC_GET(GPSSAFE_NODE, gp
 
 int main() {
     int ret;
-    bool led_state = true;
-
-    if (!gpio_is_ready_dt(&led)) {
-        printk("No LED :(\n");
-        return 0;
-    }
-
     if (!gpio_is_ready_dt(&gpsreset)) {
         printk("No GPS RST :(\n");
         return 0;
@@ -30,23 +19,19 @@ int main() {
         return 0;
     }
 
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        printk("Failed to conf led :(\n");
-        return 0;
-    }
     ret = gpio_pin_configure_dt(&gpsreset, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
         printk("Failed to conf gps reset :(\n");
         return 0;
     }
 
+    // Safeboot active low (send downwards before reset to enter safeboot)
     ret = gpio_pin_configure_dt(&gpssafeboot, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
         printk("Failed to conf gps safe :(\n");
         return 0;
     }
-
+    // Don't enter safeboot: pin to logic 0
     ret = gpio_pin_set_dt(&gpssafeboot, 0);
     if (ret < 0) {
         printk("couldnt set gpssafeboot: %d", ret);
@@ -54,7 +39,7 @@ int main() {
 
     k_msleep(1);
 
-    // RESET GPS
+    // Gps Reset Routine
 
     ret = gpio_pin_set_dt(&gpsreset, 1);
     if (ret < 0) {
@@ -65,24 +50,7 @@ int main() {
     if (ret < 0) {
         printk("couldnt set gpsreset: %d", ret);
     }
-    printk("Reset GPS\n");
+    printk("GPS Reset\n");
 
-    // ret = gpio_pin_configure_dt(&gpssafeboot, GPIO_INPUT);
-    // if (ret < 0) {
-    // printk("Failed to conf gps safe back to input:(\n");
-    // return 0;
-    // }
-    // printf("As timepulse");
-
-    while (1) {
-        // ret = gpio_pin_toggle_dt(&led);
-        // if (ret < 0) {
-        // return 0;
-        // }
-        //
-        // led_state = !led_state;
-        // printf("LED state: %s\n", led_state ? "ON" : "OFF");
-        k_msleep(20000);
-    }
     return 0;
 }
