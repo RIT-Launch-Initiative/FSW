e4843dfce121917bc2d08dd7b8455dbc08f039c0
Aaron Chan <aarchan108@gmail.com>
2025-04-07 02:12:27 -0400

    stube out tests and fubnctionaity

--- app/samples/compression/src/main.cpp
+++ app/samples/compression/src/main.cpp
@@ -1,9 +1,43 @@
 #include <zephyr/kernel.h>
 #include <n_autocoder_types.h>
 #include <lz4.h>
+#include <string>
+
+
+void printTestSet(std::string testSetName) {
+    printk("----------------------");
+    printk("%s", testSetName.c_str(), "");
+    printk("----------------------");
+}
+
+void getCompressionStatistics(std::string type, void *data, size_t dataSize) {
+
+
+}
+
 
 int main() {
+    NTypes::EnvironmentData envData = {0};
+    NTypes::PowerData powerData = {0};
+    NTypes::CoordinateData coordData = {0};
 
-    
+    printTestSet("Zeroed Data");
+    getCompressionStatistics("EnvironmentData", &envData, sizeof(envData));
+    getCompressionStatistics("PowerData", &powerData, sizeof(powerData));
+    getCompressionStatistics("CoordinateData", &coordData, sizeof(coordData));
+
+    printTestSet("Randomized data");
+    getCompressionStatistics("EnvironmentData", &envData, sizeof(envData));
+    getCompressionStatistics("PowerData", &powerData, sizeof(powerData));
+    getCompressionStatistics("CoordinateData", &coordData, sizeof(coordData));
+
+
+    printTestSet("Expected data");
+    getCompressionStatistics("EnvironmentData", &envData, sizeof(envData));
+    getCompressionStatistics("PowerData", &powerData, sizeof(powerData));
+    getCompressionStatistics("CoordinateData", &coordData, sizeof(coordData));
+
+
+
     return 0;
 }
