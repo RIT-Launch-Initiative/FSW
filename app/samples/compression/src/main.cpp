cc437ad30e0a6303d87b4e483a88ccbbabb6e512
Aaron Chan <aarchan108@gmail.com>
2025-04-07 02:15:27 -0400

    getcompression stats

--- app/samples/compression/src/main.cpp
+++ app/samples/compression/src/main.cpp
@@ -3,6 +3,7 @@
 #include <lz4.h>
 #include <string>
 
+static constexpr size_t MAX_BUFFER_SIZE = 256;
 
 void printTestSet(std::string testSetName) {
     printk("----------------------");
@@ -10,9 +11,15 @@ void printTestSet(std::string testSetName) {
     printk("----------------------");
 }
 
-void getCompressionStatistics(std::string type, void *data, size_t dataSize) {
+void getCompressionStatistics(const std::string& type, void* data, int dataSize) {
+    uint8_t compressedBuffer[MAX_BUFFER_SIZE] = {0};
 
+    const int64_t startTime = k_uptime_get();
+    const int compressedSize = LZ4_compress_default(static_cast<const char*>(data), reinterpret_cast<char*>(compressedBuffer), dataSize,MAX_BUFFER_SIZE);
+    const int64_t endTime = k_uptime_get();
+    const int64_t elapsedTime = endTime - startTime;
 
+    printk("\tType: %s - Compressed from %d to %d bytes in %ld milliseconds\n", type.c_str(), dataSize, compressedSize, elapsedTime);
 }
 
 
@@ -31,13 +38,10 @@ int main() {
     getCompressionStatistics("PowerData", &powerData, sizeof(powerData));
     getCompressionStatistics("CoordinateData", &coordData, sizeof(coordData));
 
-
     printTestSet("Expected data");
     getCompressionStatistics("EnvironmentData", &envData, sizeof(envData));
     getCompressionStatistics("PowerData", &powerData, sizeof(powerData));
     getCompressionStatistics("CoordinateData", &coordData, sizeof(coordData));
-
-
 
     return 0;
 }
