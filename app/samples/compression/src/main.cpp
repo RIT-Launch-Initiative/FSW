00a0bd6f86f8d58b122ebe90ee75c61ec3a29408
Aaron Chan <aarchan108@gmail.com>
2025-04-10 02:32:42 -0400

    Fix styling, but unknown how to get lz4 to auto install in CI?

--- app/samples/compression/src/main.cpp
+++ app/samples/compression/src/main.cpp
@@ -1,9 +1,9 @@
-#include <zephyr/kernel.h>
-#include <n_autocoder_types.h>
 #include <lz4.h>
+#include <n_autocoder_types.h>
+#include <random>
 #include <string.h>
 #include <string>
-#include <random>
+#include <zephyr/kernel.h>
 
 static constexpr size_t MAX_BUFFER_SIZE = 256;
 
@@ -14,42 +14,49 @@ static uint8_t fileBuffer[pretendFlightFileSize] = {0};
 
 static void randomizeFileBuffer() {
     // Gives less 0s if we initialize the buffer with some data
-    NTypes::EnvironmentData realisticEnvData {
-        .PrimaryBarometer = {
-            .Pressure = 1013.59,
-            .Temperature = -19.0,
-        },
-        .SecondaryBarometer = {
-            .Pressure = 998.45,
-            .Temperature = -22.0,
-        },
-        .Acceleration = {
-            .X = 6.13,
-            .Y = 9.37,
-            .Z = 50.0,
-        },
-        .ImuAcceleration = {
-            .X = 8.13,
-            .Y = 12.37,
-            .Z = 47.24,
-        },
-        .ImuGyroscope = {
-            .X = -50.0,
-            .Y = 50.0,
-            .Z = 15.0,
-        },
-        .Magnetometer = {
-            .X = 0.37,
-            .Y = 0.04,
-            .Z = 1.2,
-        },
+    NTypes::EnvironmentData realisticEnvData{
+        .PrimaryBarometer =
+            {
+                .Pressure = 1013.59,
+                .Temperature = -19.0,
+            },
+        .SecondaryBarometer =
+            {
+                .Pressure = 998.45,
+                .Temperature = -22.0,
+            },
+        .Acceleration =
+            {
+                .X = 6.13,
+                .Y = 9.37,
+                .Z = 50.0,
+            },
+        .ImuAcceleration =
+            {
+                .X = 8.13,
+                .Y = 12.37,
+                .Z = 47.24,
+            },
+        .ImuGyroscope =
+            {
+                .X = -50.0,
+                .Y = 50.0,
+                .Z = 15.0,
+            },
+        .Magnetometer =
+            {
+                .X = 0.37,
+                .Y = 0.04,
+                .Z = 1.2,
+            },
         .Temperature = -20.0,
     };
 
     for (size_t i = 0; i < pretendFlightFileSize / sizeof(NTypes::EnvironmentData); i++) {
         memcpy(&fileBuffer[i * sizeof(NTypes::EnvironmentData)], &realisticEnvData, sizeof(NTypes::EnvironmentData));
         // Change up some numbers
-        reinterpret_cast<NTypes::EnvironmentData*>(&fileBuffer[i * sizeof(NTypes::EnvironmentData)])->PrimaryBarometer.Pressure += static_cast<float>(rand() % 100) / 100.0f;
+        reinterpret_cast<NTypes::EnvironmentData*>(&fileBuffer[i * sizeof(NTypes::EnvironmentData)])
+            ->PrimaryBarometer.Pressure += static_cast<float>(rand() % 100) / 100.0f;
     }
 }
 #endif
@@ -60,12 +67,13 @@ void printTestSet(std::string testSetName) {
     printk("----------------------\n");
 }
 
-void getDecompressionSafeStatistics(const char* type, void* compressedData, void *uncompressedData, const size_t dataSize, const int maxDataSize = MAX_BUFFER_SIZE) {
+void getDecompressionSafeStatistics(const char* type, void* compressedData, void* uncompressedData,
+                                    const size_t dataSize, const int maxDataSize = MAX_BUFFER_SIZE) {
     uint8_t decompressedBuffer[maxDataSize] = {0};
     const int64_t startTime = k_uptime_get();
-    const int decompressedSize = LZ4_decompress_safe(reinterpret_cast<const char*>(compressedData),
-                                                     reinterpret_cast<char*>(decompressedBuffer), dataSize,
-                                                     maxDataSize);
+    const int decompressedSize =
+        LZ4_decompress_safe(reinterpret_cast<const char*>(compressedData), reinterpret_cast<char*>(decompressedBuffer),
+                            dataSize, maxDataSize);
     const int64_t endTime = k_uptime_get();
     const int64_t elapsedTime = endTime - startTime;
     int errorCount = 0;
@@ -75,15 +83,16 @@ void getDecompressionSafeStatistics(const char* type, void* compressedData, void
         }
     }
 
-    printk("\t\tDecompressed from %zu to %zu bytes in %lld milliseconds with %d errors\n", dataSize, decompressedSize, elapsedTime, errorCount);
+    printk("\t\tDecompressed from %zu to %zu bytes in %lld milliseconds with %d errors\n", dataSize, decompressedSize,
+           elapsedTime, errorCount);
 }
 
-void getCompressionSafeStatistics(const char* type, void* data, const size_t dataSize, const int maxDataSize = MAX_BUFFER_SIZE) {
+void getCompressionSafeStatistics(const char* type, void* data, const size_t dataSize,
+                                  const int maxDataSize = MAX_BUFFER_SIZE) {
     uint8_t compressedBuffer[maxDataSize] = {0};
     const int64_t startTime = k_uptime_get();
     const int compressedSize = LZ4_compress_default(static_cast<const char*>(data),
-                                                    reinterpret_cast<char*>(compressedBuffer), dataSize,
-                                                    maxDataSize);
+                                                    reinterpret_cast<char*>(compressedBuffer), dataSize, maxDataSize);
     const int64_t endTime = k_uptime_get();
     const int64_t elapsedTime = endTime - startTime;
 
@@ -129,54 +138,63 @@ int main() {
     getCompressionSafeStatistics("CoordinateData", &coordData, sizeof(coordData));
 
     printTestSet("Expected data");
-    NTypes::EnvironmentData realisticEnvData {
-        .PrimaryBarometer = {
-            .Pressure = 1013.59,
-            .Temperature = -19.0,
-        },
-        .SecondaryBarometer = {
-            .Pressure = 998.45,
-            .Temperature = -22.0,
-        },
-        .Acceleration = {
-            .X = 6.13,
-            .Y = 9.37,
-            .Z = 50.0,
-        },
-        .ImuAcceleration = {
-            .X = 8.13,
-            .Y = 12.37,
-            .Z = 47.24,
-        },
-        .ImuGyroscope = {
-            .X = -50.0,
-            .Y = 50.0,
-            .Z = 15.0,
-        },
-        .Magnetometer = {
-            .X = 0.37,
-            .Y = 0.04,
-            .Z = 1.2,
-        },
+    NTypes::EnvironmentData realisticEnvData{
+        .PrimaryBarometer =
+            {
+                .Pressure = 1013.59,
+                .Temperature = -19.0,
+            },
+        .SecondaryBarometer =
+            {
+                .Pressure = 998.45,
+                .Temperature = -22.0,
+            },
+        .Acceleration =
+            {
+                .X = 6.13,
+                .Y = 9.37,
+                .Z = 50.0,
+            },
+        .ImuAcceleration =
+            {
+                .X = 8.13,
+                .Y = 12.37,
+                .Z = 47.24,
+            },
+        .ImuGyroscope =
+            {
+                .X = -50.0,
+                .Y = 50.0,
+                .Z = 15.0,
+            },
+        .Magnetometer =
+            {
+                .X = 0.37,
+                .Y = 0.04,
+                .Z = 1.2,
+            },
         .Temperature = -20.0,
     };
     NTypes::PowerData realisticPowerData{
         // I actually don't know current and voltage without reading actual sensors
-        .RailBattery = {
-            .Voltage = 12.0,
-            .Current = 13.37,
-            .Power = 6.0,
-        },
-        .Rail3v3 = {
-            .Voltage = 3.3,
-            .Current = 0.5,
-            .Power = 1.0,
-        },
-        .Rail5v0 = {
-            .Voltage = 5.0,
-            .Current = 1,
-            .Power = 2.5,
-        },
+        .RailBattery =
+            {
+                .Voltage = 12.0,
+                .Current = 13.37,
+                .Power = 6.0,
+            },
+        .Rail3v3 =
+            {
+                .Voltage = 3.3,
+                .Current = 0.5,
+                .Power = 1.0,
+            },
+        .Rail5v0 =
+            {
+                .Voltage = 5.0,
+                .Current = 1,
+                .Power = 2.5,
+            },
     };
     NTypes::CoordinateData realisticCoordData = {
         .Latitude = 43.08,
