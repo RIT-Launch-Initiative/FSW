7d1eb9a4b5901afec25bcfc3683f7d85c0b21c22
Aaron <30483634+AarC10@users.noreply.github.com>
2025-04-17 10:04:21 -0400

    Report LoRa RX Statistics Over Ethernet (#293)

--- app/ground/receiver_module/src/c_lora_receive_tenant.cpp
+++ app/ground/receiver_module/src/c_lora_receive_tenant.cpp
@@ -2,6 +2,7 @@
 #include "c_receiver_module.h"
 
 #include <n_autocoder_network_defs.h>
+#include <n_autocoder_types.h>
 
 #include <zephyr/logging/log.h>
 
@@ -24,16 +25,21 @@ void CLoraReceiveTenant::Run() {
     udp.TransmitAsynchronous(buffer + portOffset, rxSize - portOffset);
 }
 
-int CLoraReceiveTenant::receive(uint8_t* buffer, const int buffSize, int* port) const {
-    // LOG_INF("Waiting for LoRa data");
+int CLoraReceiveTenant::receive(uint8_t* buffer, const int buffSize, int* port) {
     int16_t rssi = 0;
     int8_t snr = 0;
+
     const int rxSize = loraTransmitTenant.lora.ReceiveSynchronous(buffer, buffSize, &rssi, &snr, K_SECONDS(5));
     if (rxSize == -EAGAIN) {
         return rxSize;
     }
 
-    LOG_INF("RSSI: %d SNR: %d", rssi, snr);
+    const NTypes::LoRaReceiveStatistics stats{
+        .ReceivedSignalStrengthIndicator = rssi,
+        .SignalToNoiseRatio = snr,
+    };
+
+    LOG_INF("RSSI: %d SNR: %d", stats.ReceivedSignalStrengthIndicator, stats.SignalToNoiseRatio);
 
     if (rxSize < 0) {
         LOG_ERR("Failed to receive over LoRa (%d)", rxSize);
@@ -47,5 +53,7 @@ int CLoraReceiveTenant::receive(uint8_t* buffer, const int buffSize, int* port) 
 
     *port = buffer[1] << 8 | buffer[0];
     LOG_INF("Got data for port %d from LoRa", *port);
+    udp.TransmitAsynchronous(&stats, sizeof(stats), NNetworkDefs::RADIO_MODULE_LORA_RX_STATS_PORT);
+
     return rxSize;
 }
