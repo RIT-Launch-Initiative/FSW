ddcd9d5cef3baf65567fdb4aa23686f394d8bc17
cowsed <richie.sommers@outlook.com>
2025-04-24 21:01:05 -0400

    ms -> millis

--- app/payload/control_freak/flight-software/src/flight.h
+++ app/payload/control_freak/flight-software/src/flight.h
@@ -7,11 +7,11 @@
 // Boost
 // IMU
 static constexpr float imuBoostThresholdMPerS2 = 5 * 9.8;  // m/s^2
-static constexpr uint32_t imuBoostTimeThreshold = 250;     // ms
+static constexpr uint32_t imuBoostTimeThreshold = 250;     // millis
 
 // Barometer - sustained velocity of 100 m/s over 1 second
 static constexpr float baromBoostThresholdMPerS = 100;    // m/s
-static constexpr uint32_t baromBoostTimeThreshold = 1000; // ms
+static constexpr uint32_t baromBoostTimeThreshold = 1000; // millis
 
 enum Events {
     PadReady,
