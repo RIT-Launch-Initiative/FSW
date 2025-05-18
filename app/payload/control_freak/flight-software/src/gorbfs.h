ee57454d8e8e376abd4ce8c445d27a5b483f0cc5
cowsed <richie.sommers@outlook.com>
2025-05-18 15:51:11 -0400

    remove funny stuff (dont mess up the branches this time)

--- app/payload/control_freak/flight-software/src/gorbfs.h
+++ app/payload/control_freak/flight-software/src/gorbfs.h
@@ -4,29 +4,6 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <zephyr/kernel.h>
-// Parts
-// Memslab
-// Message Queue for submitting slab chunks
-//
-
-// Storage Thread
-// loop {
-//     wait for data
-//     write to flash
-//     free slab
-// }
-//
-// feeding thread {
-//    get slab
-//    do stuff
-//    submit slab
-// }
-
-/*
-if circular, just do a circl (make sure you circle over everything tho lol)
-if non circular, write finishing block if there is a block leftover
-if you're the reco thingy, sucks to be you but you have gps time lol
-*/
 
 int gfs_alloc_slab(struct SuperFastPacket **slab, k_timeout_t timeout);
 int gfs_submit_slab(struct SuperFastPacket *slab, k_timeout_t timeout);
