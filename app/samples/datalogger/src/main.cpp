07defad229e0d03b61df21f031f3bc6ed44d9f02
Aaron <30483634+AarC10@users.noreply.github.com>
2025-05-24 23:59:53 +0000

    Data Logger Tenant Syncs With Filesystem (#300)

--- app/samples/datalogger/src/main.cpp
+++ app/samples/datalogger/src/main.cpp
@@ -12,14 +12,14 @@ CDataLogger<Packet> wrap_logger{"/lfs/wrap.bin", LogMode::Circular, 10};
 
 int main() {
     for (uint8_t i = 0; i < 100; i++) {
-        expand_logger.write({i, (uint8_t) (100 - i)});
-        fill_logger.write({i, (uint8_t) (100 - i)});
-        wrap_logger.write({i, (uint8_t) (100 - i)});
+        expand_logger.Write({i, (uint8_t) (100 - i)});
+        fill_logger.Write({i, (uint8_t) (100 - i)});
+        wrap_logger.Write({i, (uint8_t) (100 - i)});
         k_msleep(10);
     }
-    expand_logger.close();
-    fill_logger.close();
-    wrap_logger.close();
+    expand_logger.Close();
+    fill_logger.Close();
+    wrap_logger.Close();
     printk("Finished!\n");
     return 0;
 }
