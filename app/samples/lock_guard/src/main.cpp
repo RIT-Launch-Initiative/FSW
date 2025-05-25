f1001d3ccd9d4245770f954d24775b2e23fd3422
Aaron Chan <aarchan108@gmail.com>
2025-05-24 19:49:50 -0700

    Basic run without mutex

--- app/samples/lock_guard/src/main.cpp
+++ app/samples/lock_guard/src/main.cpp
@@ -1,20 +1,62 @@
 /*
-* Copyright (c) 2024 RIT Launch Initiative
+* Copyright (c) 2025 RIT Launch Initiative
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <zephyr/kernel.h>
 #include <f_core/os/n_rtos.h>
 #include <f_core/os/c_task.h>
+#include <f_core/os/c_tenant.h>
+
 
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main);
 
+static constexpr int maxCount = 10000;
+static int finishedTasks = 0;
+
+class CIncrementor : public CTenant {
+public:
+    CIncrementor(const char *name, int &counter) : CTenant(name), counter(counter) {
+    }
+
+    void Run() override {
+        if (counter < maxCount) {
+            counter++;
+
+            if (!finished) {
+                finishedTasks++;
+                finished = false;
+            }
+
+        }
+    }
+
+private:
+    static int id;
+    int &counter;
+    bool finished = false;
+};
+
 int main() {
+    int counter = 0;
+    static CIncrementor incrementerOne("One", counter);
+    static CIncrementor incrementerTwo("Two", counter);
+    static CIncrementor incrementerThree("Three", counter);
+    static CTask taskOne("One", 15, 512, 0);
+    static CTask taskTwo("Two", 15, 512, 0);
+    static CTask taskThree("Three", 15, 512, 0);
+
+    NRtos::AddTask(taskOne);
+    NRtos::AddTask(taskTwo);
+    NRtos::AddTask(taskThree);
+
     NRtos::StartRtos();
-    k_msleep(5000);
+    k_msleep(5000); // More than enough time to finish counting to a "small" enough number (i.e. 10000)
     NRtos::StopRtos();
+
+    LOG_INF("%d", counter);
 
     return 0;
 }
