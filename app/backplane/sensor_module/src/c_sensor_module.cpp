83639768e421fc37d479b28a989fdeb949331c61
Aaron <30483634+AarC10@users.noreply.github.com>
2025-06-01 18:34:19 -0700

    Backplane Single Flight Logs (#310)

--- app/backplane/sensor_module/src/c_sensor_module.cpp
+++ app/backplane/sensor_module/src/c_sensor_module.cpp
@@ -21,28 +21,8 @@ static auto alertMsgQueue = CMsgqMessagePort<NAlerts::AlertPacket>(alertQueue);
 
 CSensorModule::CSensorModule()
     : CProjectConfiguration(), sensorDataBroadcastMessagePort(broadcastMsgQueue), downlinkMessagePort(downlinkMsgQueue),
-      sensorDataLogMessagePort(dataLogMsgQueue), alertMessagePort(alertMsgQueue), flight_log{generateFlightLogPath()} {}
+      sensorDataLogMessagePort(dataLogMsgQueue), alertMessagePort(alertMsgQueue), flight_log{"/lfs/flight_log.txt"} {}
 
-std::string CSensorModule::generateFlightLogPath() {
-    constexpr size_t MAX_FLIGHT_LOG_PATH_SIZE = 32;
-    char flightLogPath[MAX_FLIGHT_LOG_PATH_SIZE] = {0};
-    for (size_t i = 0; i < 100; i++) {
-        snprintf(flightLogPath, MAX_FLIGHT_LOG_PATH_SIZE, "/lfs/flight_log%02d.txt", i);
-        struct fs_dirent ent;
-        int ret = fs_stat(flightLogPath, &ent);
-        if (ret == -ENOENT) {
-            // This path works
-            LOG_INF("Using %s for flight log", flightLogPath);
-            break;
-        } else if (ret != 0) {
-            LOG_WRN("Error reading filesystem for flight log. (error %d) Using name %s", ret, flightLogPath);
-            break;
-        }
-        // otherwise, keep counting up
-    }
-    std::string copystr{flightLogPath};
-    return copystr;
-}
 void CSensorModule::AddTenantsToTasks() {
     // Networking
     networkTask.AddTenant(broadcastTenant);
