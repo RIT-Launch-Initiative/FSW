c90be1edc3d67f57ecafb8340d7222a8f709d067
Aaron Chan <aarchan108@gmail.com>
2025-06-07 08:57:01 -0700

    Add two new functions for pausing and resuming tasks

--- include/f_core/os/c_task.h
+++ include/f_core/os/c_task.h
@@ -48,6 +48,16 @@ public:
     void Run();
 
     /**
+     * @brief Suspend the task from executing
+     */
+    void Suspend();
+
+    /**
+     * @brief Resume task execution
+     */
+    void Resume();
+
+    /**
      * Get the Zephyr thread associated with the task
      * @return Zephyr thread
      */
--- include/f_core/os/n_rtos.h
+++ include/f_core/os/n_rtos.h
@@ -9,21 +9,33 @@
 #include "f_core/os/c_task.h"
 
 namespace NRtos {
-    /**
-     * Add a task to be scheduled on the RTOS
-     * @param task Task to be scheduled on the RTOS
-     */
-    void AddTask(CTask &task);
+/**
+ * Add a task to be scheduled on the RTOS
+ * @param task Task to be scheduled on the RTOS
+ */
+void AddTask(CTask& task);
 
-    /**
-     * Initialize and start all tasks added to the RTOS
-     */
-    void StartRtos();
+/**
+ * Initialize and start all tasks added to the RTOS
+ */
+void StartRtos();
 
-    /**
-     * Cleanup tasks and abort all added tasks
-     */
-    void StopRtos();
+/**
+ * Cleanup tasks and abort all added tasks
+ */
+void StopRtos();
+
+/**
+ * Suspend a task from executing
+ * @param taskId The ID of the task to suspend
+ */
+void SuspendTask(k_tid_t taskId);
+
+/**
+ * Resume a task's execution
+ * @param taskId The ID of the task to resume
+ */
+void ResumeTask(k_tid_t taskId);
 };
 
 
