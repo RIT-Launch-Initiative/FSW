2ed3fc84e5c9e175afc9246cd4f02d17ace343bc
Aaron Chan <aarchan108@gmail.com>
2025-06-17 20:19:08 -0700

    Init the tenant in sensor mod

--- app/backplane/sensor_module/include/c_sensor_module.h
+++ app/backplane/sensor_module/include/c_sensor_module.h
@@ -74,7 +74,7 @@ class CSensorModule : public CProjectConfiguration {
     CUdpBroadcastTenant<NTypes::LoRaBroadcastSensorData> downlinkTelemTenant{"Telemetry Downlink Tenant", ipAddrStr.c_str(), telemetryDownlinkPort, telemetryDownlinkPort, downlinkMessagePort};
     CUdpBroadcastTenant<NAlerts::AlertPacket> udpAlertTenant{"UDP Alert Tenant", ipAddrStr.c_str(), alertPort, alertPort, alertMessagePort};
     CDataLoggerTenant<NTypes::TimestampedSensorData> dataLoggerTenant{"Data Logger Tenant", "/lfs/sensor_module_data.bin", LogMode::Growing, 0, sensorDataLogMessagePort, K_SECONDS(3), 64};
-    CCpuMonitorTenant& cpuMonitorTenant
+    CCpuMonitorTenant& cpuMonitorTenant{cpuMonitorMessagePort};
 
 
     // Tasks
