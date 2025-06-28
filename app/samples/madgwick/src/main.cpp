dd307e4fb6c8c0e462c5fc449f5eb6dbdfdfdd4f
Aaron Chan <aarchan108@gmail.com>
2025-06-27 22:39:26 -0400

    Madgwick class stub

--- app/samples/madgwick/src/main.cpp
+++ app/samples/madgwick/src/main.cpp
@@ -1,3 +1,5 @@
+#include <f_core/math/filters/c_madgwick.h>
+
 int main() {
   return 0;
 }
---
+++ include/f_core/math/filters/c_madgwick.h
@@ -0,0 +1,12 @@
+#ifndef C_MADGWICK_H
+#define C_MADGWICK_H
+
+class CMadgwick {
+public:
+
+private:
+
+
+};
+
+#endif //C_MADGWICK_H
