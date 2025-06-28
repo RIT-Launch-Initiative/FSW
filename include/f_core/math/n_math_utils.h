93ee0cd1e452fb4020f3cd48ab7d8abb7a8ca7c4
Aaron Chan <aarchan108@gmail.com>
2025-06-28 00:42:40 -0400

    Checkout math lib and add nmathutils

---
+++ include/f_core/math/n_math_utils.h
@@ -0,0 +1,8 @@
+#ifndef N_MATH_UTILS_H
+#define N_MATH_UTILS_H
+
+namespace NMathUtils {
+
+};
+
+#endif //N_MATH_UTILS_H
---
+++ lib/f_core/math/n_math_utils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aaron.m.chan on 6/28/25.
+//
+
+#include "n_math_utils.h"
