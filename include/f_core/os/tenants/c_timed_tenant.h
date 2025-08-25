3b892900bc11ff6ac612c591ca94dfdc2be2f80e
Aaron Chan <aarchan108@gmail.com>
2025-08-25 19:00:56 -0400

    Docs

--- include/f_core/os/tenants/c_timed_tenant.h
+++ include/f_core/os/tenants/c_timed_tenant.h
@@ -1,13 +1,25 @@
 #ifndef C_TIMED_TENANT_H
 #define C_TIMED_TENANT_H
+
 #include "f_core/os/c_tenant.h"
 #include "f_core/utils/c_soft_timer.h"
 
 class CTimedTenant : public CTenant {
 public:
+    /**
+     * @brief Constructor
+     */
     explicit CTimedTenant(const char* name)
         : CTenant(name) {}
 
+    /**
+     * @brief See parent docs.
+     */
+    void Startup() override;
+
+    /**
+     * @brief See parent docs.
+     */
     void Run() override {
         // Should be no behavior
     }
