aac726f03b75b50d71a690ff1f109897d8d81524
Aaron Chan <aarchan108@gmail.com>
2025-09-15 17:58:58 -0400

    Revert "Transmit data callbacks"
    This reverts commit bc7c60c9170e5473115da54bf5038c7bb1f49eb1.

--- app/backplane/power_module/src/udp_transmit.cpp
+++ app/backplane/power_module/src/udp_transmit.cpp
@@ -6,23 +6,12 @@
 #include <f_core/utils/c_hashmap.h>
 #include <f_core/net/transport/c_udp_socket.h>
 
-void transmitRawData(const zbus_channel* chan) {
-    CHashMap<std::string, void*> userData = *static_cast<CHashMap<std::string, void*>*>(zbus_chan_user_data(chan));
-    CUdpSocket* socket = static_cast<CUdpSocket*>(userData.Get("TelemetrySocket").value_or(nullptr));
-    if (socket == nullptr) {
-        LOG_ERR("No socket was set for telemetry transmission");
-        return;
-    }
-
-    socket->TransmitAsynchronous(zbus_chan_const_msg(chan), zbus_chan_msg_size(chan));
+void transmitRawData(const struct zbus_channel* chan) {
+    CHashMap<std::string, CUdpSocket> udpSockets = static_cast<CHashMap<std::string, CUdpSocket>>(zbus_chan_user_data(chan));
+    udpSockets.Get("TelemetrySocket")->TransmitAsynchronous(zbus_chan_const_msg(chan), zbus_chan_msg_size(chan));
 }
 
-void transmitDownlinkData(const zbus_channel* chan) {
-    CHashMap<std::string, void*> userData = *static_cast<CHashMap<std::string, void*>*>(zbus_chan_user_data(chan));
-    CUdpSocket* socket = static_cast<CUdpSocket*>(userData.Get("DownlinkSocket").value_or(nullptr));
-    if (socket == nullptr) {
-        LOG_ERR("No socket was set for LoRa transmission");
-        return;
-    }
-    socket->TransmitAsynchronous(zbus_chan_const_msg(chan), zbus_chan_msg_size(chan));
+void transmitDownlinkData(const struct zbus_channel* chan) {
+    CHashMap<std::string, CUdpSocket> udpSockets = static_cast<CHashMap<std::string, CUdpSocket>>(zbus_chan_user_data(chan));
+    udpSockets.Get("DownlinkSocket")->TransmitAsynchronous(zbus_chan_const_msg(chan), zbus_chan_msg_size(chan));
 }
