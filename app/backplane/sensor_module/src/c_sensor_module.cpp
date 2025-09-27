b0b2cca8b3709fe2f356d92e9ccbd5c757f86f46
Aaron Chan <aarchan108@gmail.com>
2025-09-27 11:54:44 -0400

    Define functions

--- app/backplane/sensor_module/src/c_sensor_module.cpp
+++ app/backplane/sensor_module/src/c_sensor_module.cpp
@@ -4,6 +4,7 @@
 #include <f_core/messaging/c_msgq_message_port.h>
 #include <f_core/os/n_rtos.h>
 #include <f_core/utils/n_time_utils.h>
+#include <f_core/os/c_raw_datalogger.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(sensor_module);
 
--- include/f_core/os/c_raw_datalogger.h
+++ include/f_core/os/c_raw_datalogger.h
@@ -1,4 +1,30 @@
 #ifndef C_RAW_DATALOGGER_H
 #define C_RAW_DATALOGGER_H
 
+#include <zephyr/storage/stream_flash.h>
+
+template <typename T, size_t bufferSize>
+class CRawDataLogger {
+public:
+    CRawDataLogger(const device &flash) : flash(flash), ctx(nullptr) {}
+
+    int Write() {
+
+    }
+
+    int Close() {
+
+    }
+
+    int Flush() {
+
+    }
+
+
+private:
+    const device& flash;
+    stream_flash_ctx *ctx;
+    uint8_t buffer[bufferSize];
+};
+
 #endif //C_RAW_DATALOGGER_H
