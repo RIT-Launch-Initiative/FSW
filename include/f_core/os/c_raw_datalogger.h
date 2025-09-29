cdcf5dbb1b6345c69ad0d468e0220dff32b6f436
Aaron Chan <aarchan108@gmail.com>
2025-09-29 19:23:13 -0400

    MRead metadata and fix formatting and use off_t

--- include/f_core/os/c_raw_datalogger.h
+++ include/f_core/os/c_raw_datalogger.h
@@ -26,9 +26,9 @@ constexpr uint32_t DATALOGGER_VERSION = 1;
 template <typename T, size_t packetBufferSize>
 class CRawDataLogger {
 public:
-    CRawDataLogger(const device* flash_dev, size_t flashAddress, size_t fileSize, const std::string& filename,
+    CRawDataLogger(const device* flashDev, off_t flashAddress, off_t fileSize, const std::string& filename,
                    DataloggerMode mode)
-        : flash(flash_dev), flashAddress(flashAddress), fileSize(fileSize), mode(mode), lastError(0),
+        : flash(flashDev), flashAddress(flashAddress), fileSize(fileSize), mode(mode), lastError(0),
           initialized(false), currentOffset(0), nextFileAddress(0) {
         memset(&ctx, 0, sizeof(ctx));
         memset(buffer, 0, sizeof(buffer));
@@ -54,6 +54,10 @@ public:
         }
 
         ret = flash_get_size(flash, &flashSize);
+        if (ret < 0) {
+            lastError = ret;
+            initialized = false;
+        }
     }
 
     bool IsInitialized() const { return initialized; }
@@ -61,9 +65,9 @@ public:
 
     int Write(const T& data, bool flush = false) {
         if (!initialized) return lastError ? lastError : -1;
-        size_t bytes_written = stream_flash_bytes_written(&ctx);
-        size_t space_left = fileSize - sizeof(metadata) - bytes_written;
-        if (space_left < sizeof(T)) {
+        size_t bytesWritten = stream_flash_bytes_written(&ctx);
+        size_t spaceLeft = fileSize - sizeof(metadata) - bytesWritten;
+        if (spaceLeft < sizeof(T)) {
             switch (mode) {
                 case DataloggerMode::Rotating:
                     currentOffset = sizeof(metadata);
@@ -100,22 +104,22 @@ public:
     }
 
     int Flush() {
-        if (!initialized) return lastError ? lastError : -1;
+        if (!initialized) {
+            return (lastError != 0) ? lastError : -1;
+        }
+
         int ret = stream_flash_buffered_write(&ctx, nullptr, 0, true);
         if (ret < 0) {
             lastError = ret;
             return ret;
         }
+
         return 0;
     }
 
     size_t GetBytesWritten() const {
         return stream_flash_bytes_written(&ctx);
     }
-
-    int GetLastError() const { return lastError; }
-    size_t GetCurrentOffset() const { return currentOffset; }
-    size_t GetNextFileAddress() const { return nextFileAddress; }
 
     std::optional<std::pair<size_t, size_t>> FindLinkedSpace() {
         if (mode != DataloggerMode::LinkedFixed && mode != DataloggerMode::LinkedTruncate) return std::nullopt;
@@ -137,8 +141,6 @@ public:
                     // Check if enough space for a new file
                     if (addr + file_sz < flashSize) {
                         return std::make_pair(addr, file_sz);
-                    } else {
-                        return std::nullopt;
                     }
                 } else if (mode == DataloggerMode::LinkedTruncate) {
                     // Truncate to next metadata or use initial size
@@ -150,6 +152,11 @@ public:
         }
         return std::nullopt;
     }
+
+
+    int GetLastError() const { return lastError; }
+    off_t GetCurrentOffset() const { return currentOffset; }
+    off_t GetNextFileAddress() const { return nextFileAddress; }
 
 private:
     const device* flash;
@@ -165,26 +172,38 @@ private:
     size_t currentOffset;
     size_t nextFileAddress;
 
-    void PrepMetadata(const std::string& filename, size_t next_addr) {
+    void PrepMetadata(const std::string& filename, size_t nextADdr) {
         memset(&metadata, 0, sizeof(metadata));
         strncpy(metadata.filename, filename.c_str(), sizeof(metadata.filename) - 1);
         metadata.allocated_size = fileSize;
         metadata.version = DATALOGGER_VERSION;
         metadata.packet_size = sizeof(T);
-        metadata.next_file_address = next_addr;
+        metadata.next_file_address = nextADdr;
     }
 
-    int ReadMetadata(size_t addr, DataloggerMetadata& out_meta) {
-        return -1; // TODO
+    int ReadMetadata(off_t addr, DataloggerMetadata& outMeta) {
+        while (addr + sizeof(DataloggerMetadata) < flashSize) {
+            int ret = flash_read(flash, addr, &outMeta, sizeof(DataloggerMetadata));
+            if (ret < 0) {
+                return ret;
+            }
+            if (outMeta.version == DATALOGGER_VERSION && outMeta.packet_size == sizeof(T) &&
+                outMeta.allocated_size == fileSize) {
+                return 0;
+            }
+            addr += sizeof(DataloggerMetadata);
+        }
     }
-    size_t FindNextMetadata(size_t start_addr, size_t max_addr) {
-        DataloggerMetadata meta;
-        for (size_t addr = start_addr; addr + sizeof(DataloggerMetadata) < max_addr; addr += sizeof(DataloggerMetadata)) {
+
+    size_t FindNextMetadata(off_t startAddr, off_t maxAddr) {
+        DataloggerMetadata meta{};
+        for (off_t addr = startAddr; addr + sizeof(DataloggerMetadata) < maxAddr;
+             addr += sizeof(DataloggerMetadata)) {
             if (ReadMetadata(addr, meta) == 0) {
                 return addr;
             }
         }
-        return max_addr;
+        return maxAddr;
     }
 };
 
