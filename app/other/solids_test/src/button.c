be6d1599e93af331b03d499e644da4baf88a3398
zizz-0 <rizzo.zoej@gmail.com>
2025-12-19 15:25:50 -0500

    swap tx/rx and button/key_switch

--- app/other/solids_test/src/button.c
+++ app/other/solids_test/src/button.c
@@ -11,10 +11,11 @@
 
 LOG_MODULE_REGISTER(button, LOG_LEVEL_INF);
 
-#define TX_NODE DT_NODELABEL(tx)
-#define RX_NODE DT_NODELABEL(rx)
-static const struct gpio_dt_spec tx = GPIO_DT_SPEC_GET(TX_NODE, gpios);
-static const struct gpio_dt_spec rx = GPIO_DT_SPEC_GET(RX_NODE, gpios);
+// button = tx, key switch = rx
+#define BUTTON_NODE DT_NODELABEL(button)
+#define SWITCH_NODE DT_NODELABEL(key_switch)
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON_NODE, gpios);
+static const struct gpio_dt_spec key_switch = GPIO_DT_SPEC_GET(SWITCH_NODE, gpios);
 
 static struct gpio_callback button_cb_data;
 static struct gpio_callback switch_cb_data;
@@ -24,42 +25,41 @@ static bool buzzing = false;
 
 K_THREAD_DEFINE(buzz_thread, 512, buzzer_task, NULL, NULL, NULL, 10, 0, 0);
 
-// tx = button, rx = key switch
 int button_switch_init() {
-    int ret = gpio_pin_configure_dt(&rx, GPIO_INPUT);
+    int ret = gpio_pin_configure_dt(&key_switch, GPIO_INPUT);
     if (ret < 0) {
-        LOG_ERR("Failed to conf rx :(");
+        LOG_ERR("Failed to conf key switch (rx) :(");
         return -1;
     }
 
-    ret = gpio_pin_configure_dt(&tx, GPIO_INPUT);
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
     if (ret < 0) {
-        LOG_ERR("Failed to conf tx :(");
+        LOG_ERR("Failed to conf button (tx) :(");
         return -1;
     }
 
-    if (!gpio_is_ready_dt(&tx)) {
-		LOG_ERR("Error: button device %s is not ready\n", tx.port->name);
+    if (!gpio_is_ready_dt(&button)) {
+		LOG_ERR("Error: button device %s is not ready\n", button.port->name);
         return -1;
 	}
 
-	ret = gpio_pin_interrupt_configure_dt(&tx, GPIO_INT_EDGE_TO_ACTIVE);
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
 	if (ret != 0) {
-		LOG_ERR("Error %d: failed to configure interrupt on %s pin %d\n", ret, tx.port->name, tx.pin);
+		LOG_ERR("Error %d: failed to configure interrupt on %s pin %d\n", ret, button.port->name, button.pin);
         return -1;
 	}
 
-    ret = gpio_pin_interrupt_configure_dt(&rx, GPIO_INT_EDGE_TO_ACTIVE);
+    ret = gpio_pin_interrupt_configure_dt(&key_switch, GPIO_INT_EDGE_TO_ACTIVE);
 	if (ret != 0) {
-		LOG_ERR("Error %d: failed to configure interrupt on %s pin %d\n", ret, rx.port->name, rx.pin);
+		LOG_ERR("Error %d: failed to configure interrupt on %s pin %d\n", ret, key_switch.port->name, key_switch.pin);
         return -1;
 	}
 
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(tx.pin));
-	gpio_add_callback(tx.port, &button_cb_data);
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
 
-    gpio_init_callback(&switch_cb_data, key_switch_state, BIT(rx.pin));
-	gpio_add_callback(rx.port, &switch_cb_data);
+    gpio_init_callback(&switch_cb_data, key_switch_state, BIT(key_switch.pin));
+	gpio_add_callback(key_switch.port, &switch_cb_data);
 
     return 0;
 }
@@ -70,7 +70,7 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t
 }
 
 void key_switch_state(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
-    int val = gpio_pin_get_dt(&rx);
+    int val = gpio_pin_get_dt(&key_switch);
     if (val > 0) {
         key_switched = true;
         set_ematch(1);
