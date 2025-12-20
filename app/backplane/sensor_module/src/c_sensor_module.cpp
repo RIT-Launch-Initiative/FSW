e1e6f5927a2ad4f2a146850131a2b7ebc1f161cd
Aaron Chan <aarchan108@gmail.com>
2025-12-20 17:05:12 -0500

    Define latest message port

--- app/backplane/sensor_module/src/c_sensor_module.cpp
+++ app/backplane/sensor_module/src/c_sensor_module.cpp
@@ -2,6 +2,7 @@
 
 // F-Core Tenant
 #include <f_core/messaging/c_msgq_message_port.h>
+#include <f_core/messaging/c_latest_message_port.h>
 #include <f_core/os/n_rtos.h>
 #include <f_core/utils/n_time_utils.h>
 #include <zephyr/logging/log.h>
