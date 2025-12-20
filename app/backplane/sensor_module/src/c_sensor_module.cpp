41307b3625976b30dae89e0167b0af2af306268b
Aaron Chan <aarchan108@gmail.com>
2025-12-20 17:14:36 -0500

    Change implementation from MsgqMessagePort to LatestMessagePort

--- app/backplane/sensor_module/src/c_sensor_module.cpp
+++ app/backplane/sensor_module/src/c_sensor_module.cpp
@@ -8,11 +8,8 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(sensor_module);
 
-K_MSGQ_DEFINE(broadcastQueue, sizeof(NTypes::SensorData), 10, 4);
-static auto broadcastMsgQueue = CMsgqMessagePort<NTypes::SensorData>(broadcastQueue);
-
-K_MSGQ_DEFINE(downlinkQueue, sizeof(NTypes::LoRaBroadcastSensorData), 10, 4);
-static auto downlinkMsgQueue = CMsgqMessagePort<NTypes::LoRaBroadcastSensorData>(downlinkQueue);
+static auto broadcastMsgQueue = CLatestMessagePort<NTypes::SensorData>();
+static auto downlinkMsgQueue = CLatestMessagePort<NTypes::LoRaBroadcastSensorData>();
 
 K_MSGQ_DEFINE(dataLogQueue, sizeof(NTypes::TimestampedSensorData), 12, 4);
 static auto dataLogMsgQueue = CMsgqMessagePort<NTypes::TimestampedSensorData>(dataLogQueue);
