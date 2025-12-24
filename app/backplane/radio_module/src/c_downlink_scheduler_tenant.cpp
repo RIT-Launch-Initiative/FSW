ef5b1dda3c9323d67d2e75dacbdcfdf476f43fed
Aaron Chan <aarchan108@gmail.com>
2025-12-23 19:22:51 -0500

    comments for donwlink sched

--- app/backplane/radio_module/src/c_downlink_scheduler_tenant.cpp
+++ app/backplane/radio_module/src/c_downlink_scheduler_tenant.cpp
@@ -1,8 +1,26 @@
 #include "c_downlink_scheduler_tenant.h"
 
-void CDownlinkSchedulerTenant::HandleFrame(const LaunchLoraFrame& frame) {}
-void CDownlinkSchedulerTenant::Run() {}
-void CDownlinkSchedulerTenant::PadRun() {}
-void CDownlinkSchedulerTenant::FlightRun() {}
-void CDownlinkSchedulerTenant::LandedRun() {}
-void CDownlinkSchedulerTenant::GroundRun() {}
+void CDownlinkSchedulerTenant::HandleFrame(const LaunchLoraFrame& frame) {
+    if (this->state == State::FLIGHT) {
+
+
+    }
+}
+
+void CDownlinkSchedulerTenant::Run() {
+    this->Clock();
+}
+
+void CDownlinkSchedulerTenant::PadRun() {
+    // HandleFrame handles this :)
+    // Unless we want something like heartbeats or something
+}
+void CDownlinkSchedulerTenant::FlightRun() {
+    // TODO: Implement some sort of weighting. Right now just blast
+}
+void CDownlinkSchedulerTenant::LandedRun() {
+    // Only transmit GPS every 5 seconds
+}
+void CDownlinkSchedulerTenant::GroundRun() {
+    return;
+}
