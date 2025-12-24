7b3e7bed5ad76c5c2547cb6c9e385cc6915c57e2
Aaron Chan <aarchan108@gmail.com>
2025-12-23 21:04:15 -0500

    Pass by ref

--- include/f_core/radio/frame_handlers/c_lora_frame_to_udp_handler.h
+++ include/f_core/radio/frame_handlers/c_lora_frame_to_udp_handler.h
@@ -12,7 +12,7 @@ public:
      * @param sock Socket to use for UDP transmission
      * @param srcPort Source port for UDP packets
      */
-    explicit CLoraFrameToUdpHandler(CUdpSocket sock, uint16_t srcPort);
+    explicit CLoraFrameToUdpHandler(const CUdpSocket& sock, const uint16_t srcPort);
 
 
     /**
--- lib/f_core/radio/frame_handlers/c_lora_frame_to_udp_handler.cpp
+++ lib/f_core/radio/frame_handlers/c_lora_frame_to_udp_handler.cpp
@@ -1,6 +1,6 @@
 #include "f_core/radio/frame_handlers/c_lora_frame_to_udp_handler.h"
 
-CLoraFrameToUdpHandler::CLoraFrameToUdpHandler(CUdpSocket sock, uint16_t srcPort) :
+CLoraFrameToUdpHandler::CLoraFrameToUdpHandler(const CUdpSocket& sock, const uint16_t srcPort) :
     sock(sock), srcPort(srcPort) {}
 
 void CLoraFrameToUdpHandler::HandleFrame(const LaunchLoraFrame& frame) {
