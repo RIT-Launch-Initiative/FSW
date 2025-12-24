d28300d8a24d5f28854001f676257368772dd925
Aaron Chan <aarchan108@gmail.com>
2025-12-23 19:51:23 -0500

    Implemented handle frame

--- app/backplane/radio_module/src/c_downlink_scheduler_tenant.cpp
+++ app/backplane/radio_module/src/c_downlink_scheduler_tenant.cpp
@@ -1,9 +1,26 @@
 #include "c_downlink_scheduler_tenant.h"
 
 void CDownlinkSchedulerTenant::HandleFrame(const LaunchLoraFrame& frame) {
-    if (this->state == State::FLIGHT) {
+    if (this->state == State::PAD) {
+        for (size_t i = 0; i + 1 < frame.Size; i += 2) {
+            uint16_t port = (static_cast<uint16_t>(frame.Payload[i]) << 8) | static_cast<uint16_t>(frame.Payload[i + 1]);
+            auto portMsgPortOpt = telemetryMessagePortMap.Get(port);
+            if (!portMsgPortOpt.has_value()) {
+                continue;
+            }
 
+            CMessagePort<LaunchLoraFrame>* portMsgPort = portMsgPortOpt.value();
+            LaunchLoraFrame telemFrame{};
+            int ret = portMsgPort->Receive(telemFrame, K_NO_WAIT);
+            if (ret < 0) {
+                LOG_ERR("Failed to receive telemetry frame on port %d", port);
+            }
 
+            ret = loraDownlinkMessagePort.Send(telemFrame, K_NO_WAIT);
+            if (ret < 0) {
+                LOG_ERR("Failed to send telemetry frame on port %d", port);
+            }
+        }
     }
 }
 
