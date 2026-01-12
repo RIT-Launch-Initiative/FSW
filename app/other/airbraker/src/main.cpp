0fe0489b8bfd744ac8d921e0f554e7da2b600d6c
Richard Sommers <44383226+cowsed@users.noreply.github.com>
2026-01-12 07:35:56 -0500

    Airbrake Program Setup (#368)

--- app/other/airbraker/src/main.cpp
+++ app/other/airbraker/src/main.cpp
@@ -1,6 +1,5 @@
-#include "n_model.h"
-#include "n_storage.h"
 #include "servo.h"
+#include "n_storage.h"
 
 #include <zephyr/init.h>
 #include <zephyr/logging/log.h>
@@ -10,20 +9,6 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_AIRBRAKE_LOG_LEVEL);
 SYS_INIT(servo_init, APPLICATION, 1);
 SYS_INIT(storage_init, APPLICATION, 2);
 
-volatile float alt = 1.0;
-volatile float acc = 0;
-
 int main() {
-    using namespace NModel;
-
-    k_msleep(1000);
-
-    printk("starting....\n");
-
-    k_msleep(1000);
-
-
-
     printk("Time to break air\n");
-    
 }
