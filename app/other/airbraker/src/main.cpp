1ec6e7fb8576bcdf0b512dbf1847077b323c8ecc
cowsed <richie.sommers@outlook.com>
2026-01-27 10:36:46 -0500

    add benchmark

--- app/other/airbraker/src/main.cpp
+++ app/other/airbraker/src/main.cpp
@@ -4,6 +4,8 @@
 #include <zephyr/init.h>
 #include <zephyr/logging/log.h>
 
+#include "math/n_benchmark.hpp"
+
 LOG_MODULE_REGISTER(main, CONFIG_APP_AIRBRAKE_LOG_LEVEL);
 
 SYS_INIT(servo_init, APPLICATION, 1);
@@ -11,4 +13,6 @@ SYS_INIT(storage_init, APPLICATION, 2);
 
 int main() {
     printk("Time to break air\n");
+
+    NBenchMark::benchmark();
 }
