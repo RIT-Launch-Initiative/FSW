b8a992d2de778c210d32a96ce0c2dbb811710f73
cowsed <richie.sommers@outlook.com>
2026-02-14 13:32:09 -0500

    spi on demand on button

--- app/other/spitest/fake_pi/src/main.c
+++ app/other/spitest/fake_pi/src/main.c
@@ -26,23 +26,30 @@ static const struct spi_config spi_periph_cfg = {
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define SW0_NODE DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS_OKAY(SW0_NODE)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
 
 int main(void) {
 
     int ret;
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) {
-        return 0;
-    }
     if (!device_is_ready(spi_periph_dev)) {
         printk("Spi not ready\n");
         return 0;
     }
 
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
+    if (!gpio_is_ready_dt(&button)) {
+        printk("Error: button device %s is not ready\n", button.port->name);
+        return 0;
+    }
+
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (ret != 0) {
+        printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name, button.pin);
         return 0;
     }
 
@@ -53,20 +60,25 @@ int main(void) {
 
     struct spi_buf_set tx_bufs = {.buffers = &tx_buf, .count = 1};
     struct spi_buf_set rx_bufs = {.buffers = &rx_buf, .count = 1}; // &rx_buf
-    printk("transceiveing\n");
 
     while (1) {
-        ret = gpio_pin_set_dt(&led, true);
-        if (ret < 0) {
-            return 0;
+        int ret = 0;
+
+        while (1) {
+            int val = gpio_pin_get_dt(&button);
+            if (val) {
+                break;
+            }
+            k_msleep(10);
         }
+        printk("transceiveing\n");
 
         // printk("transceiveing\n");
         ret = spi_transceive(spi_periph_dev, &spi_periph_cfg, &tx_bufs, &rx_bufs);
         if (ret != 0) {
             printk("Ret: %d\n", ret);
         }
-        ret = gpio_pin_set_dt(&led, false);
+        // ret = gpio_pin_set_dt(&led, false);
 
         k_msleep(50);
     }
