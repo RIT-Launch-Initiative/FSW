5300795db5e1d7f6196151b7626538879774a85f
cowsed <richie.sommers@outlook.com>
2026-02-19 00:11:13 -0500

    get rid of a lot of old benchmarking stuff

--- app/other/airbraker/src/main.cpp
+++ app/other/airbraker/src/main.cpp
@@ -13,30 +13,6 @@ SYS_INIT(servo_init, APPLICATION, 1);
 SYS_INIT(storage_init, APPLICATION, 2);
 
 int main() {
-    k_msleep(1000);
-
-    printk("thinking\n");
-    uint64_t uptimeUS = k_ticks_to_us_near64(k_uptime_ticks());
-    float alt_m = 1.0;
-    float accel_v = 16.8;
-
-    uint64_t start = k_cycle_get_64();
-
-    NModel::FeedKalman(uptimeUS, alt_m, accel_v);
-    uint64_t postUpdate = k_cycle_get_64();
-    NModel::KalmanState state = NModel::LastKalmanState();
-    uint64_t postState = k_cycle_get_64();
-    float effort = NModel::CalcActuatorEffort(0.F, 1.F);
-    uint64_t postEffort = k_cycle_get_64();
-
-    uint64_t fullDelta = k_cycle_get_64() - start;
-    uint64_t updateDelta = postUpdate - start;
-    uint64_t getDelta = postState - postUpdate;
-    uint64_t effortDelta = postEffort - postState;
-
-
-    k_msleep(1000);
-
     printk("Time to break air\n");
 
     NBenchMark::benchmark();
