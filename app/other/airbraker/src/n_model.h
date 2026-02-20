e63d2f9e247533daa14e887d93f764207f700e2b
Richard Sommers <44383226+cowsed@users.noreply.github.com>
2026-02-19 22:42:55 -0500

    Automatic Lookuptable generator for airbrakes (#374)
    Co-authored-by: Aaron <30483634+AarC10@users.noreply.github.com>

---
+++ app/other/airbraker/src/n_model.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "n_autocoder_types.h"
+
+namespace NModel {
+
+struct KalmanState {
+    float estAltitude;
+    float estVelocity;
+    float estAcceleration;
+};
+
+void FeedKalman(uint64_t usSinceBoot, float verticalAccelerationMS2, float altitude_feet);
+KalmanState LastKalmanState();
+
+struct GyroState {
+    float angleOffInitial;
+    float angleUncertainty;
+};
+
+void FeedGyro(uint64_t usSinceBoot, const NTypes::GyroscopeData &gyro);
+
+float CalcActuatorEffort(float altitude, float velocity);
+} // namespace NModel
