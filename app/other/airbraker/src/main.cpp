9f4d79f692985016277204705cf04575fc52242d
Richard Sommers <44383226+cowsed@users.noreply.github.com>
2026-02-19 22:42:55 -0500

    Automatic Lookuptable generator for airbrakes (#374)
    Co-authored-by: Aaron <30483634+AarC10@users.noreply.github.com>

--- app/other/airbraker/src/main.cpp
+++ app/other/airbraker/src/main.cpp
@@ -1,10 +1,9 @@
-#include "servo.h"
+#include "n_model.h"
 #include "n_storage.h"
+#include "servo.h"
 
 #include <zephyr/init.h>
 #include <zephyr/logging/log.h>
-
-#include "math/n_benchmark.hpp"
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_AIRBRAKE_LOG_LEVEL);
 
@@ -14,5 +13,6 @@ SYS_INIT(storage_init, APPLICATION, 2);
 int main() {
     printk("Time to break air\n");
 
-    NBenchMark::benchmark();
+    float effort = NModel::CalcActuatorEffort(1039.0475, 270.98915);
+    printk("Effort: %f\n", (double)effort);
 }
--- app/other/airbraker/src/n_model.cpp
+++ app/other/airbraker/src/n_model.cpp
@@ -1,6 +1,9 @@
 #include "n_model.h"
 
 #include "math/matrix.hpp"
+#include "n_model.h"
+
+#include "math/matrix.hpp"
 #include "n_autocoder_types.h"
 
 namespace NModel {
@@ -9,15 +12,15 @@ using StateTransitionT = Matrix<4, 4>;
 // clang-format off
 
 const StateTransitionT state_transition_matrix{{ // aka F
-    1, 2, 3, 4,
-    1, 2, 3, 4,
-    1, 2, 3, 4,
-    1, 2, 3, 4,
+    1,    0.01,    0.00005, 0,
+    0,    1,       0,       0,
+    0,    0,       1,       0,
+    0,    0,       0,       1,
 }};
 
-const Matrix<2, 4> kalman_output_matrix{{
-    1,2,3,4,
-    1,2,3,4,
+const Matrix<2, 4> kalman_output_matrix{{ // aka H
+    1,0,0,0,
+    0,0,1,1,
 }};
 const Matrix<4, 2> kalman_gain{{
     1,2,
@@ -25,6 +28,8 @@ const Matrix<4, 2> kalman_gain{{
     1,2,
     1,2
 }};
+
+Matrix<4, 1> kalman_state({1.0,10.0,16.9,-9.8});
 
 // clang-format on
 
@@ -39,6 +44,17 @@ Matrix<4, 1> kalmanPredictAndUpdate(const Matrix<4, 1> &state, const float altit
     Matrix<4, 1> correction = kalman_gain * innovation;
 
     return fx + correction;
+}
+
+void FeedKalman(uint64_t usSinceBoot, float verticalAccelerationMS2, float altitudeMeters) {
+    kalman_state = kalmanPredictAndUpdate(kalman_state, verticalAccelerationMS2, altitudeMeters);
+}
+KalmanState LastKalmanState() {
+    return {
+        .estAltitude = kalman_state.Get(0, 0),
+        .estVelocity = kalman_state.Get(1, 0),
+        .estAcceleration = kalman_state.Get(2, 0),
+    };
 }
 
 } // namespace NModel
