69397175b5c08fc2161df67bed774d6f3451fb27
Richard Sommers <44383226+cowsed@users.noreply.github.com>
2026-02-19 22:42:55 -0500

    Automatic Lookuptable generator for airbrakes (#374)
    Co-authored-by: Aaron <30483634+AarC10@users.noreply.github.com>

--- app/other/airbraker/src/n_model.cpp
+++ app/other/airbraker/src/n_model.cpp
@@ -1,20 +1,19 @@
-#include "n_model.hpp"
+#include "n_model.h"
+
+#include "math/matrix.hpp"
+#include "n_model.h"
 
 #include "math/matrix.hpp"
 #include "n_autocoder_types.h"
-#include "n_model.hpp"
-
-#include <cmath>
 
 namespace NModel {
-
 using StateTransitionT = Matrix<4, 4>;
 
 // clang-format off
 
 const StateTransitionT state_transition_matrix{{ // aka F
-    1,    0.01,    5e-5,    0,
-    0,    1,       0.01,    0,
+    1,    0.01,    0.00005, 0,
+    0,    1,       0,       0,
     0,    0,       1,       0,
     0,    0,       0,       1,
 }};
@@ -22,22 +21,23 @@ const StateTransitionT state_transition_matrix{{ // aka F
 const Matrix<2, 4> kalman_output_matrix{{ // aka H
     1,0,0,0,
     0,0,1,1,
+const Matrix<2, 4> kalman_output_matrix{{ // aka H
+    1,0,0,0,
+    0,0,1,1,
 }};
 const Matrix<4, 2> kalman_gain{{
-    0.1989683408635,     1.0003523356894e-09,
-    1.12897625945632,    2.48344519337973e-07,
-    1.82817311684792,    0.967713284079819,
-    -1.82817311684447,   0.032260907861791
+    1,2,
+    1,2,
+    1,2,
+    1,2
 }};
 
-static Matrix<4, 1> kalman_state({0, 0,  0,  9.8});
-static bool everWentOutOfBounds = false;
+Matrix<4, 1> kalman_state({1.0,10.0,16.9,-9.8});
 
 // clang-format on
 
-Matrix<4, 1> kalmanPredictAndUpdate(const Matrix<4, 1> &state, const float altitudeMeters,
-                                    const float verticalAccelerationMS2) {
-    Matrix<2, 1> sensorIn = Matrix<2, 1>::Column({altitudeMeters, verticalAccelerationMS2});
+Matrix<4, 1> kalmanPredictAndUpdate(const Matrix<4, 1> &state, const float altitude, const float acceleration) {
+    Matrix<2, 1> sensorIn = Matrix<2, 1>::Column({altitude, acceleration});
 
     // change via physics model
     Matrix<4, 1> fx = state_transition_matrix * state;
@@ -49,28 +49,15 @@ Matrix<4, 1> kalmanPredictAndUpdate(const Matrix<4, 1> &state, const float altit
     return fx + correction;
 }
 
-void FeedKalman(uint64_t usSinceBoot, float altitudeMeters, float verticalAccelerationMS2) {
-
-    kalman_state = kalmanPredictAndUpdate(kalman_state, altitudeMeters, verticalAccelerationMS2);
+void FeedKalman(uint64_t usSinceBoot, float verticalAccelerationMS2, float altitudeMeters) {
+    kalman_state = kalmanPredictAndUpdate(kalman_state, verticalAccelerationMS2, altitudeMeters);
 }
 KalmanState LastKalmanState() {
     return {
         .estAltitude = kalman_state.Get(0, 0),
         .estVelocity = kalman_state.Get(1, 0),
         .estAcceleration = kalman_state.Get(2, 0),
-        .estBias = kalman_state.Get(3, 0),
     };
 }
-
-float AltitudeMetersFromPressureKPa(float pressure_kpa) {
-    float pressure = pressure_kpa * 10;
-    float altitude = (1 - powf(pressure / 1013.25F, 0.190284F)) * (float) (145366.45 * 0.3048);
-    return altitude;
-}
-
-void FeedGyro(uint64_t usSinceBoot, const NTypes::GyroscopeData &gyro) {}
-int GetOrientation() { return 0; }
-bool gyroOutOfBounds() { return false; }
-bool EverWentOutOfBounds() { return everWentOutOfBounds;}
 
 } // namespace NModel
