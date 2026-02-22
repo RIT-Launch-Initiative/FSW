f46855da735bcc2117c5f3beff99383dbb128e91
LiterallyJaMaR <jrandolph195@gmail.com>
2026-02-22 18:49:56 -0500

    Add Motor Directions, NSleep and Reset Ripple Count

--- app/payload/CRASHOUT/motor_driver_test/src/main.c
+++ app/payload/CRASHOUT/motor_driver_test/src/main.c
@@ -10,24 +10,26 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/kernel.h>
+#include <string.h>
 
 const struct device* const i2c_bus = DEVICE_DT_GET(DT_NODELABEL(arduino_i2c));
 const struct i2c_dt_spec i2c_dev = {.bus = i2c_bus, .addr = 0x30};
+uint8_t flt = 0;
+bool motorOn = false;
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS 1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
-// #define NSLEEP_NODE DT_ALIAS(nsleep)
+#define NSLEEP_NODE DT_NODELABEL(nsleep)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-// static const struct gpio_dt_spec nSleep = GPIO_DT_SPEC_GET(NSLEEP_NODE, gpios);
+static const struct gpio_dt_spec nSleep = GPIO_DT_SPEC_GET(NSLEEP_NODE, gpios);
 
 float map_voltage(uint8_t vmtr) { return ((float) vmtr / 228.f) * 38.f; }
 
@@ -61,6 +63,7 @@ float speed_rpm(uint8_t speed, int32_t w_scale) {
 #define RC_CTRL6_REG 0x17
 #define RC_CTRL7_REG 0x18
 
+
 void set_voltage(float volts) {
     if (volts > 38) {
         printk("invalid volts");
@@ -70,6 +73,100 @@ void set_voltage(float volts) {
     uint8_t regval = (uint8_t) (val + .5f);
     i2c_reg_write_byte_dt(&i2c_dev, REG_CTRL_1_REG, regval);
 }
+void turnOn(){
+    uint8_t flt = 0;
+
+    gpio_pin_configure_dt(&nSleep, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+
+    k_msleep(10);
+
+    gpio_pin_configure_dt(&nSleep, GPIO_OUTPUT_ACTIVE);
+
+    uint8_t reg_ctrl_0 = 0b00110111;
+    i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL0_REG, 0b11100010);
+    i2c_reg_write_byte_dt(&i2c_dev, REG_CTRL_0_REG, reg_ctrl_0);
+    i2c_reg_write_byte_dt(&i2c_dev, REG_CTRL_1_REG, 0xFF);
+    motorOn = true;
+}
+void clearRippleCount(){
+    gpio_pin_configure_dt(&nSleep, GPIO_OUTPUT_INACTIVE);
+    k_msleep(10);
+    gpio_pin_configure_dt(&nSleep, GPIO_OUTPUT_ACTIVE);
+    uint8_t reg_ctrl_0 = 0b00110111;
+    i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL0_REG, 0b11100010);
+    i2c_reg_write_byte_dt(&i2c_dev, REG_CTRL_0_REG, reg_ctrl_0);
+    i2c_reg_write_byte_dt(&i2c_dev, REG_CTRL_1_REG, 0xFF);
+}
+void setSpeedMode(){
+}
+void spin(uint8_t dir){
+    if(!motorOn){
+        printk("Motor is off, cannot spin\n");
+        return;
+    }
+    int ret;
+    int ns;
+    bool led_state = true;
+
+    uint32_t w_scale_value = 16;
+
+    printk("Initted i2c dev\n");
+    i2c_reg_write_byte_dt(&i2c_dev, CONFIG4_REG, 0x34);
+    i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
+    printk("Fault 34: %02x\n", flt);
+    
+    i2c_reg_write_byte_dt(&i2c_dev, CONFIG0_REG, 0xe0);
+    i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
+    printk("Fault e0: %02x\n", flt);
+
+    i2c_reg_write_byte_dt(&i2c_dev, CONFIG4_REG, dir); // 0x36 one dir, 0x37 other dir
+    i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
+    printk("Fault 36: %02x\n", flt);
+
+    i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
+    printk("Fault: %02x\n", flt);
+
+    // set_voltage(11.0);
+
+    for (int i = 0; i < 100; i++) {
+        ret = gpio_pin_toggle_dt(&led);
+        if (ret < 0) {
+            return 0;
+        }
+
+        led_state = !led_state;
+        // printf("LED state: %s\n", led_state ? "ON" : "OFF");
+        uint8_t vmtr = 0;
+        i2c_reg_read_byte_dt(&i2c_dev, REG_STATUS1, &vmtr);
+        float volts = map_voltage(vmtr);
+        uint8_t speed;
+        i2c_reg_read_byte_dt(&i2c_dev, RC_STATUS1, &speed);
+        printk("Speed: %02x\n", speed);
+        float speedrpm = speed_rpm(speed, w_scale_value);
+
+        uint8_t rcc0 = 0;
+        uint8_t rcc1 = 0;
+        i2c_reg_read_byte_dt(&i2c_dev, RC_STATUS2, &rcc0);
+        i2c_reg_read_byte_dt(&i2c_dev, RC_STATUS3, &rcc1);
+        uint16_t rc_cnt = (rcc1 << 8) | rcc0;
+
+        printk("V=%.4f V, S=%.4f rpm, RC=%d, ", (double) volts, (double) speedrpm, (int) rc_cnt);
+        if(dir == 0x36){
+            printk("Clockwise\n");
+        } else if(dir == 0x37){
+            printk("Counter Clockwise\n");
+        } else{
+            printk("Failed to read Direction\n");
+        }
+
+        i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
+
+        k_msleep(40);
+    }
+    i2c_reg_write_byte_dt(&i2c_dev, CONFIG4_REG, 0x34);
+}
+
 
 int main(void) {
     int ret;
@@ -82,16 +179,10 @@ int main(void) {
         printk("No i2c ready");
         return 0;
     }
-    k_msleep(500);
-    // clear all
-    i2c_reg_write_byte_dt(&i2c_dev, CONFIG0_REG, 0b0110 | 0xc0);
-
-    // resistance .001 kOhm
-    // resistance 1ohm = R
-    // inv_r_scale = 64 = 0b01
-    // INV_R_SCALE / R = 64
-    // INV_R = 64
-    // RC_CONTROL_2 = 0x13
+    
+    k_msleep(1000);
+    turnOn();
+    
     static const uint8_t inv_r_scale = 0b01;
     static const uint8_t inv_r = 64;
 
@@ -106,66 +197,21 @@ int main(void) {
     i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL3_REG, rc_ctrl_3);
     i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL4_REG, rc_ctrl_4);
 
-    // max speed = 1046.666666666667 rad/s at motor
-    // uint8_t w_scale_bit = 0b00; // x16 max speed 4080 rad/s
-    uint32_t w_scale_value = 16;
-    uint8_t reg_ctrl_0 = 0b00111111; // defaults and x16 w_scale and voltage control
-    i2c_reg_write_byte_dt(&i2c_dev, REG_CTRL_0_REG, reg_ctrl_0);
+    spin(0x36);
+    k_msleep(1000);
+    clearRippleCount();
+    i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL2_REG, rc_ctrl_2);
+    i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL3_REG, rc_ctrl_3);
+    i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL4_REG, rc_ctrl_4);
 
-    printk("Initted i2c dev\n");
+    spin(0x37);
+    k_msleep(1000);
+    clearRippleCount();
+    i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL2_REG, rc_ctrl_2);
+    i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL3_REG, rc_ctrl_3);
+    i2c_reg_write_byte_dt(&i2c_dev, RC_CTRL4_REG, rc_ctrl_4);
 
-    uint8_t flt = 0;
-    i2c_reg_write_byte_dt(&i2c_dev, CONFIG4_REG, 0x34); // 0x36 one dir, 0x37 other dir
-    i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
-    printk("Fault 34: %02x\n", flt);
-    i2c_reg_write_byte_dt(&i2c_dev, CONFIG0_REG, 0xe0);
-    i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
-    printk("Fault e0: %02x\n", flt);
-    i2c_reg_write_byte_dt(&i2c_dev, CONFIG4_REG, 0x36); // 0x36 one dir, 0x37 other dir
-    i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
-    printk("Fault 36: %02x\n", flt);
-
-    i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
-    printk("Fault: %02x\n", flt);
-
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        return 0;
-    }
-
-    set_voltage(11.0);
-
-	float weezer[10] = {9.6, 7.1, 9.6, 11.0, 11.9, 10.6, 9.6, 7.6, 7.1, 7.1};
-    for (int i = 0; i < 100; i++) {
-        float freq = weezer[(i/10) % 10];
-		set_voltage(freq);
-        ret = gpio_pin_toggle_dt(&led);
-        if (ret < 0) {
-            return 0;
-        }
-
-        led_state = !led_state;
-        // printf("LED state: %s\n", led_state ? "ON" : "OFF");
-        uint8_t vmtr = 0;
-        i2c_reg_read_byte_dt(&i2c_dev, REG_STATUS1, &vmtr);
-        float volts = map_voltage(vmtr);
-        uint8_t speed = 0;
-        i2c_reg_read_byte_dt(&i2c_dev, RC_STATUS1, &speed);
-        float speedrpm = speed_rpm(speed, w_scale_value);
-
-        uint8_t rcc0 = 0;
-        uint8_t rcc1 = 0;
-        i2c_reg_read_byte_dt(&i2c_dev, RC_STATUS2, &rcc0);
-        i2c_reg_read_byte_dt(&i2c_dev, RC_STATUS3, &rcc1);
-        uint16_t rc_cnt = (rcc1 << 8) | rcc0;
-
-        printk("V=%.4f V, S=%.4f rpm, RC=%d\n", (double) volts, (double) speedrpm, (int) rc_cnt);
-
-        i2c_reg_read_byte_dt(&i2c_dev, 0, &flt);
-
-        k_msleep(40);
-    }
-    i2c_reg_write_byte_dt(&i2c_dev, CONFIG4_REG, 0x34); // 0x36 one dir, 0x37 other dir
+    spin(0x36);
 
     return 0;
 }
