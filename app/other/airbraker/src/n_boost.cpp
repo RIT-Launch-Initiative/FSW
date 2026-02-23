f7327f589a91bc9b7f87ab84f1268195976a0ab8
cowsed <richie.sommers@outlook.com>
2026-02-23 16:47:53 -0500

    dont undetect boost

--- app/other/airbraker/src/n_boost.cpp
+++ app/other/airbraker/src/n_boost.cpp
@@ -8,6 +8,11 @@ CCircularBuffer<float, NUM_SAMPLES_OVER_BOOST_THRESHOLD_REQUIRED> buffer{0};
 bool isDetected = false;
 
 void FeedDetector(float accelVertical) {
+    // don't want to undetect after detection even if someone feeds us more
+    if (isDetected){
+        return;
+    }
+
     buffer.AddSample(accelVertical);
     bool detected = true;
     for (unsigned int i = 0; i < buffer.Size(); i++) {
