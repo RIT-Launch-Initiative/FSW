304a6e3dea667ebdd02a2951513f73b219393abc
cowsed <richie.sommers@outlook.com>
2026-02-26 18:42:10 -0500

    take theirs

--- app/other/airbraker/src/main.cpp
+++ app/other/airbraker/src/main.cpp
@@ -7,20 +7,12 @@
 
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
-#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_AIRBRAKE_LOG_LEVEL);
 
 SYS_INIT(servo_init, APPLICATION, 1);
 SYS_INIT(storage_init, APPLICATION, 2);
-
-K_TIMER_DEFINE(measurement_timer, NULL, NULL);
-
-uint32_t packet_timestamp() {
-    int64_t ms = k_uptime_get();
-    return (uint32_t) ms;
-}
 
 K_TIMER_DEFINE(measurement_timer, NULL, NULL);
 
