a90be23f9f5325137888ec225096144a16176df7
Aaron Chan <aarchan108@gmail.com>
2026-04-16 16:13:30 -0400

    native sim has signal handlers to clean up resources during shutdown

--- app/backplane/radio_module/src/main.cpp
+++ app/backplane/radio_module/src/main.cpp
@@ -8,8 +8,18 @@
 
 #include <f_core/os/c_task.h>
 #include <f_core/os/n_rtos.h>
+#ifdef CONFIG_ARCH_POSIX
+#include <signal.h>
+#include <nsi_main.h>
+#endif
 
 LOG_MODULE_REGISTER(main);
+
+#ifdef CONFIG_ARCH_POSIX
+static volatile sig_atomic_t shutdownRequested = 0;
+
+static void NativeSimSignalHandler(int) { shutdownRequested = 1; }
+#endif
 
 int main() {
 #ifdef CONFIG_LICENSED_FREQUENCY
@@ -28,8 +38,15 @@ int main() {
     NRtos::StartRtos();
 
 #ifdef CONFIG_ARCH_POSIX
-    k_sleep(K_SECONDS(300));
+    signal(SIGINT, NativeSimSignalHandler);
+    signal(SIGTERM, NativeSimSignalHandler);
+
+    while (!shutdownRequested) {
+        k_sleep(K_MSEC(100));
+    }
+
     NRtos::StopRtos();
+    nsi_exit(0);
 #endif
 
     return 0;
