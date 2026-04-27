ea0159bd26b82063228e256e67f9f2875cc5e086
Richard Sommers <44383226+cowsed@users.noreply.github.com>
2026-04-27 10:56:38 -0400

    Crashout Bringup (#389)

--- boards/arm/crashout/board.cmake
+++ boards/arm/crashout/board.cmake
@@ -2,9 +2,9 @@
 
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
-board_runner_args(jlink "--device=STM32F103RB" "--speed=4000")
+board_runner_args(jlink "--device=STM32F103C8" "--speed=4000")
 
 # keep first
-include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
